5b9487f7b88f501a3cc28f1363d6162a10196b2e
Jochen Klar <jklar@aip.de>
2015-09-04 16:23:18 +0200

    test for login link

--- core/tests.py
+++ core/tests.py
@@ -1,11 +1,26 @@
 from django.test import TestCase, Client
+from django.conf import settings
+from django.contrib.auth.models import User
 
 class ClientTestCase(TestCase):
 
     def setUp(self):
-        pass
+        User.objects.create_user('test','test@example.com','test')
 
     def test_home(self):
-        """ The home page can be accessed """
+        """ The home page can be accessed. """
         response = Client().get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_login_link(self):
+        """ The login link is rendered correctly. """
+
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.content.find('<a href="%s">Login</a>' % settings.LOGIN_URL), -1)
+
+        c.login(username='test', password='test')
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.content.find('<a href="%s">Logout</a>' % settings.LOGOUT_URL), -1)
