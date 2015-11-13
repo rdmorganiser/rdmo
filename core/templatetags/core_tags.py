e36217b512912457e9b17fa477aaee771fa255ee
Jochen Klar <jochenklar@gmail.com>
2015-11-13 20:16:31 +0100

    base_url template tag

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -2,39 +2,46 @@ from django import template
 from django.conf import settings
 from django.utils import translation
 from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
 
 register = template.Library()
+
+
+@register.simple_tag()
+def base_url():
+    path = '/'.join(Site.objects.get_current().domain.rstrip('/').split('/')[1:])
+    if path:
+        path = '/' + path
+    return path
+
+
+@register.simple_tag()
+def internal_link(name, text=None, style=None):
+    url = reverse(name)
+
+    if text is None:
+        text = url
+
+    return "<a href=\"%s%s\">%s</a>" % (base_url(), url, text)
 
 
 @register.simple_tag(takes_context=True)
 def login_link(context):
     if context.request.user.is_authenticated():
-        url = reverse('logout')
-        text = 'Logout'
+        return internal_link('logout', 'Logout')
     else:
-        url = reverse('login')
-        text = 'Login'
-
-    return "<a href=\"%s\">%s</a>" % (url, text)
-
-
-@register.simple_tag()
-def internal_link(name, text=None):
-    url = reverse(name)
-
-    if text is None:
-        text = url
-    return "<a href=\"%s\">%s</a>" % (url, text)
+        return internal_link('login', 'Login')
 
 
 @register.simple_tag()
 def i18n_switcher():
+    base = base_url() + '/i18n/'
     string = ''
-    for language in settings.LANGUAGES:
-        if language[0] == translation.get_language():
-            string += "<li><a href=\"/i18n/%s\"><u>%s</u></a></li>" % language
+    for language, language_string in settings.LANGUAGES:
+        if language == translation.get_language():
+            string += "<li><a href=\"%s%s\"><u>%s</u></a></li>" % (base, language, language_string)
         else:
-            string += "<li><a href=\"/i18n/%s\">%s</a></li>" % language
+            string += "<li><a href=\"%s%s\">%s</a></li>" % (base, language, language_string)
 
     return string
 
