835ca8163018551aa3b64cc45ab095a4e8044c27
Jochen Klar <jklar@aip.de>
2015-11-17 17:44:56 +0100

    base_url removed, utils added, working with script alias fixed

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -2,17 +2,8 @@ from django import template
 from django.conf import settings
 from django.utils import translation
 from django.core.urlresolvers import reverse
-from django.contrib.sites.models import Site
 
 register = template.Library()
-
-
-@register.simple_tag()
-def base_url():
-    path = '/'.join(Site.objects.get_current().domain.rstrip('/').split('/')[1:])
-    if path:
-        path = '/' + path
-    return path
 
 
 @register.simple_tag()
@@ -22,7 +13,7 @@ def internal_link(name, text=None, style=None):
     if text is None:
         text = url
 
-    return "<a href=\"%s%s\">%s</a>" % (base_url(), url, text)
+    return "<a href=\"%s\">%s</a>" % (url, text)
 
 
 @register.simple_tag(takes_context=True)
@@ -35,13 +26,16 @@ def login_link(context):
 
 @register.simple_tag()
 def i18n_switcher():
-    base = base_url() + '/i18n/'
     string = ''
     for language, language_string in settings.LANGUAGES:
+        # lookup the i18n url and (for reasons unknown) remove the trailing slash
+        url = reverse('i18n_switcher', args=[language]).rstrip('/')
+
+        # create the underlined link for the active language or the normal one
         if language == translation.get_language():
-            string += "<li><a href=\"%s%s\"><u>%s</u></a></li>" % (base, language, language_string)
+            string += "<li><a href=\"%s\"><u>%s</u></a></li>" % (url, language_string)
         else:
-            string += "<li><a href=\"%s%s\">%s</a></li>" % (base, language, language_string)
+            string += "<li><a href=\"%s\">%s</a></li>" % (url, language_string)
 
     return string
 
---
+++ core/utils.py
@@ -0,0 +1,14 @@
+from django.utils.six.moves.urllib.parse import urlparse
+
+
+def get_script_alias(request):
+    return request.path[:-len(request.path_info)]
+
+
+def get_referer_path_info(request, default=None):
+    referer = request.META.get('HTTP_REFERER', None)
+    if not referer:
+        return default
+
+    script_alias = get_script_alias(request)
+    return urlparse(referer).path[len(script_alias):]
