8fa5f91d94b56c52b31d54f12ee045b6dc1aec35
Jochen Klar <jklar@aip.de>
2015-11-20 12:20:18 +0100

    permissions and internal links

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -3,25 +3,34 @@ from django.conf import settings
 from django.utils import translation
 from django.core.urlresolvers import reverse
 
+from core.utils import get_internal_link
+
 register = template.Library()
 
 
-@register.simple_tag()
-def internal_link(name, text=None, style=None):
-    url = reverse(name)
+@register.simple_tag(takes_context=True)
+def internal_link(context, name, text=None, permission=None):
+    if permission:
+        if not context.request.user.has_perm(permission):
+            return ''
 
-    if text is None:
-        text = url
+    return get_internal_link(name, text)
 
-    return "<a href=\"%s\">%s</a>" % (url, text)
+
+@register.simple_tag(takes_context=True)
+def admin_link(context, text='Admin'):
+    if not context.request.user.is_superuser:
+        return ''
+
+    return get_internal_link('admin:index', text)
 
 
 @register.simple_tag(takes_context=True)
 def login_link(context):
     if context.request.user.is_authenticated():
-        return internal_link('logout', 'Logout')
+        return get_internal_link('logout', 'Logout')
     else:
-        return internal_link('login', 'Login')
+        return get_internal_link('login', 'Login')
 
 
 @register.simple_tag()
