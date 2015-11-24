731cdeb0e4ff4041dc5cb6b75823bcd32bab396d
Jochen Klar <jochenklar@gmail.com>
2015-11-24 18:35:12 +0100

    urls refactored and more

--- DMPwerkzeug/urls.py
+++ DMPwerkzeug/urls.py
@@ -4,40 +4,21 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
-from registration.backends.default.views import RegistrationView, ActivationView
+from apps import accounts
 
 handler404 = 'apps.core.views.not_found'
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='home'),
 
-    # edit own profile
-    url(_(r'^profile/$'), 'apps.accounts.views.profile_update', name='profile_update'),
-
-    # change and retrieve password
-    url(_(r'^password/change/$'), auth_views.password_change, name='password_change'),
-    url(_(r'^password/change/done/$'), auth_views.password_change_done, name='password_change_done'),
-    url(_(r'^password/reset/$'), auth_views.password_reset, name='password_reset'),
-    url(_(r'^password/reset/done/$'), auth_views.password_reset_done, name='password_reset_done'),
-    url(_(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'),
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(_(r'^password/reset/complete/$'), auth_views.password_reset_complete, name='password_reset_complete'),
-
-    # user registration
-    url(_(r'^register/$'), RegistrationView.as_view(), name='registration_register'),
-    url(_(r'^register/complete/$'), TemplateView.as_view(template_name='registration/registration_complete.html'),
-        name='registration_complete'),
-    url(_(r'^activate/complete/$'), TemplateView.as_view(template_name='registration/activation_complete.html'),
-        name='registration_activation_complete'),
-    url(_(r'^activate/(?P<activation_key>\w+)/$'), ActivationView.as_view(), name='registration_activate'),
-    url(_(r'^register/closed/$'), TemplateView.as_view(template_name='registration/registration_closed.html'),
-        name='registration_disallowed'),
+    # accounts app
+    url(_(r'^account/'), include('apps.accounts.urls'), name='profile_update'),
 
     # test page
     url(r'^test/$', TemplateView.as_view(template_name='core/test.html'), name='test'),
 
     # login and logout
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'home'}, name='logout'),
 
     # langage switcher
