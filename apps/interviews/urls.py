bf97b7de238e4a9b197207e5655e8842f1822326
Jochen Klar <jklar@aip.de>
2016-01-18 16:39:33 +0100

    dummy CRUD for interview

--- apps/interviews/urls.py
+++ apps/interviews/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import interview_create
+from .views import interview, interview_create, interview_update, interview_delete
 
 urlpatterns = [
-    # edit own profile
+    url(r'^(?P<pk>[0-9]+)$', interview, name='interview'),
     url(r'^create$', interview_create, name='interview_create'),
+    url(r'^(?P<pk>[0-9]+)/update$', interview_update, name='interview_update'),
+    url(r'^(?P<pk>[0-9]+)/delete$', interview_delete, name='interview_delete'),
 ]
--- apps/interviews/views.py
+++ apps/interviews/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 from .models import Interview
 
 
-def interview_create(request):
+def interview(request, pk):
+    interview = Interview.objects.get(pk=pk)
+    return render(request, 'interviews/interview.html', {'interview': interview})
 
+
+def interview_create(request):
     return render(request, 'interviews/interview_create.html')
+
+
+def interview_update(request, pk):
+    return render(request, 'interviews/interview_update.html')
+
+
+def interview_delete(request, pk):
+    return render(request, 'interviews/interview_delete.html')
