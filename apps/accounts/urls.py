9b226953dcef2fc6a0d2d79beb2a68d15daa18d9
Jochen Klar <jklar@aip.de>
2016-01-18 18:02:20 +0100

    url translation refactored

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,40 +6,79 @@ from django.views.generic.base import TemplateView
 from registration.backends.default.views import RegistrationView, ActivationView
 
 urlpatterns = [
-    # edit own profile
+    # /accounts/
     url(r'^$', 'apps.accounts.views.profile_update', name='profile_update'),
 
-    # change password
-    url(_(r'^password/change/$'), auth_views.password_change, {
-        'template_name': 'accounts/password_change_form.html'
-        }, name='password_change'),
-    url(_(r'^password/change/done/$'), auth_views.password_change_done, {
-        'template_name': 'accounts/password_change_done.html'
-        }, name='password_change_done'),
+    # /accounts/password/change
+    url(r'^%s/%s/$' % (_('password'), _('change')),
+        auth_views.password_change,
+        {
+            'template_name': 'accounts/password_change_form.html'
+        },
+        name='password_change'),
 
-    # reset password
-    url(_(r'^password/reset/$'), auth_views.password_reset, {
-        'template_name': 'accounts/password_reset_form.html',
-        'email_template_name': 'accounts/password_reset_email.txt',
-        'subject_template_name': 'accounts/password_reset_subject.txt',
-        }, name='password_reset'),
-    url(_(r'^password/reset/done/$'), auth_views.password_reset_done, {
-        'template_name': 'accounts/password_reset_done.html',
-        }, name='password_reset_done'),
-    url(_(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'), auth_views.password_reset_confirm, {
-        'template_name': 'accounts/password_reset_confirm.html',
-        }, name='password_reset_confirm'),
-    url(_(r'^password/reset/complete/$'), auth_views.password_reset_complete, {
-        'template_name': 'accounts/password_reset_complete.html',
-        }, name='password_reset_complete'),
+    # /accounts/password/change/done
+    url(r'^%s/%s/%s/$' % (_('password'), _('change'), _('done')),
+        auth_views.password_change_done,
+        {
+            'template_name': 'accounts/password_change_done.html'
+        },
+        name='password_change_done'),
 
-    # user registration
-    url(_(r'^register/$'), RegistrationView.as_view(), name='registration_register'),
-    url(_(r'^register/complete/$'), TemplateView.as_view(template_name='registration/registration_complete.html'),
+    # /accounts/password/reset/
+    url(r'^%s/%s/$' % (_('password'), _('reset')),
+        auth_views.password_reset,
+        {
+            'template_name': 'accounts/password_reset_form.html',
+            'email_template_name': 'accounts/password_reset_email.txt',
+            'subject_template_name': 'accounts/password_reset_subject.txt',
+        },
+        name='password_reset'),
+
+    # /accounts/password/reset/done/
+    url(r'^%s/%s/%s/$' % (_('password'), _('reset'), _('done')),
+        auth_views.password_reset_done,
+        {
+            'template_name': 'accounts/password_reset_done.html'
+        },
+        name='password_reset_done'),
+
+    # /accounts/password/<uidb64>/<token>/
+    url(r'^%s/%s/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$' % (_('password'), _('reset')),
+        auth_views.password_reset_confirm,
+        {
+            'template_name': 'accounts/password_reset_confirm.html',
+        },
+        name='password_reset_confirm'),
+
+    # /accounts/password/reset/complete/
+    url(r'^%s/%s/%s/$' % (_('password'), _('reset'), _('complete')),
+        auth_views.password_reset_complete,
+        {
+            'template_name': 'accounts/password_reset_complete.html',
+        },
+        name='password_reset_complete'),
+
+    # /accounts/register/
+    url(r'^%s/$' % _('register'), RegistrationView.as_view(), name='registration_register'),
+
+    # /accounts/register/complete/
+    url(r'^%s/%s/$' % (_('register'), _('complete')),
+        TemplateView.as_view(template_name='registration/registration_complete.html'),
         name='registration_complete'),
-    url(_(r'^activate/complete/$'), TemplateView.as_view(template_name='registration/activation_complete.html'),
+
+    # /accounts/activate/complete/
+    url(r'^%s/%s/$' % (_('activate'), _('complete')),
+        TemplateView.as_view(template_name='registration/activation_complete.html'),
         name='registration_activation_complete'),
-    url(_(r'^activate/(?P<activation_key>\w+)/$'), ActivationView.as_view(), name='registration_activate'),
-    url(_(r'^register/closed/$'), TemplateView.as_view(template_name='registration/registration_closed.html'),
+
+    # /accounts/activate/<activation_key>/
+    url(r'^%s/(?P<activation_key>\w+)/$' % _('activate'),
+        ActivationView.as_view(),
+        name='registration_activate'),
+
+    # /accounts/register/closed/
+    url(r'^%s/%s/$' % (_('register'), _('closed')),
+        TemplateView.as_view(template_name='registration/registration_closed.html'),
         name='registration_disallowed'),
 ]
--- apps/interviews/urls.py
+++ apps/interviews/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
 
 from .views import interview, interview_create, interview_update, interview_delete
 
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)$', interview, name='interview'),
-    url(r'^create$', interview_create, name='interview_create'),
-    url(r'^(?P<pk>[0-9]+)/update$', interview_update, name='interview_update'),
-    url(r'^(?P<pk>[0-9]+)/delete$', interview_delete, name='interview_delete'),
+    url(r'^%s$' % _('create'), interview_create, name='interview_create'),
+    url(r'^(?P<pk>[0-9]+)/%s$' % _('update'), interview_update, name='interview_update'),
+    url(r'^(?P<pk>[0-9]+)/%s$' % _('delete'), interview_delete, name='interview_delete'),
 ]
