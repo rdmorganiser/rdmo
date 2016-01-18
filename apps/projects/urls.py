9e8845b0a9616c6d5a8d31093cd6fc0da79a74fc
Jochen Klar <jklar@aip.de>
2016-01-18 16:39:33 +0100

    dummy CRUD for interview

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from .views import projects, project, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
 
 urlpatterns = [
-    # edit own profile
     url(r'^$', projects, name='projects'),
     url(r'^(?P<pk>[0-9]+)$', project, name='project'),
     url(r'^create$', ProjectCreateView.as_view(), name='project_create'),
