672c7c2f5e14a35edfada477b054cf02a12d7b1b
Jochen Klar <jklar@aip.de>
2016-01-19 12:19:44 +0100

    new translations

--- apps/interviews/urls.py
+++ apps/interviews/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
 
 from .views import interview, interview_create, interview_update, interview_delete
 
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)$', interview, name='interview'),
-    url(r'^create$', interview_create, name='interview_create'),
-    url(r'^(?P<pk>[0-9]+)/update$', interview_update, name='interview_update'),
-    url(r'^(?P<pk>[0-9]+)/delete$', interview_delete, name='interview_delete'),
+    url(_(r'^create$'), interview_create, name='interview_create'),
+    url(_(r'^(?P<pk>[0-9]+)/update$'), interview_update, name='interview_update'),
+    url(_(r'^(?P<pk>[0-9]+)/delete$'), interview_delete, name='interview_delete'),
 ]
