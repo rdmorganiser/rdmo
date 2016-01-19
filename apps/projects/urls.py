7aadb8ca385892be7c8e1910606fdb30dd126684
Jochen Klar <jklar@aip.de>
2016-01-19 12:19:44 +0100

    new translations

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
 
 from .views import projects, project, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
 
 urlpatterns = [
     url(r'^$', projects, name='projects'),
     url(r'^(?P<pk>[0-9]+)$', project, name='project'),
-    url(r'^create$', ProjectCreateView.as_view(), name='project_create'),
-    url(r'^(?P<pk>[0-9]+)/update$', ProjectUpdateView.as_view(), name='project_update'),
-    url(r'^(?P<pk>[0-9]+)/delete$', ProjectDeleteView.as_view(), name='project_delete'),
+    url(_(r'^create$'), ProjectCreateView.as_view(), name='project_create'),
+    url(_(r'^(?P<pk>[0-9]+)/update$'), ProjectUpdateView.as_view(), name='project_update'),
+    url(_(r'^(?P<pk>[0-9]+)/delete$'), ProjectDeleteView.as_view(), name='project_delete'),
 ]
