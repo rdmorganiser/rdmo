57e485534a7be05fa1e542e078f45d26d6e4fa07
Jochen Klar <jklar@aip.de>
2016-02-19 17:46:49 +0100

    migrations reset

--- apps/projects/migrations/0001_initial.py
+++ apps/projects/migrations/0001_initial.py
@@ -2,29 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Project',
-            fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=256)),
-                ('pi', models.CharField(max_length=256)),
-                ('description', models.TextField(blank=True)),
-                ('owner', models.ManyToManyField(to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-                'verbose_name': 'Project',
-                'verbose_name_plural': 'Projects',
-                'ordering': ('name',),
-            },
-        ),
     ]
