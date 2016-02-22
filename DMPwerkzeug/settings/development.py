b73100366e430425c4119f26fb112ccdeeda1964
Jochen Klar <jklar@aip.de>
2016-02-22 15:44:21 +0100

    graph_models

---
+++ DMPwerkzeug/settings/development.py
@@ -0,0 +1,38 @@
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['localhost']
+
+DEVELOPMENT_APPS = (
+    'django_extensions',
+)
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': '',
+#     }
+# }
