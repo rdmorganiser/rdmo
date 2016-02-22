106a56d5b7ebe416487c7bcb0ae138f2387db4f5
Jochen Klar <jklar@aip.de>
2016-02-22 15:44:21 +0100

    graph_models

---
+++ DMPwerkzeug/settings/production.py
@@ -0,0 +1,57 @@
+import os
+
+# SECRET_KEY = 'this is not a very secret key'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': '',
+#     }
+# }
+
+# LANGUAGE_CODE = 'en-us'
+
+# TIME_ZONE = 'Europe/Berlin'
+
+# MEDIA_URL = '/media/'
+# STATIC_URL = '/static/'
+
+# LOGIN_URL = '/login/'
+# LOGIN_REDIRECT_URL = '/'
+# LOGOUT_URL = '/logout/'
+
+# ACCOUNT_ACTIVATION_DAYS = 7
+# REGISTRATION_EMAIL_HTML = False
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = '25'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
