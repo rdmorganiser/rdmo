4779d6d3cec70bdd244906a2d2f25cedc9a57518
Jochen Klar <jklar@aip.de>
2016-03-18 17:15:25 +0100

    more tests for apps.accounts

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,28 +1,33 @@
 import re
 
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.core import mail
 from django.core.urlresolvers import reverse
 from django.utils import translation
 
+from apps.core.tests import TestModelStringMixin
+
 from .models import DetailKey, Profile
 
 
-class ClientTestCase(TestCase):
+class AccountsTestCase(TestCase):
     fixtures = ['core/testing.json', 'accounts/testing.json']
+
+
+class ProfileTests(TestModelStringMixin, AccountsTestCase):
 
     def setUp(self):
         translation.activate('en')
+        self.instance = Profile.objects.get(user__username='admin')
 
     def test_get_profile_update(self):
         """
         An authorized GET request to the profile update form returns the form.
         """
-        client = Client()
-        client.login(username='user', password='user')
+        self.client.login(username='user', password='user')
 
         url = reverse('profile_update')
-        response = client.get(url)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_get_profile_update_redirect(self):
@@ -30,10 +35,8 @@ class ClientTestCase(TestCase):
         An unauthorized GET request to the profile update form gets
         redirected to login.
         """
-        client = Client()
-
         url = reverse('profile_update')
-        response = client.get(url)
+        response = self.client.get(url)
         self.assertRedirects(response, reverse('login') + '?next=' + url)
 
     def test_post_profile_update(self):
@@ -41,11 +44,10 @@ class ClientTestCase(TestCase):
         An authorized POST request to the profile update form updates the
         user and redirects to home.
         """
-        client = Client()
-        client.login(username='user', password='user')
+        self.client.login(username='user', password='user')
 
         url = reverse('profile_update')
-        response = client.post(url, {
+        response = self.client.post(url, {
             'email': 'test@example.com',
             'first_name': 'Albert',
             'last_name': 'Admin'
@@ -57,11 +59,10 @@ class ClientTestCase(TestCase):
         An authorized POST request to the profile update form updates with
         cancel redirects to home.
         """
-        client = Client()
-        client.login(username='user', password='user')
+        self.client.login(username='user', password='user')
 
         url = reverse('profile_update')
-        response = client.post(url, {
+        response = self.client.post(url, {
             'email': 'test@example.com',
             'first_name': 'Albert',
             'last_name': 'Admin',
@@ -74,11 +75,10 @@ class ClientTestCase(TestCase):
         An authorized POST request to the profile update form updates with
         cancel and the next field redirects to the given url.
         """
-        client = Client()
-        client.login(username='user', password='user')
+        self.client.login(username='user', password='user')
 
         url = reverse('profile_update')
-        response = client.post(url, {
+        response = self.client.post(url, {
             'email': 'test@example.com',
             'first_name': 'Albert',
             'last_name': 'Admin',
@@ -92,11 +92,10 @@ class ClientTestCase(TestCase):
         An authorized POST request to the profile update form with next field
         updates the user and redirects to the given url.
         """
-        client = Client()
-        client.login(username='user', password='user')
+        self.client.login(username='user', password='user')
 
         url = reverse('profile_update')
-        response = client.post(url, {
+        response = self.client.post(url, {
             'email': 'test@example.com',
             'first_name': 'Albert',
             'last_name': 'Admin',
@@ -115,11 +114,10 @@ class ClientTestCase(TestCase):
         An authorized POST request to the profile update form with next
         field set to profile_update updates the user and redirects to home.
         """
-        client = Client()
-        client.login(username='user', password='user')
+        self.client.login(username='user', password='user')
 
         url = reverse('profile_update')
-        response = client.post(url, {
+        response = self.client.post(url, {
             'email': 'test@example.com',
             'first_name': 'Albert',
             'last_name': 'Admin',
@@ -133,64 +131,69 @@ class ClientTestCase(TestCase):
         })
         self.assertRedirects(response, reverse('home'), target_status_code=302)
 
-    def test_get_password_change(self):
+
+class DetailKeyTests(TestModelStringMixin, AccountsTestCase):
+
+    def setUp(self):
+        translation.activate('en')
+        self.instance = DetailKey.objects.get(key='text')
+
+
+class PasswordTests(AccountsTestCase):
+
+    def setUp(self):
+        translation.activate('en')
+
+    def test_password_change_get(self):
         """
         An authorized GET request to the password change form returns the form.
         """
-        client = Client()
-        client.login(username='user', password='user')
+        self.client.login(username='user', password='user')
 
         url = reverse('password_change')
-        response = client.get(url)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_post_password_change(self):
+    def test_password_change_post(self):
         """
         An authorized POST request to the password change form updates the
         password and redirects to home.
         """
-        client = Client()
-        client.login(username='user', password='user')
+        self.client.login(username='user', password='user')
 
         url = reverse('password_change')
-        response = client.post(url, {
+        response = self.client.post(url, {
             'old_password': 'user',
             'new_password1': 'resu',
             'new_password2': 'resu',
         })
         self.assertRedirects(response, reverse('password_change_done'))
 
-    def test_get_password_reset(self):
+    def test_password_reset_get(self):
         """
         A GET request to the password reset form returns the form.
         """
-        client = Client()
-
         url = reverse('password_reset')
-        response = client.get(url)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_post_password_reset_invalid(self):
+    def test_password_reset_post_invalid(self):
         """
         A POST request to the password reset form with an invalid mail address
         sends no mail.
         """
-        client = Client()
-
         url = reverse('password_reset')
-        response = client.post(url, {'email': 'wrong@example.com'})
+        response = self.client.post(url, {'email': 'wrong@example.com'})
         self.assertRedirects(response, reverse('password_reset_done'))
         self.assertEqual(len(mail.outbox), 0)
 
-    def test_post_password_reset_valid(self):
+    def test_password_reset_post_valid(self):
         """
         A POST request to the password reset form with an invalid mail address
         sends a mail with a correct link.
         """
-        client = Client()
-
         url = reverse('password_reset')
-        response = client.post(url, {'email': 'user@example.com'})
+        response = self.client.post(url, {'email': 'user@example.com'})
         self.assertRedirects(response, reverse('password_reset_done'))
         self.assertEqual(len(mail.outbox), 1)
 
@@ -199,20 +202,5 @@ class ClientTestCase(TestCase):
         self.assertEqual(len(urls), 1)
 
         # get the password_reset page
-        response = client.get(urls[0])
+        response = self.client.get(urls[0])
         self.assertEqual(response.status_code, 200)
-
-
-class ModelTestCase(TestCase):
-    fixtures = ['accounts/testing.json']
-
-    def setUp(self):
-        translation.activate('en')
-
-    def test_profile_str(self):
-        profile = Profile.objects.get(user__username='admin')
-        self.assertEqual(profile.user.username, profile.__str__())
-
-    def test_detail_key_str(self):
-        detail_key = DetailKey(key='test', label='Test', type='text', required=True)
-        self.assertEqual('test', detail_key.__str__())
