14cb3ffd3422965eb76644adaa2f6258e67e932e
Jochen Klar <jklar@aip.de>
2016-03-18 17:00:19 +0100

    cleanup

--- apps/projects/tests.py
+++ apps/projects/tests.py
@@ -1,21 +1,28 @@
-from django.test import TestCase, Client
-from django.core.urlresolvers import reverse
+from django.test import TestCase
 from django.utils import translation
 
-from apps.core.tests import TestListViewMixin, TestRetrieveViewMixin
-from apps.core.tests import TestCreateViewMixin, TestUpdateViewMixin, TestDeleteViewMixin
-from apps.core.tests import TestModelStringMixin
+from apps.core.tests import \
+    TestListViewMixin, \
+    TestRetrieveViewMixin, \
+    TestCreateViewMixin, \
+    TestUpdateViewMixin, \
+    TestDeleteViewMixin, \
+    TestModelStringMixin
 
 from .models import Project
 
 
 class ProjectsTestCase(TestCase):
-    fixtures = ['accounts/testing.json', 'domain/testing.json', 'questions/testing.json']
+    fixtures = ['accounts/testing.json', 'domain/testing.json', 'questions/testing.json', 'projects/testing.json']
 
 
-class ProjectTests(TestListViewMixin, TestRetrieveViewMixin,
-                   TestCreateViewMixin, TestUpdateViewMixin, TestDeleteViewMixin,
-                   TestModelStringMixin, ProjectsTestCase):
+class ProjectTests(TestListViewMixin,
+                   TestRetrieveViewMixin,
+                   TestCreateViewMixin,
+                   TestUpdateViewMixin,
+                   TestDeleteViewMixin,
+                   TestModelStringMixin,
+                   ProjectsTestCase):
 
     list_url_name = 'projects'
     retrieve_url_name = 'project'
@@ -28,3 +35,15 @@ class ProjectTests(TestListViewMixin, TestRetrieveViewMixin,
         translation.activate('en')
         self.client.login(username='user', password='user')
         self.instance = Project.objects.get(title='Project')
+
+    def test_model_owner_string(self):
+        self.assertIsNotNone(self.instance.owner_string())
+
+
+class SnapshotTests(TestModelStringMixin,
+                    ProjectsTestCase):
+
+    def setUp(self):
+        translation.activate('en')
+        self.client.login(username='user', password='user')
+        self.instance = Project.objects.get(title='Project').current_snapshot
