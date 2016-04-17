73820f373503f5e7ec5903b990995ee9d1d019b6
Jochen Klar <jochenklar@gmail.com>
2016-04-17 21:32:16 +0200

    add test for questions url

--- apps/questions/tests.py
+++ apps/questions/tests.py
@@ -1,4 +1,17 @@
 from django.test import TestCase
+from django.utils import translation
+
+from apps.core.tests import TestListViewMixin
+
 
 class QuestionsTestCase(TestCase):
     fixtures = ['accounts/testing.json', 'domain/testing.json', 'questions/testing.json']
+
+
+class QuestionsTests(TestListViewMixin, QuestionsTestCase):
+
+    list_url_name = 'questions'
+
+    def setUp(self):
+        translation.activate('en')
+        self.client.login(username='user', password='user')
