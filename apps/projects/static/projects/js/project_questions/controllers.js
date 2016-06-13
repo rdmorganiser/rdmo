d26c61ff36fa27f7a711a02d071ce6deaf4e8eda
Jochen Klar <jklar@aip.de>
2016-06-13 18:44:43 +0200

    refactor checkboxes in project_questions angular app

--- apps/projects/static/projects/js/project_questions/controllers.js
+++ apps/projects/static/projects/js/project_questions/controllers.js
@@ -14,31 +14,10 @@ angular.module('project_questions')
         }
     };
 
-    // $scope.checkCheckbox = function(event) {
-    //     var checkbox = angular.element('input[type="checkbox"]', angular.element(event.target).parent())[0];
-    //     if (checkbox.checked !== true) {
-    //         checkbox.click();
-    //     }
-    // };
-
-    // $scope.changeCheckbox = function(value, options) {
-    //     if (angular.isDefined(value)) {
-    //         var text = [];
-    //         angular.forEach(value.checkbox, function(key) {
-    //             var option = $filter('filter')(options, {'key': key})[0];
-
-    //             if (option.input_field) {
-    //                 if (angular.isUndefined(value.input)) {
-    //                     value.input = {};
-    //                 }
-    //                 text.push(value.input[option.key]);
-    //             } else {
-    //                 text.push(option.text);
-    //             }
-    //         });
-
-    //         value.key = JSON.stringify(value.checkbox);
-    //         value.text = JSON.stringify(text);
-    //     }
-    // };
+    $scope.checkCheckbox = function(value, option_id) {
+        var checkbox = angular.element('input[type="checkbox"]', angular.element(event.target).parent())[0];
+        if (checkbox.checked !== true) {
+            checkbox.click();
+        }
+    };
 }]);
