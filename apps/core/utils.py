909044a03f18c0fdb4c4378d7c76ed5f5895d25a
Jochen Klar <jklar@aip.de>
2016-06-15 12:10:21 +0200

    add pdf export for questions

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -1,5 +1,15 @@
+import cStringIO as StringIO
+from xhtml2pdf import pisa
+
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+
+from cgi import escape
+
 from django.core.urlresolvers import reverse, resolve, Resolver404
 from django.utils.six.moves.urllib.parse import urlparse
+
 
 
 def get_script_alias(request):
@@ -35,3 +45,20 @@ def get_internal_link(text, name, *args, **kwargs):
         text = url
 
     return "<a href=\"%s\">%s</a>" % (url, text)
+
+
+def render_to_pdf(template_src, context_dict):
+    '''
+    taken from http://stackoverflow.com/questions/1377446/render-html-to-pdf-in-django-site
+    '''
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html = template.render(context)
+    result = StringIO.StringIO()
+
+    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
+
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    else:
+        return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
