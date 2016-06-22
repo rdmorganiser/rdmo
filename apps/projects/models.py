5d3545924021b155b269db52580b084cb1d2642b
Jochen Klar <jklar@aip.de>
2016-06-22 17:19:23 +0200

    fix i18n

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -17,11 +17,11 @@ class Project(Model):
 
     owner = models.ManyToManyField(User)
 
-    title = models.CharField(max_length=256)
-    description = models.TextField(blank=True, help_text=_('You can use markdown syntax in the description.'))
+    title = models.CharField(max_length=256, verbose_name=_('title'))
+    description = models.TextField(blank=True, help_text=_('You can use markdown syntax in the description.'), verbose_name=_('description'))
 
     current_snapshot = models.ForeignKey('Snapshot', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
-    catalog = models.ForeignKey(Catalog, related_name='+', help_text=_('The catalog which will be used for this project.'))
+    catalog = models.ForeignKey(Catalog, related_name='+', help_text=_('The catalog which will be used for this project.'), verbose_name=_('catalog'))
 
     class Meta:
         ordering = ('title', )
