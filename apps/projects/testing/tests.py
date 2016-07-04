24d7ad572bb5436537c2fe4f017c292020490b21
Jochen Klar <jklar@aip.de>
2016-07-04 16:31:02 +0200

    refactor testing

--- apps/projects/testing/tests.py
+++ apps/projects/testing/tests.py
@@ -1,19 +1,13 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
 from django.utils import translation
 
-from apps.core.test_mixins import *
+from apps.core.testing.mixins import *
+from apps.accounts.testing.factories import UserFactory
 
 from .factories import ProjectFactory
 
 
-class ProjectsTestCase(TestCase):
-    fixtures = [
-        'testing/accounts.json'
-    ]
-
-
-class ProjectTests(TestModelViewMixin, TestModelStringMixin, ProjectsTestCase):
+class ProjectTests(TestModelViewMixin, TestModelStringMixin, TestCase):
 
     list_url_name = 'projects'
     retrieve_url_name = 'project'
@@ -26,18 +20,22 @@ class ProjectTests(TestModelViewMixin, TestModelStringMixin, ProjectsTestCase):
 
     def setUp(self):
         translation.activate('en')
+
+        user = UserFactory()
         self.client.login(username='user', password='user')
-        user = User.objects.get(username='user')
+
         self.instance = ProjectFactory.create(owner=[user])
 
     def test_model_owner_string(self):
         self.assertIsNotNone(self.instance.owner_string())
 
 
-class SnapshotTests(TestModelStringMixin, ProjectsTestCase):
+class SnapshotTests(TestModelStringMixin, TestCase):
 
     def setUp(self):
         translation.activate('en')
+
+        user = UserFactory()
         self.client.login(username='user', password='user')
-        user = User.objects.get(username='user')
+
         self.instance = ProjectFactory.create(owner=[user]).current_snapshot
--- apps/questions/testing/tests.py
+++ apps/questions/testing/tests.py
@@ -1,81 +1,95 @@
 from django.test import TestCase
 from django.utils import translation
 
-from apps.core.test_mixins import *
+from apps.core.testing.mixins import *
+from apps.accounts.testing.factories import AdminFactory
 
 from ..models import *
 from .factories import *
 
 
-class QuestionsTestCase(TestCase):
-    fixtures = [
-        'testing/accounts.json'
-    ]
-
-
-class QuestionsTests(TestListViewMixin, QuestionsTestCase):
+class QuestionsTests(TestListViewMixin, TestCase):
 
     list_url_name = 'questions'
 
     def setUp(self):
         translation.activate('en')
+
+        AdminFactory()
         self.client.login(username='admin', password='admin')
 
 
-class CatalogTests(TestModelAPIViewMixin, QuestionsTestCase):
+class CatalogTests(TestModelAPIViewMixin, TestCase):
 
     api_url_name = 'questions:catalog'
 
     def setUp(self):
         translation.activate('en')
+
+        AdminFactory()
         self.client.login(username='admin', password='admin')
+
         self.instance = CatalogFactory()
 
 
-class SectionTests(TestModelAPIViewMixin, QuestionsTestCase):
+class SectionTests(TestModelAPIViewMixin, TestCase):
 
     api_url_name = 'questions:section'
 
     def setUp(self):
         translation.activate('en')
+
+        AdminFactory()
         self.client.login(username='admin', password='admin')
+
         self.instance = SectionFactory()
 
 
-class SubsectionTests(TestModelAPIViewMixin, QuestionsTestCase):
+class SubsectionTests(TestModelAPIViewMixin, TestCase):
 
     api_url_name = 'questions:subsection'
 
     def setUp(self):
         translation.activate('en')
+
+        AdminFactory()
         self.client.login(username='admin', password='admin')
+
         self.instance = SubsectionFactory()
 
 
-class QuestionEntityTests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, QuestionsTestCase):
+class QuestionEntityTests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, TestCase):
 
     api_url_name = 'questions:entity'
 
     def setUp(self):
         translation.activate('en')
+
+        AdminFactory()
         self.client.login(username='admin', password='admin')
+
         self.instance = QuestionEntityFactory()
 
 
-class QuestionTests(TestModelAPIViewMixin, QuestionsTestCase):
+class QuestionTests(TestModelAPIViewMixin, TestCase):
 
     api_url_name = 'questions:question'
 
     def setUp(self):
         translation.activate('en')
+
+        AdminFactory()
         self.client.login(username='admin', password='admin')
+
         self.instance = QuestionFactory()
 
 
-class WidgetTypeTests(TestListAPIViewMixin, QuestionsTestCase):
+class WidgetTypeTests(TestListAPIViewMixin, TestCase):
 
     api_url_name = 'questions:widgettype'
 
     def setUp(self):
         translation.activate('en')
+
+        AdminFactory()
         self.client.login(username='admin', password='admin')
