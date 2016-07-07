9c8f61bebd43ef911531f44698b7c0bab32299f8
Jochen Klar <jklar@aip.de>
2016-07-07 17:45:18 +0200

    refactor domain_export

--- apps/domain/urls.py
+++ apps/domain/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     url(r'^$', domain, name='domain'),
     url(r'^export/$', domain_export, name='domain_export'),
+    url(r'^export/(?P<format>[a-z]+)/$', domain_export, name='domain_export'),
 ]
