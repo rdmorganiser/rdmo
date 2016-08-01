94548d5d00879857079df494a9b9d51b12036fdb
Jochen Klar <jklar@aip.de>
2016-08-01 18:09:17 +0200

    add csv export to domain

--- apps/domain/urls.py
+++ apps/domain/urls.py
@@ -5,5 +5,6 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', domain, name='domain'),
+    url(_(r'^export/csv/$'), domain_export_csv, name='domain_export_csv'),
     url(_(r'^export/(?P<format>[a-z]+)/$'), domain_export, name='domain_export'),
 ]
