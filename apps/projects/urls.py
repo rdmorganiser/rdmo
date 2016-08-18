b79012995352cc9fc933f363d325d5aab88f259b
Jochen Klar <jklar@aip.de>
2016-08-18 15:59:04 +0200

    update i18n

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     url(_(r'^(?P<pk>[0-9]+)/update/$'), ProjectUpdateView.as_view(), name='project_update'),
     url(_(r'^(?P<pk>[0-9]+)/delete/$'), ProjectDeleteView.as_view(), name='project_delete'),
 
-    url(_(r'^(?P<pk>[0-9]+)/summary/$'), project_answers, name='project_answers'),
-    url(_(r'^(?P<pk>[0-9]+)/summary/export/(?P<format>[a-z]+)/$'), project_answers_export, name='project_answers_export'),
+    url(_(r'^(?P<pk>[0-9]+)/answers/$'), project_answers, name='project_answers'),
+    url(_(r'^(?P<pk>[0-9]+)/answers/export/(?P<format>[a-z]+)/$'), project_answers_export, name='project_answers_export'),
 
     url(_(r'^(?P<project_id>[0-9]+)/view/(?P<view_id>[0-9]+)/$'), project_view, name='project_view'),
     url(_(r'^(?P<project_id>[0-9]+)/view/(?P<view_id>[0-9]+)/export/(?P<format>[a-z]+)/$'), project_view_export, name='project_view_export'),
