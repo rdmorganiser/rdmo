10a9378b58d7ec157d10a2fc2d85881adb22dca9
Jochen Klar <jochenklar@gmail.com>
2016-09-15 18:15:56 +0200

    remove i18n urls

--- rdmo/urls.py
+++ rdmo/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import include, url
-from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
@@ -11,13 +10,13 @@ urlpatterns = [
     url(r'^$', home, name='home'),
 
     # apps
-    url(_(r'^account/'), include('apps.accounts.urls')),
-    url(_(r'^domain/'), include('apps.domain.urls')),
-    url(_(r'^projects/'), include('apps.projects.urls')),
-    url(_(r'^questions/'), include('apps.questions.urls')),
-    url(_(r'^tasks/'), include('apps.tasks.urls')),
-    url(_(r'^conditions/'), include('apps.conditions.urls')),
-    url(_(r'^views/'), include('apps.views.urls')),
+    url(r'^account/', include('apps.accounts.urls')),
+    url(r'^domain/', include('apps.domain.urls')),
+    url(r'^projects/', include('apps.projects.urls')),
+    url(r'^questions/', include('apps.questions.urls')),
+    url(r'^tasks/', include('apps.tasks.urls')),
+    url(r'^conditions/', include('apps.conditions.urls')),
+    url(r'^views/', include('apps.views.urls')),
 
     # REST api
     url(r'^api/domain/', include('apps.domain.urls_api', namespace='domain')),
