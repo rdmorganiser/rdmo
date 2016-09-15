744934445e2b72eaf74a827199684505e935240b
Jochen Klar <jochenklar@gmail.com>
2016-09-15 18:15:56 +0200

    remove i18n urls

--- apps/conditions/urls.py
+++ apps/conditions/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from django.utils.translation import ugettext_lazy as _
 
 from .views import *
 
 urlpatterns = [
     url(r'^$', conditions, name='conditions'),
-    url(_(r'^export/(?P<format>[a-z]+)/$'), conditions_export, name='conditions_export'),
+    url(r'^export/(?P<format>[a-z]+)/$', conditions_export, name='conditions_export'),
 ]
--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.utils.translation import ugettext_lazy as _
 
 from .views import *
 
@@ -7,15 +6,15 @@ urlpatterns = [
     url(r'^$', projects, name='projects'),
     url(r'^(?P<pk>[0-9]+)/$', project, name='project'),
 
-    url(_(r'^create/$'), ProjectCreateView.as_view(), name='project_create'),
-    url(_(r'^(?P<pk>[0-9]+)/update/$'), ProjectUpdateView.as_view(), name='project_update'),
-    url(_(r'^(?P<pk>[0-9]+)/delete/$'), ProjectDeleteView.as_view(), name='project_delete'),
+    url(r'^create/$', ProjectCreateView.as_view(), name='project_create'),
+    url(r'^(?P<pk>[0-9]+)/update/$', ProjectUpdateView.as_view(), name='project_update'),
+    url(r'^(?P<pk>[0-9]+)/delete/$', ProjectDeleteView.as_view(), name='project_delete'),
 
-    url(_(r'^(?P<pk>[0-9]+)/answers/$'), project_answers, name='project_answers'),
-    url(_(r'^(?P<pk>[0-9]+)/answers/export/(?P<format>[a-z]+)/$'), project_answers_export, name='project_answers_export'),
+    url(r'^(?P<pk>[0-9]+)/answers/$', project_answers, name='project_answers'),
+    url(r'^(?P<pk>[0-9]+)/answers/export/(?P<format>[a-z]+)/$', project_answers_export, name='project_answers_export'),
 
-    url(_(r'^(?P<project_id>[0-9]+)/view/(?P<view_id>[0-9]+)/$'), project_view, name='project_view'),
-    url(_(r'^(?P<project_id>[0-9]+)/view/(?P<view_id>[0-9]+)/export/(?P<format>[a-z]+)/$'), project_view_export, name='project_view_export'),
+    url(r'^(?P<project_id>[0-9]+)/view/(?P<view_id>[0-9]+)/$', project_view, name='project_view'),
+    url(r'^(?P<project_id>[0-9]+)/view/(?P<view_id>[0-9]+)/export/(?P<format>[a-z]+)/$', project_view_export, name='project_view_export'),
 
-    url(_(r'^(?P<project_id>[0-9]+)/questions/'), project_questions, name='project_questions'),
+    url(r'^(?P<project_id>[0-9]+)/questions/', project_questions, name='project_questions'),
 ]
