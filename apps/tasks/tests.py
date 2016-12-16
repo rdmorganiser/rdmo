b41dc7a803b2d6b2417c9e378a40c90d0925be00
Jochen Klar <jklar@aip.de>
2016-12-16 14:59:58 +0100

    refactor tests for tasks django app

---
+++ apps/tasks/tests.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+from django.utils import translation
+
+from apps.core.testing.mixins import *
+
+from apps.conditions.models import Condition
+
+from .models import *
+
+
+class TasksTestCase(TestCase):
+
+    fixtures = (
+        'conditions.json',
+        'domain.json',
+        'options.json',
+        'tasks.json'
+    )
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+
+
+class TasksTests(TestListViewMixin, TasksTestCase):
+
+    list_url_name = 'tasks'
+
+    def setUp(self):
+        translation.activate('en')
+        self.client.login(username='admin', password='admin')
+
+
+class TaskTests(TestModelAPIViewMixin, TasksTestCase):
+
+    api_url_name = 'tasks:task'
+
+    def setUp(self):
+        translation.activate('en')
+        self.client.login(username='admin', password='admin')
+        self.instances = Task.objects.all()
+
+    def prepare_create_instance(self, instance):
+        instance.identifier += '_new'
+        return instance
+
+
+class ConditionTests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, TasksTestCase):
+
+    api_url_name = 'tasks:condition'
+
+    def setUp(self):
+        translation.activate('en')
+        self.client.login(username='admin', password='admin')
+        self.instances = Condition.objects.all()
