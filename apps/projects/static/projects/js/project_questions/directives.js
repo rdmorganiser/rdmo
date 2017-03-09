6fdd6cb005ebcbb2c212a154b58743fd0e6437b5
Jochen Klar <jklar@aip.de>
2017-03-09 15:02:11 +0100

    fix directives in project_questions angular app

--- apps/projects/static/projects/js/project_questions/directives.js
+++ apps/projects/static/projects/js/project_questions/directives.js
@@ -1,65 +1,70 @@
 angular.module('project_questions')
 
-.directive('input', ['$timeout', function($timeout) {
+.directive('rangeInput', ['$timeout', function($timeout) {
     return {
-        restrict: 'E',
+        restrict: 'C',
         require: 'ngModel',
         link: function(scope, element, attrs, ngModelController) {
-            if (attrs.type === 'range') {
-                if (attrs.minValue && attrs.maxValue) {
-                    ngModelController.$parsers.push(function(val) {
-                        var min = parseFloat(attrs.minValue),
-                            max = parseFloat(attrs.maxValue);
+            if (attrs.minValue && attrs.maxValue) {
+                ngModelController.$parsers.push(function(val) {
+                    var min = parseFloat(attrs.minValue),
+                        max = parseFloat(attrs.maxValue);
 
-                        return 0.01 * (max - min) * (parseFloat(val) + min);
-                    });
-                    ngModelController.$formatters.push(function(val) {
-                        var min = parseFloat(attrs.minValue),
-                            max = parseFloat(attrs.maxValue);
-
-                        return 100.0 / (max - min) * (parseFloat(val) + min);
-                    });
-                }
-            } else if ($(element).hasClass('date')) {
-
-                ngModelController.$parsers.push(function(view_value) {
-
-                    if (view_value === '') {
-                        return '';
-                    }
-
-                    if (attrs.language === 'de') {
-                        return moment(view_value, 'DD.MM.YYYY').format();
-                    } else {
-                        return moment(view_value).format();
-                    }
-
+                    return 0.01 * (max - min) * (parseFloat(val) + min);
                 });
+                ngModelController.$formatters.push(function(val) {
+                    var min = parseFloat(attrs.minValue),
+                        max = parseFloat(attrs.maxValue);
 
-                ngModelController.$formatters.push(function(model_value) {
-
-                    if (model_value === '') {
-                        return '';
-                    }
-
-                    if (attrs.language === 'de') {
-                        return moment(model_value).format('DD.MM.YYYY');
-                    } else {
-                        return moment(model_value).format('MM/DD/YYYY');
-                    }
-
-                });
-
-                $timeout(function() {
-                    $('.date').datepicker({
-                        autoclose: true,
-                        clearBtn: true,
-                        orientation: 'bottom',
-                        language: attrs.language
-                    });
-                    $('.date:focus').datepicker('show');
+                    return 100.0 / (max - min) * (parseFloat(val) + min);
                 });
             }
+        }
+    };
+}])
+
+.directive('dateInput', ['$timeout', function($timeout) {
+    return {
+        restrict: 'C',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModelController) {
+            ngModelController.$parsers.push(function(view_value) {
+
+                if (view_value === '') {
+                    return '';
+                }
+
+                if (attrs.language === 'de') {
+                    return moment(view_value, 'DD.MM.YYYY').format();
+                } else {
+                    return moment(view_value).format();
+                }
+
+            });
+
+            ngModelController.$formatters.push(function(model_value) {
+
+                if (model_value === '') {
+                    return '';
+                }
+
+                if (attrs.language === 'de') {
+                    return moment(model_value).format('DD.MM.YYYY');
+                } else {
+                    return moment(model_value).format('MM/DD/YYYY');
+                }
+
+            });
+
+            $timeout(function() {
+                $('.date').datepicker({
+                    autoclose: true,
+                    clearBtn: true,
+                    orientation: 'bottom',
+                    language: attrs.language
+                });
+                $('.date:focus').datepicker('show');
+            });
         }
     };
 }]);
