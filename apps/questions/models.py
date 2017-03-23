14983a64fe21dc01f9eb450a2f188a23882dbe80
Jochen Klar <jklar@aip.de>
2017-03-23 16:34:58 +0100

    clear cache on changes in projects/questions

--- apps/questions/models.py
+++ apps/questions/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.core.cache import cache
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
@@ -317,6 +318,9 @@ class QuestionEntity(Model, TranslationMixin):
 
         for question in self.questions.all():
             question.save()
+
+        # invalidate the cache so that changes appear instantly
+        cache.clear()
 
     def clean(self):
         try:
