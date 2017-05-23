49bf691e0254fc2cbd4149d61a6ce7b4f3c3a78b
Jochen Klar <jklar@aip.de>
2017-05-23 10:45:16 +0200

    refactor questions tests

--- apps/questions/tests.py
+++ apps/questions/tests.py
@@ -1,14 +1,11 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from test_mixins.views import TestListViewMixin
+from test_mixins.viewsets import TestModelViewsetMixin, TestListViewsetMixin, TestRetrieveViewsetMixin
+
+from apps.core.testing.mixins import TestImportViewMixin
 from apps.accounts.utils import set_group_permissions
-from apps.core.testing.mixins import (
-    TestListViewMixin,
-    TestImportViewMixin,
-    TestModelAPIViewMixin,
-    TestListAPIViewMixin,
-    TestRetrieveAPIViewMixin
-)
 
 from .models import Catalog, Section, Subsection, QuestionEntity, Question
 
@@ -36,16 +33,27 @@ class QuestionsTestCase(TestCase):
     )
 
     status_map = {
-        'list': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 302},
-        'export': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 302}
-    }
-
-    api_status_map = {
-        'list': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 403},
-        'retrieve': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 403},
-        'create': {'editor': 201, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403},
-        'update': {'editor': 200, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403},
-        'delete': {'editor': 204, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403}
+        'list_view': {
+            'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 302
+        },
+        'export_view': {
+            'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 302
+        },
+        'list_viewset': {
+            'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 403
+        },
+        'retrieve_viewset': {
+            'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 403
+        },
+        'create_viewset': {
+            'editor': 201, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403
+        },
+        'update_viewset': {
+            'editor': 200, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403
+        },
+        'delete_viewset': {
+            'editor': 204, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403
+        }
     }
 
     def setUp(self):
@@ -55,81 +63,86 @@ class QuestionsTestCase(TestCase):
 class QuestionsTests(TestListViewMixin, QuestionsTestCase):
 
     url_names = {
-        'list': 'catalogs'
+        'list_view': 'catalogs'
     }
 
 
-class CatalogTests(TestModelAPIViewMixin, QuestionsTestCase):
+class CatalogTests(TestModelViewsetMixin, QuestionsTestCase):
 
     instances = Catalog.objects.all()
-
-    api_url_name = 'internal-questions:catalog'
+    url_names = {
+        'viewset': 'internal-questions:catalog'
+    }
 
     def prepare_create_instance(self, instance):
         instance.key += '_new'
         return instance
 
 
-class SectionTests(TestModelAPIViewMixin, QuestionsTestCase):
+class SectionTests(TestModelViewsetMixin, QuestionsTestCase):
 
     instances = Section.objects.all()
-
-    api_url_name = 'internal-questions:section'
+    url_names = {
+        'viewset': 'internal-questions:section'
+    }
 
     def prepare_create_instance(self, instance):
         instance.key += '_new'
         return instance
 
 
-class SubsectionTests(TestModelAPIViewMixin, QuestionsTestCase):
+class SubsectionTests(TestModelViewsetMixin, QuestionsTestCase):
 
     instances = Subsection.objects.all()
-
-    api_url_name = 'internal-questions:subsection'
+    url_names = {
+        'viewset': 'internal-questions:subsection'
+    }
 
     def prepare_create_instance(self, instance):
         instance.key += '_new'
         return instance
 
 
-class QuestionSetTests(TestModelAPIViewMixin, QuestionsTestCase):
+class QuestionSetTests(TestModelViewsetMixin, QuestionsTestCase):
 
     instances = QuestionEntity.objects.filter(question=None)
-
-    api_url_name = 'internal-questions:questionset'
+    url_names = {
+        'viewset': 'internal-questions:questionset'
+    }
 
     def prepare_create_instance(self, instance):
         instance.key += '_new'
         return instance
 
 
-class QuestionTests(TestModelAPIViewMixin, QuestionsTestCase):
+class QuestionTests(TestModelViewsetMixin, QuestionsTestCase):
 
     instances = Question.objects.all()
-
-    api_url_name = 'internal-questions:question'
+    url_names = {
+        'viewset': 'internal-questions:question'
+    }
 
     def prepare_create_instance(self, instance):
         instance.key += '_new'
         return instance
 
 
-class WidgetTypeTests(TestListAPIViewMixin, QuestionsTestCase):
+class WidgetTypeTests(TestListViewsetMixin, QuestionsTestCase):
 
-    api_url_name = 'internal-questions:widgettype'
-    api_status_map = {
-        'list': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 200, 'anonymous': 403}
+    url_names = {
+        'viewset': 'internal-questions:widgettype'
+    }
+    status_map = {
+        'list_viewset': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 200, 'anonymous': 403}
     }
 
 
 class CatalogExportTests(QuestionsTestCase):
 
     instances = Catalog.objects.all()
-
     url_names = {
-        'export': 'questions_catalog_export'
+        'export_view': 'questions_catalog_export'
     }
-
     export_formats = ('xml', 'html', 'rtf')
 
     def test_export_detail(self):
@@ -140,14 +153,14 @@ class CatalogExportTests(QuestionsTestCase):
 
             for instance in self.instances:
                 for format in self.export_formats:
-                    url = reverse(self.url_names['export'], kwargs={
+                    url = reverse(self.url_names['export_view'], kwargs={
                         'pk': instance.pk,
                         'format': format
                     })
                     response = self.client.get(url)
 
                     try:
-                        self.assertEqual(response.status_code, self.status_map['export'][username])
+                        self.assertEqual(response.status_code, self.status_map['export_view'][username])
                     except AssertionError:
                         print(
                             ('test', 'test_export'),
@@ -167,36 +180,41 @@ class CatalogImportTests(TestImportViewMixin, TestCase):
     import_file = 'testing/xml/catalog.xml'
 
 
-class CatalogAPITests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, QuestionsTestCase):
+class CatalogAPITests(TestListViewsetMixin, TestRetrieveViewsetMixin, QuestionsTestCase):
 
     instances = Catalog.objects.all()
+    url_names = {
+        'viewset': 'api-v1-questions:catalog'
+    }
 
-    api_url_name = 'api-v1-questions:catalog'
 
-
-class SectionAPITests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, QuestionsTestCase):
+class SectionAPITests(TestListViewsetMixin, TestRetrieveViewsetMixin, QuestionsTestCase):
 
     instances = Section.objects.all()
+    url_names = {
+        'viewset': 'api-v1-questions:section'
+    }
 
-    api_url_name = 'api-v1-questions:section'
 
-
-class SubsectionAPITests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, QuestionsTestCase):
+class SubsectionAPITests(TestListViewsetMixin, TestRetrieveViewsetMixin, QuestionsTestCase):
 
     instances = Subsection.objects.all()
+    url_names = {
+        'viewset': 'api-v1-questions:subsection'
+    }
 
-    api_url_name = 'api-v1-questions:subsection'
 
-
-class QuestionSetAPITests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, QuestionsTestCase):
+class QuestionSetAPITests(TestListViewsetMixin, TestRetrieveViewsetMixin, QuestionsTestCase):
 
     instances = QuestionEntity.objects.filter(question=None)
+    url_names = {
+        'viewset': 'api-v1-questions:questionset'
+    }
 
-    api_url_name = 'api-v1-questions:questionset'
 
-
-class QuestionAPITests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, QuestionsTestCase):
+class QuestionAPITests(TestListViewsetMixin, TestRetrieveViewsetMixin, QuestionsTestCase):
 
     instances = Question.objects.all()
-
-    api_url_name = 'api-v1-questions:question'
+    url_names = {
+        'viewset': 'api-v1-questions:question'
+    }
