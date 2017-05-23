fcdfa231c0a0f78f50f0305d8bf2b0d64adf73ae
Jochen Klar <jklar@aip.de>
2017-05-23 10:41:52 +0200

    refactor tasks test

--- apps/tasks/tests.py
+++ apps/tasks/tests.py
@@ -1,15 +1,10 @@
 from django.test import TestCase
 
-from apps.accounts.utils import set_group_permissions
-from apps.core.testing.mixins import (
-    TestListViewMixin,
-    TestExportViewMixin,
-    TestImportViewMixin,
-    TestModelAPIViewMixin,
-    TestListAPIViewMixin,
-    TestRetrieveAPIViewMixin
-)
+from test_mixins.views import TestListViewMixin
+from test_mixins.viewsets import TestModelViewsetMixin, TestListViewsetMixin, TestRetrieveViewsetMixin
 
+from apps.core.testing.mixins import TestExportViewMixin, TestImportViewMixin
+from apps.accounts.utils import set_group_permissions
 from apps.conditions.models import Condition
 
 from .models import Task
@@ -38,16 +33,27 @@ class TasksTestCase(TestCase):
     )
 
     status_map = {
-        'list': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 302},
-        'export': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 302}
-    }
-
-    api_status_map = {
-        'list': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 403},
-        'retrieve': {'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 403},
-        'create': {'editor': 201, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403},
-        'update': {'editor': 200, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403},
-        'delete': {'editor': 204, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403}
+        'list_view': {
+            'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 302
+        },
+        'export_view': {
+            'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 302
+        },
+        'list_viewset': {
+            'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 403
+        },
+        'retrieve_viewset': {
+            'editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 403
+        },
+        'create_viewset': {
+            'editor': 201, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403
+        },
+        'update_viewset': {
+            'editor': 200, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403
+        },
+        'delete_viewset': {
+            'editor': 204, 'reviewer': 403, 'api': 403, 'user': 403, 'anonymous': 403
+        }
     }
 
     def setUp(self):
@@ -57,32 +63,34 @@ class TasksTestCase(TestCase):
 class TasksTests(TestListViewMixin, TasksTestCase):
 
     url_names = {
-        'list': 'tasks'
+        'list_view': 'tasks'
     }
 
 
-class TaskTests(TestModelAPIViewMixin, TasksTestCase):
+class TaskTests(TestModelViewsetMixin, TasksTestCase):
 
     instances = Task.objects.all()
-
-    api_url_name = 'internal-tasks:task'
+    url_names = {
+        'viewset': 'internal-tasks:task'
+    }
 
     def prepare_create_instance(self, instance):
         instance.key += '_new'
         return instance
 
 
-class ConditionTests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, TasksTestCase):
+class ConditionTests(TestListViewsetMixin, TestRetrieveViewsetMixin, TasksTestCase):
 
     instances = Condition.objects.all()
-
-    api_url_name = 'internal-tasks:condition'
+    url_names = {
+        'viewset': 'internal-tasks:condition'
+    }
 
 
 class TasksExportTests(TestExportViewMixin, TasksTestCase):
 
     url_names = {
-        'export': 'tasks_export'
+        'export_view': 'tasks_export'
     }
 
 
@@ -91,8 +99,9 @@ class TasksImportTests(TestImportViewMixin, TestCase):
     import_file = 'testing/xml/tasks.xml'
 
 
-class TaskAPITests(TestListAPIViewMixin, TestRetrieveAPIViewMixin, TasksTestCase):
+class TaskAPITests(TestListViewsetMixin, TestRetrieveViewsetMixin, TasksTestCase):
 
     instances = Task.objects.all()
-
-    api_url_name = 'api-v1-tasks:task'
+    url_names = {
+        'viewset': 'api-v1-tasks:task'
+    }
