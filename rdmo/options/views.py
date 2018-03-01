2bf8c58a0254c87fea7da560acf9483f58a2f001
Olaf Michaelis <omichaelis@aip.de>
2018-03-01 12:00:22 +0100

    Fix options import, multiple improvements

--- rdmo/options/views.py
+++ rdmo/options/views.py
@@ -1,14 +1,23 @@
+import logging
+
 from django.conf import settings
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import TemplateView, ListView
 
+from rdmo.core.imports import handle_uploaded_file, validate_xml
 from rdmo.core.views import ModelPermissionMixin
 from rdmo.core.utils import get_model_field_meta, render_to_format
 
+from .forms import UploadFileForm
+from .imports import import_options
 from .models import OptionSet, Option
 from .serializers.export import OptionSetSerializer as ExportSerializer
 from .renderers import XMLRenderer
+
+log = logging.getLogger(__name__)
 
 
 class OptionsView(ModelPermissionMixin, TemplateView):
@@ -39,3 +48,31 @@ class OptionsExportView(ModelPermissionMixin, ListView):
             return response
         else:
             return render_to_format(self.request, format, _('Options'), 'options/options_export.html', context)
+
+
+class OptionsImportXMLView(ModelPermissionMixin, ListView):
+    permission_required = 'projects.export_project_object'
+    success_url = '/options'
+    template_name = 'options/file_upload.html'
+
+    def get(self, request, *args, **kwargs):
+        form = UploadFileForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        # context = self.get_context_data(**kwargs)
+        tempfilename = handle_uploaded_file(request.FILES['uploaded_file'])
+        # TODO: improve validation function
+        exit_code, xmltree = validate_xml(tempfilename, 'options')
+        if exit_code == 0:
+            import_options(xmltree)
+            return HttpResponseRedirect(self.success_url)
+        else:
+            log.info('Xml parsing error. Import failed.')
+            return HttpResponse('Xml parsing error. Import failed.')
+
+    def form_valid(self, form, request, *args, **kwargs):
+        form.save(commit=True)
+        messages.success(request, 'File uploaded!')
+        # return super(ProjectImportXMLView, self).form_valid(form)
+        return
