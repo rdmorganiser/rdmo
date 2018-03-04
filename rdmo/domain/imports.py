603712db0855cded0d9b4495b4d8e99fb2bdc536
olaf michaelis <o.mic@web.de>
2018-03-04 11:36:48 +0100

    Refactore domain import But still having problems with parents

--- rdmo/domain/imports.py
+++ rdmo/domain/imports.py
@@ -17,17 +17,15 @@ def import_domain(domain_node):
         if entity_node.tag == 'entity':
             import_attribute_entity(entity_node, nsmap)
         elif entity_node.tag == 'attribute':
+        # else:
             import_attribute(entity_node, nsmap)
 
 
 def import_attribute_entity(entity_node, nsmap, parent=None):
     uri = entity_node.find(get_ns_tag('dc:uri', nsmap)).text
-    log.info('Importing attribute_entity with uri "' + uri + '"...')
-
     try:
         entity = AttributeEntity.objects.get(uri=uri, parent=parent)
     except AttributeEntity.DoesNotExist:
-        log.info('Attribute entity import: ' + str(AttributeEntity.DoesNotExist))
         entity = AttributeEntity()
 
     entity.parent = parent
@@ -35,14 +33,14 @@ def import_attribute_entity(entity_node, nsmap, parent=None):
     entity.key = uri.split('/')[-1]
     entity.comment = get_value_from_xml_node(entity_node, get_ns_tag('dc:comment', nsmap))
     entity.is_collection = entity_node.find('is_collection') == 'True'
-    log.info('Saving entity ' + str(entity))
+    log.info('Saving entity: "' + str(uri) + '", parent "' + str(parent) + '"...')
     entity.save()
 
     if entity_node.find('verbosename').text is not None:
         import_verbose_name(get_value_from_xml_node(entity_node, 'verbosename'), entity)
 
     if entity_node.find('conditions') is not None:
-        for condition_node in entity_node.find('conditions').findall('condition'):
+        for condition_node in entity_node.find('conditions').iter():
             try:
                 condition_uri = condition_node.get(get_ns_tag('dc:uri', nsmap))
                 condition = Condition.objects.get(uri=condition_uri)
@@ -51,59 +49,55 @@ def import_attribute_entity(entity_node, nsmap, parent=None):
                 log.info('Condition import failed: ' + str(Condition.DoesNotExist))
                 pass
 
-    for child_node in entity_node.findall('children'):
+    log.info('Processing children...')
+    for child_node in entity_node.find('children').iter():
         if child_node.tag == 'entity':
             import_attribute_entity(child_node, nsmap, parent=entity)
-        else:
+        elif child_node.tag == 'attribute':
             import_attribute(child_node, nsmap, parent=entity)
 
 
 def import_attribute(attribute_node, nsmap, parent=None):
+    uri = ''
+    uri = attribute_node.find(get_ns_tag('dc:uri', nsmap)).text
     try:
-        uri = attribute_node.find(get_ns_tag('dc:uri', nsmap)).text
-    except Exception as e:
-        log.error(e)
-    else:
-        log.info('Importing attribute with uri "' + uri + '"...')
+        attribute = Attribute.objects.get(uri=uri)
+    except Attribute.DoesNotExist:
+        attribute = Attribute()
 
-        try:
-            attribute = Attribute.objects.get(uri=uri)
-        except Attribute.DoesNotExist:
-            attribute = Attribute()
+    attribute.parent = parent
+    attribute.uri_prefix = uri.split('/domain/')[0]
+    attribute.key = uri.split('/')[-1]
+    attribute.comment = get_value_from_xml_node(attribute_node, get_ns_tag('dc:comment', nsmap))
+    attribute.is_collection = attribute_node.find('is_collection') == 'True'
+    attribute.value_type = get_value_from_xml_node(attribute_node, 'value_type')
+    attribute.unit = get_value_from_xml_node(attribute_node, 'unit')
+    log.info('Saving attribute. Uri: ' + str(uri) + ', parent: ' + str(parent))
+    attribute.save()
 
-        attribute.parent = parent
-        attribute.uri_prefix = uri.split('/domain/')[0]
-        attribute.key = uri.split('/')[-1]
-        attribute.comment = get_value_from_xml_node(attribute_node, get_ns_tag('dc:comment', nsmap))
-        attribute.is_collection = attribute_node.find('is_collection') == 'True'
-        attribute.value_type = get_value_from_xml_node(attribute_node, 'value_type')
-        attribute.unit = get_value_from_xml_node(attribute_node, 'unit')
-        log.info('Saving attribute ' + str(attribute))
-        attribute.save()
+    if hasattr(attribute_node, 'range'):
+        import_verbose_name(attribute_node.range, attribute)
 
-        if hasattr(attribute_node, 'range'):
-            import_verbose_name(attribute_node.range, attribute)
+    if hasattr(attribute_node, 'verbosename'):
+        import_verbose_name(attribute_node.verbosename, attribute)
 
-        if hasattr(attribute_node, 'verbosename'):
-            import_verbose_name(attribute_node.verbosename, attribute)
+    if hasattr(attribute_node, 'optionsets'):
+        for optionset_node in attribute_node.optionsets.iterchildren():
+            try:
+                optionset_uri = optionset_node.get(get_ns_tag('dc:uri', nsmap))
+                optionset = OptionSet.objects.get(uri=optionset_uri)
+                attribute.optionsets.add(optionset)
+            except OptionSet.DoesNotExist:
+                pass
 
-        if hasattr(attribute_node, 'optionsets'):
-            for optionset_node in attribute_node.optionsets.iterchildren():
-                try:
-                    optionset_uri = optionset_node.get(get_ns_tag('dc:uri', nsmap))
-                    optionset = OptionSet.objects.get(uri=optionset_uri)
-                    attribute.optionsets.add(optionset)
-                except OptionSet.DoesNotExist:
-                    pass
-
-        if hasattr(attribute_node, 'conditions'):
-            for condition_node in attribute_node.conditions.iterchildren():
-                try:
-                    condition_uri = condition_node.get(get_ns_tag('dc:uri', nsmap))
-                    condition = Condition.objects.get(uri=condition_uri)
-                    attribute.conditions.add(condition)
-                except Condition.DoesNotExist:
-                    pass
+    if hasattr(attribute_node, 'conditions'):
+        for condition_node in attribute_node.conditions.iterchildren():
+            try:
+                condition_uri = condition_node.get(get_ns_tag('dc:uri', nsmap))
+                condition = Condition.objects.get(uri=condition_uri)
+                attribute.conditions.add(condition)
+            except Condition.DoesNotExist:
+                pass
 
 
 def import_verbose_name(verbosename_node, entity):
@@ -121,3 +115,11 @@ def import_verbose_name(verbosename_node, entity):
     except Exception as e:
         log.info('An exception occured: ' + str(e))
         pass
+
+
+# NOTE: not used yet
+def find_rec(node, element, result):
+    for item in node.findall(element):
+        result.append(item)
+        find_rec(item, element, result)
+    return result
