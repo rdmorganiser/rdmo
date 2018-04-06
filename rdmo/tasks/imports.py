a9a9655b33e4e2af335519f855fe60f833bd8157
Olaf Michaelis <omichaelis@aip.de>
2018-04-06 13:52:59 +0200

    Add timeframe import

--- rdmo/tasks/imports.py
+++ rdmo/tasks/imports.py
@@ -42,6 +42,44 @@ def import_tasks(tasks_node):
         log.info('Task saving to "' + str(task_uri) + '"')
         task.save()
 
+        try:
+            timeframe = TimeFrame.objects.get(task=task)
+        except TimeFrame.DoesNotExist as e:
+            timeframe = TimeFrame()
+            timeframe.task = task
+            log.info('Timeframe not in db. Created with task uri ' + str(task.uri))
+        else:
+            log.info('Timeframe does exist. Loaded from task uri ' + str(task.uri))
+
+        timeframe_node = task_node.find('timeframe')
+        do_save = False
+        if timeframe_node.find('start_attribute') is not None:
+            try:
+                start_attribute_uri = timeframe_node.find('start_attribute').get(get_ns_tag('dc:uri', nsmap))
+                timeframe.start_attribute = Attribute.objects.get(uri=start_attribute_uri)
+            except Attribute.DoesNotExist:
+                pass
+
+        if timeframe_node.find('end_attribute') is not None:
+            try:
+                end_attribute_uri = timeframe_node.find('end_attribute').get(get_ns_tag('dc:uri', nsmap))
+                timeframe.end_attribute = Attribute.objects.get(uri=end_attribute_uri)
+            except Attribute.DoesNotExist:
+                pass
+
+        days_before = get_value_from_treenode(timeframe_node, 'days_before')
+        if days_before.isdigit() is True:
+            do_save = True
+            timeframe.days_before = get_value_from_treenode(timeframe_node, 'days_before')
+
+        days_after = get_value_from_treenode(timeframe_node, 'days_after')
+        if days_after.isdigit() is True:
+            do_save = True
+            timeframe.days_after = get_value_from_treenode(timeframe_node, 'days_after')
+
+        if do_save is True:
+            timeframe.save()
+
         if hasattr(task_node, 'conditions'):
             for condition_node in task_node.find('condition').findall('conditions'):
                 try:
