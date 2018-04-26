e3c5d4e37994aadf9bd2b147bf7d3429eee87f14
Olaf Michaelis <omichaelis@aip.de>
2018-04-26 16:05:29 +0200

    Release 0.9.5

--- rdmo/__init__.py
+++ rdmo/__init__.py
@@ -1,8 +1,8 @@
 __title__ = 'rdmo'
-__version__ = '0.9.3'
+__version__ = '0.9.5'
 __author__ = 'Jochen Klar'
 __email__ = 'jklar@aip.de'
 __license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2015-2017 Leibniz Institute for Astrophysics Potsdam (AIP)'
+__copyright__ = 'Copyright 2015-2018 Leibniz Institute for Astrophysics Potsdam (AIP)'
 
 VERSION = __version__
