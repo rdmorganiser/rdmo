f02d8e1de0125cba64f82198fcedb87ba87bfd43
Jochen Klar <jochenklar@gmail.com>
2018-07-31 12:43:21 +0200

    Fix view_tags

--- rdmo/views/templatetags/view_tags.py
+++ rdmo/views/templatetags/view_tags.py
@@ -4,21 +4,36 @@ register = template.Library()
 
 
 def get_values(context, attribute_path, set_index='*', index='*'):
-    try:
-        values = context['values'][attribute_path]
-    except KeyError:
-        return []
+    if set_index == '*' and index == '*':
+        try:
+            return context['values'][attribute_path]
+        except KeyError:
+            return []
 
-    if set_index == '*':
-        if index == '*':
-            return values
-        else:
-            return [value_set[index] for value_set in values]
+    elif set_index == '*':
+        try:
+            value_sets = context['values'][attribute_path]
+        except KeyError:
+            return []
+
+        values = []
+        for value_set in value_sets:
+            try:
+                values.append(value_set[index])
+            except IndexError:
+                values.append('')
+        return values
+
+    elif index == '*':
+        try:
+            return context['values'][attribute_path][set_index]
+        except (KeyError, IndexError):
+            return []
     else:
-        if index == '*':
-            return values[set_index]
-        else:
-            return values[set_index][index]
+        try:
+            return context['values'][attribute_path][set_index][index]
+        except (KeyError, IndexError):
+            return ''
 
 
 @register.simple_tag(takes_context=True)
