e9f95aabcf710f9c91eeef4783c745cab9f2f7cb
Jochen Klar <jklar@aip.de>
2018-08-02 17:21:34 +0200

    Fix migration

--- rdmo/questions/migrations/0022_data_migration.py
+++ rdmo/questions/migrations/0022_data_migration.py
@@ -8,15 +8,25 @@ def set_questionentity_is_collection(apps, schema_editor):
     QuestionEntity = apps.get_model('questions', 'QuestionEntity')
 
     for queryset in QuestionEntity.objects.filter(question=None):
-        if queryset.attribute_entity.parent_collection:
-            queryset.attribute_entity = queryset.attribute_entity.parent_collection
-            queryset.is_collection = True
-        else:
-            queryset.is_collection = queryset.attribute_entity.is_collection
+
+        try:
+            if queryset.attribute_entity.parent_collection:
+                queryset.attribute_entity = queryset.attribute_entity.parent_collection
+                queryset.is_collection = True
+            else:
+                queryset.is_collection = queryset.attribute_entity.is_collection
+        except AttributeError:
+            queryset.is_collection = False
+
         queryset.save()
 
     for question in QuestionEntity.objects.exclude(question=None):
-        question.is_collection = question.attribute_entity.is_collection
+
+        try:
+            question.is_collection = question.attribute_entity.is_collection
+        except AttributeError:
+            queryset.is_collection = False
+
         question.save()
 
 
