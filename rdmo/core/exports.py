f98c5867e8380060fa476118d842a74dc26f89db
Olaf Michaelis <omichaelis@aip.de>
2018-08-24 13:17:07 +0200

    Resolve import conflict

--- rdmo/core/exports.py
+++ rdmo/core/exports.py
@@ -1,6 +1,8 @@
-import xml.dom.minidom
+from __future__ import absolute_import
+
+from xml.dom.minidom import parseString
 
 
 def prettify_xml(xmlstring):
-    xmlobj = xml.dom.minidom.parseString(xmlstring)
+    xmlobj = parseString(xmlstring)
     return xmlobj.toprettyxml()
