813bdf1449a086b8c719692bcc441e8ceae2442a
Olaf Michaelis <omichaelis@aip.de>
2019-01-08 10:39:36 +0100

    Prevent error when no catalog

--- rdmo/projects/utils.py
+++ rdmo/projects/utils.py
@@ -29,75 +29,80 @@ def get_answers_tree(project, snapshot=None):
     # then we loop over sections and questionsets to collect questions and answers
 
     sections = []
-    for catalog_section in project.catalog.sections.order_by('order'):
-        questionsets = []
-        for catalog_questionset in catalog_section.questionsets.order_by('order'):
+    try:
+        project.catalog.sections
+    except AttributeError:
+        pass
+    else:
+        for catalog_section in project.catalog.sections.order_by('order'):
+            questionsets = []
+            for catalog_questionset in catalog_section.questionsets.order_by('order'):
 
-            if catalog_questionset.attribute and catalog_questionset.is_collection:
+                if catalog_questionset.attribute and catalog_questionset.is_collection:
 
-                questions = []
-                for catalog_question in catalog_questionset.questions.order_by('order'):
+                    questions = []
+                    for catalog_question in catalog_questionset.questions.order_by('order'):
 
-                    # for a questionset collection loop over valuesets
-                    if catalog_questionset.attribute.id in valuesets:
+                        # for a questionset collection loop over valuesets
+                        if catalog_questionset.attribute.id in valuesets:
 
-                        sets = []
-                        for set_index in valuesets[catalog_questionset.attribute.id]:
-                            valueset = valuesets[catalog_questionset.attribute.id][set_index]
+                            sets = []
+                            for set_index in valuesets[catalog_questionset.attribute.id]:
+                                valueset = valuesets[catalog_questionset.attribute.id][set_index]
 
-                            # try to get the values for this question's attribute and set_index
-                            answers = get_answers(values, catalog_question.attribute.id, set_index)
+                                # try to get the values for this question's attribute and set_index
+                                answers = get_answers(values, catalog_question.attribute.id, set_index)
 
-                            if answers:
-                                sets.append({
-                                    'id': valueset,
-                                    'answers': answers
+                                if answers:
+                                    sets.append({
+                                        'id': valueset,
+                                        'answers': answers
+                                    })
+
+                            if sets:
+                                questions.append({
+                                    'sets': sets,
+                                    'text': catalog_question.text,
+                                    'attribute': catalog_question.attribute,
+                                    'is_collection': catalog_question.is_collection or catalog_question.widget_type == 'checkbox'
                                 })
 
-                        if sets:
+                    if questions:
+                        questionsets.append({
+                            'questions': questions,
+                            'attribute': catalog_questionset.attribute,
+                            'is_collection': True,
+                        })
+
+                else:
+                    # # for a questionset loop over questions
+                    questions = []
+                    for catalog_question in catalog_questionset.questions.order_by('order'):
+
+                        # try to get the values for this question's attribute
+                        answers = get_answers(values, catalog_question.attribute.id)
+
+                        if answers:
                             questions.append({
-                                'sets': sets,
                                 'text': catalog_question.text,
                                 'attribute': catalog_question.attribute,
+                                'answers': answers,
                                 'is_collection': catalog_question.is_collection or catalog_question.widget_type == 'checkbox'
                             })
 
-                if questions:
-                    questionsets.append({
-                        'questions': questions,
-                        'attribute': catalog_questionset.attribute,
-                        'is_collection': True,
-                    })
-
-            else:
-                # # for a questionset loop over questions
-                questions = []
-                for catalog_question in catalog_questionset.questions.order_by('order'):
-
-                    # try to get the values for this question's attribute
-                    answers = get_answers(values, catalog_question.attribute.id)
-
-                    if answers:
-                        questions.append({
-                            'text': catalog_question.text,
-                            'attribute': catalog_question.attribute,
-                            'answers': answers,
-                            'is_collection': catalog_question.is_collection or catalog_question.widget_type == 'checkbox'
+                    if questions:
+                        questionsets.append({
+                            'title': catalog_questionset.title,
+                            'questions': questions,
+                            'attribute': catalog_questionset.attribute,
+                            'is_collection': False
                         })
 
-                if questions:
-                    questionsets.append({
-                        'title': catalog_questionset.title,
-                        'questions': questions,
-                        'attribute': catalog_questionset.attribute,
-                        'is_collection': False
-                    })
-
-        if questionsets:
-            sections.append({
-                'title': catalog_section.title,
-                'questionsets': questionsets
-            })
+            if questionsets:
+                sections.append({
+                    'title': catalog_section.title,
+                    'questionsets': questionsets
+                })
 
     return {'sections': sections}
 
