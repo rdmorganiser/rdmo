73d12ed0e09c948e0a92cc2f4e14ff61326f38b2
Jochen Klar <mail@jochenklar.de>
2019-04-04 19:30:30 +0200

    Fix MySQL tests

--- testing/config/settings/mysql.py
+++ testing/config/settings/mysql.py
@@ -7,6 +7,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rdmo',
         'USER': 'root',
-        'PASSWORD': ''
+        'PASSWORD': '',
+        'TEST': {
+            'CHARSET': 'utf8',
+            'COLLATION': 'utf8_general_ci',
+        }
     }
 }
