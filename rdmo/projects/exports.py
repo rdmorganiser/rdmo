f19354f0ffd05871efeb856bb4f284f6009b9516
Jochen Klar <mail@jochenklar.de>
2020-06-05 17:42:01 +0200

    Add get_ functions to Export class

--- rdmo/projects/exports.py
+++ rdmo/projects/exports.py
@@ -1,7 +1,6 @@
 import re
 
 from django.http import HttpResponse
-
 from rdmo.core.exports import prettify_xml
 from rdmo.core.utils import render_to_csv
 
@@ -15,9 +14,59 @@ class Export(object):
     def __init__(self, project, snapshot=None):
         self.project = project
         self.snapshot = snapshot
+        self.values = self.project.values.filter(snapshot=self.snapshot)
 
     def render(self):
         raise NotImplementedError
+
+    def get_set(self, path):
+        return self.values.filter(attribute__path=path)
+
+    def get_values(self, path, set_index=0):
+        return self.values.filter(attribute__path=path, set_index=set_index)
+
+    def get_value(self, path, set_index=0, collection_index=0):
+        try:
+            return self.get_values(path, set_index)[collection_index]
+        except IndexError:
+            return None
+
+    def get_text(self, path, set_index=0, collection_index=0):
+        try:
+            return self.get_values(path, set_index)[collection_index].text
+        except IndexError:
+            return None
+
+    def get_timestamp(self, path, set_index=0, collection_index=0):
+        try:
+            return self.get_values(path, set_index)[collection_index].value.isoformat()
+        except (IndexError, AttributeError):
+            return None
+
+    def get_year(self, path, set_index=0, collection_index=0):
+        try:
+            return self.get_values(path, set_index)[collection_index].value.year
+        except (IndexError, AttributeError):
+            return None
+
+    def get_list(self, path, set_index=0):
+        values = self.get_values(path, set_index)
+        return [value.text for value in values if value.text]
+
+    def get_bool(self, path, set_index=0, collection_index=0):
+        value = self.get_value(path, set_index, collection_index)
+        if value:
+            return True if value.text == '1' else False
+        else:
+            return None
+
+    def get_option(self, options, path, set_index=0, collection_index=0, default=None):
+        value = self.get_value(path, set_index, collection_index)
+        if value and value.option:
+            # lookup option dict in class
+            return options.get(value.option.path, default)
+        else:
+            return default
 
 
 class CSVExport(Export):
