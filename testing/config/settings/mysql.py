b58421664cf8f7091219f458e93f7779f4ddbfc0
Jochen Klar <mail@jochenklar.de>
2020-06-30 16:46:37 +0200

    Fix mysql testing

--- testing/config/settings/mysql.py
+++ testing/config/settings/mysql.py
@@ -11,6 +11,9 @@ DATABASES = {
         'TEST': {
             'CHARSET': 'utf8',
             'COLLATION': 'utf8_general_ci',
+        },
+        'OPTIONS': {
+            'init_command': "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));"
         }
     }
 }
