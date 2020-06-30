cc8f6bea933096fc9402746549e4cf71892cadcf
olaf michaelis <o.mic@web.de>
2020-06-30 20:11:39 +0200

    Bump version number to 1.0.7

--- rdmo/__init__.py
+++ rdmo/__init__.py
@@ -1,8 +1,8 @@
 __title__ = 'rdmo'
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 __author__ = 'Jochen Klar'
 __email__ = 'jklar@aip.de'
 __license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2015-2018 Leibniz Institute for Astrophysics Potsdam (AIP)'
+__copyright__ = 'Copyright 2015-2020 Leibniz Institute for Astrophysics Potsdam (AIP)'
 
 VERSION = __version__
