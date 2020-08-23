f925c69901f2db277b65a778a137d0214da66d14
Jochen Klar <mail@jochenklar.de>
2020-08-23 10:40:43 +0200

    Add model copy tests

--- rdmo/questions/tests/test_models.py
+++ rdmo/questions/tests/test_models.py
@@ -13,6 +13,20 @@ def test_catalog_clean(db):
         instance.clean()
 
 
+def test_catalog_save(db):
+    instances = Catalog.objects.all()
+    for instance in instances:
+        new_uri_prefix = 'https://new.example.com/'
+        new_key = instance.key + '*'
+
+        new_instance = instance.copy(new_uri_prefix, instance.key + '*')
+        assert new_instance.uri_prefix == new_uri_prefix
+        assert new_instance.key == new_key
+        assert list(new_instance.sites.values('id')) == list(new_instance.sites.values('id'))
+        assert list(new_instance.groups.values('id')) == list(new_instance.groups.values('id'))
+        assert new_instance.sections.count() == instance.sections.count()
+
+
 def test_section_str(db):
     instances = Section.objects.all()
     for instance in instances:
@@ -23,6 +37,18 @@ def test_section_clean(db):
     instances = Section.objects.all()
     for instance in instances:
         instance.clean()
+
+
+def test_section_save(db):
+    instances = Section.objects.all()
+    for instance in instances:
+        new_uri_prefix = 'https://new.example.com/'
+        new_key = instance.key + '*'
+
+        new_instance = instance.copy(new_uri_prefix, instance.key + '*')
+        assert new_instance.uri_prefix == new_uri_prefix
+        assert new_instance.key == new_key
+        assert new_instance.questionsets.count() == instance.questionsets.count()
 
 
 def test_questionset_str(db):
@@ -37,6 +63,20 @@ def test_questionset_clean(db):
         instance.clean()
 
 
+def test_questionset_save(db):
+    instances = QuestionSet.objects.all()
+    for instance in instances:
+        new_uri_prefix = 'https://new.example.com/'
+        new_key = instance.key + '*'
+
+        new_instance = instance.copy(new_uri_prefix, instance.key + '*')
+        assert new_instance.uri_prefix == new_uri_prefix
+        assert new_instance.key == new_key
+        assert new_instance.attribute == instance.attribute
+        assert list(new_instance.conditions.values('id')) == list(new_instance.conditions.values('id'))
+        assert new_instance.questions.count() == instance.questions.count()
+
+
 def test_question_str(db):
     instances = Question.objects.all()
     for instance in instances:
@@ -47,3 +87,17 @@ def test_question_clean(db):
     instances = Question.objects.all()
     for instance in instances:
         instance.clean()
+
+
+def test_question_save(db):
+    instances = Question.objects.all()
+    for instance in instances:
+        new_uri_prefix = 'https://new.example.com/'
+        new_key = instance.key + '*'
+
+        new_instance = instance.copy(new_uri_prefix, instance.key + '*')
+        assert new_instance.uri_prefix == new_uri_prefix
+        assert new_instance.key == new_key
+        assert new_instance.attribute == instance.attribute
+        assert list(new_instance.conditions.values('id')) == list(new_instance.conditions.values('id'))
+        assert list(new_instance.optionsets.values('id')) == list(new_instance.optionsets.values('id'))
--- rdmo/views/tests/test_models.py
+++ rdmo/views/tests/test_models.py
@@ -11,3 +11,17 @@ def test_view_clean(db):
     instances = View.objects.all()
     for instance in instances:
         instance.clean()
+
+
+def test_view_save(db):
+    instances = View.objects.all()
+    for instance in instances:
+        new_uri_prefix = 'https://new.example.com/'
+        new_key = instance.key + '*'
+
+        new_instance = instance.copy(new_uri_prefix, instance.key + '*')
+        assert new_instance.uri_prefix == new_uri_prefix
+        assert new_instance.key == new_key
+        assert list(new_instance.catalogs.values('id')) == list(new_instance.catalogs.values('id'))
+        assert list(new_instance.sites.values('id')) == list(new_instance.sites.values('id'))
+        assert list(new_instance.groups.values('id')) == list(new_instance.groups.values('id'))
