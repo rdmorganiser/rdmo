c9251f7c251f2d2b8e7c128470e35c7d4817e113
Jochen Klar <mail@jochenklar.de>
2020-10-02 17:30:08 +0200

    Fix migrations

--- rdmo/projects/migrations/0041_value_external_id.py
+++ rdmo/projects/migrations/0041_value_external_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0038_integration_integrationoption'),
+        ('projects', '0040_issueresource'),
     ]
 
     operations = [
