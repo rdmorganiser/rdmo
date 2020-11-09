0d69d1320c3bb73f089a5d1f77590f0b10548c09
Jochen Klar <mail@jochenklar.de>
2020-11-09 16:13:08 +0100

    Fix tasks and views admin

--- rdmo/tasks/admin.py
+++ rdmo/tasks/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-
 from rdmo.core.utils import get_language_fields
 
 from .models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
-    search_fields = ['uri'] + get_language_fields('title') + get_language_fields('help')
+    search_fields = ['uri'] + get_language_fields('title') + get_language_fields('text')
     list_display = ('uri', 'title', 'text')
     readonly_fields = ('uri', )
 
