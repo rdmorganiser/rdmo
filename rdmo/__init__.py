7bb3841baf8d9f1465034cff2c389322fc1fd73e
Jochen Klar <mail@jochenklar.de>
2021-03-05 14:52:10 +0100

    Update metadata and NOTICE

--- rdmo/__init__.py
+++ rdmo/__init__.py
@@ -1,8 +1,7 @@
 __title__ = 'rdmo'
 __version__ = '1.5.3'
-__author__ = 'Jochen Klar'
-__email__ = 'jklar@aip.de'
+__author__ = 'RDMO Consortium'
+__email__ = 'rdmo-team@listserv.dfn.de'
 __license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2015-2020 Leibniz Institute for Astrophysics Potsdam (AIP)'
 
 VERSION = __version__
