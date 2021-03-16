55257f71f335c1e4ce59ea2b70be91db1627a44f
Jochen Klar <mail@jochenklar.de>
2021-03-16 17:03:48 +0100

    Update metadata and NOTICE

--- rdmo/__init__.py
+++ rdmo/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'rdmo'
 __version__ = '1.5.3'
-__author__ = 'RDMO Consortium'
+__author__ = 'RDMO Arbeitsgemeinschaft'
 __email__ = 'rdmo-team@listserv.dfn.de'
 __license__ = 'Apache-2.0'
 
