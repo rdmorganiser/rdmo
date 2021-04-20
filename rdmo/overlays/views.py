d85fee84be51686619b222308898e2fe9fcbfb6f
Jochen Klar <mail@jochenklar.de>
2021-04-20 19:03:47 +0200

    Update overlays

--- rdmo/overlays/views.py
+++ rdmo/overlays/views.py
@@ -3,6 +3,8 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
+from rdmo.core.utils import get_next, get_referer_path_info
+
 from .models import Overlay
 
 logger = logging.getLogger(__name__)
@@ -12,6 +14,8 @@ logger = logging.getLogger(__name__)
 def reset_overlays(request):
     if request.method == 'POST':
         Overlay.objects.filter(user=request.user).delete()
-        return redirect('projects')
+        return redirect(get_next(request))
 
-    return render(request, 'overlays/reset_overlays.html')
+    return render(request, 'overlays/reset_overlays.html', {
+        'next': get_referer_path_info(request, default='/')
+    })
