66234d271eedac7e4a77398ebbebf811d27fe93e
Jochen Klar <mail@jochenklar.de>
2021-07-19 13:32:06 +0200

    Add output to deploy managament script

--- rdmo/core/management/commands/deploy.py
+++ rdmo/core/management/commands/deploy.py
@@ -8,5 +8,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        print('>>> python manage.py collectstatic --noinput --clear')
+        print()
         call_command('collectstatic', '--noinput', '--clear')
+        print()
+
+        print('>>> touch config/wsgi.py')
+        print()
         subprocess.call(['touch', 'config/wsgi.py'])
+        print()
