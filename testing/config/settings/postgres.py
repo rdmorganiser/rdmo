18c40d2a6409d29d9471979cb2bf596ca1b96a3e
Max Schröder <max.schroeder@uni-rostock.de>
2021-10-21 21:14:00 +0200

    Implemented Github Actions for pytest

--- testing/config/settings/postgres.py
+++ testing/config/settings/postgres.py
@@ -6,6 +6,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'rdmo',
-        'USER': 'postgres'
+        'USER': 'postgres',
+        'PASSWORD': 'postgres_password',
+        'HOST': '127.0.0.1',
     }
 }
