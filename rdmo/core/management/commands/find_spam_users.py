71a1a4984c8191b665f73f0a7986e94b0323ce5b
olaf michaelis <o.mic@web.de>
2021-12-01 10:31:49 +0100

    Add cli arguments to find_spam_users

--- rdmo/core/management/commands/find_spam_users.py
+++ rdmo/core/management/commands/find_spam_users.py
@@ -1,5 +1,4 @@
 import csv
-import json
 import os
 
 from django.contrib.auth.models import User
@@ -7,81 +6,73 @@ from django.core.management.base import BaseCommand
 
 from rdmo.projects.models import Membership
 
-date_string = '%Y-%m-%dT%H:%M:%S.%f'
-filename_prefix = 'potential_spam_users_list'
-min_diff_seconds = 3
-min_group_size = 3
-
-
-def rm(filename):
-    if os.path.exists(filename):
-        os.remove(filename)
-
-
-def save_json(data):
-    filename = filename_prefix + '.json'
-    with open(filename, 'w') as outfile:
-        json.dump(data, outfile, indent=4, sort_keys=True)
-
-
-def save_txt(data):
-    filename = filename_prefix + '.txt'
-    rm(filename)
-    fil = open(filename, 'a')
-    for el in data:
-        group = data[el]
-        for user in group:
-            fil.write(str(user) + '\n')
-
-
-def save_csv(data):
-    filename = filename_prefix + '.csv'
-    rm(filename)
-    data_file = open(filename, 'w')
-    csv_writer = csv.writer(data_file)
-    for el in data:
-        group = data[el]
-        for user in group:
-            csv_writer.writerow(user.values())
-
-
-def get_users_having_projects():
-    arr = []
-    memberships = Membership.objects.all().values_list('user')
-    for mem in memberships:
-        arr.append(
-            User.objects.filter(id=mem[0]).values('id')[0]['id']
-        )
-    return arr
-
-
-def append_to_group(group_list, group_count, user, list_users_having_projects):
-    last_login = user['last_login']
-    if last_login is not None:
-        last_login = last_login.strftime(date_string)
-
-    has_project = user['id'] in list_users_having_projects
-    # if has_project is True:
-    #     no_users_having_projects += 1
-    group_list[group_count].append(
-        {
-            'id': user['id'],
-            'email': user['email'],
-            'username': user['username'],
-            'first_name': user['first_name'],
-            'last_name': user['last_name'],
-            'date_joined': user['date_joined'].strftime(date_string),
-            'last_login': last_login,
-            'has_project': has_project,
-        }
-    )
-    return group_list
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-o', '--output_file', default='potential_spam_users.csv',
+            help='output file, default is \'potential_spam_users.csv\''
+        )
+        parser.add_argument(
+            '-t', '--timespan', default=2, type=int,
+            help='timespan in seconds between two joining users, ' +
+            'less than the given value is considered to be suspicious ' +
+            ', default is 2'
+        )
+        parser.add_argument(
+            '-n', '--occurence', default=3, type=int,
+            help='number of sequentially occuring timespan ' +
+            'violations at which users are put into the ' +
+            'potential spam users list, default is 3'
+        )
+
+    def rm(self, filename):
+        if os.path.exists(filename):
+            os.remove(filename)
+
+    def save_csv(self, data, filename):
+        self.rm(filename)
+        data_file = open(filename, 'w')
+        csv_writer = csv.writer(data_file)
+        for el in data:
+            group = data[el]
+            for user in group:
+                csv_writer.writerow(user.values())
+        print('List written to ' + filename)
+
+    def get_users_having_projects(self):
+        arr = []
+        memberships = Membership.objects.all().values_list('user')
+        for mem in memberships:
+            arr.append(
+                User.objects.filter(id=mem[0]).values('id')[0]['id']
+            )
+        return arr
+
+    def append_to_group(self, group_list, group_count, user, list_users_having_projects):
+        date_string = '%Y-%m-%dT%H:%M:%S.%f'
+        last_login = user['last_login']
+        if last_login is not None:
+            last_login = last_login.strftime(date_string)
+
+        has_project = user['id'] in list_users_having_projects
+        group_list[group_count].append(
+            {
+                'id': user['id'],
+                'email': user['email'],
+                'username': user['username'],
+                'first_name': user['first_name'],
+                'last_name': user['last_name'],
+                'date_joined': user['date_joined'].strftime(date_string),
+                'last_login': last_login,
+                'has_project': has_project,
+            }
+        )
+        return group_list
 
     def handle(self, *args, **options):
 
-        list_users_having_projects = get_users_having_projects()
+        list_users_having_projects = self.get_users_having_projects()
         no_users_having_projects = 0
 
         arr = []
@@ -105,12 +96,12 @@ class Command(BaseCommand):
         group_count = 0
         for idx, user in enumerate(arr):
             prev = None
-            diff = min_diff_seconds
+            diff = options['timespan']
             if idx > 0:
                 prev = arr[idx-1]
                 diff = user['unix_joined'] - prev['unix_joined']
 
-            if prev is not None and diff >= min_diff_seconds:
+            if prev is not None and diff >= options['timespan']:
                 group_count += 1
 
             try:
@@ -118,7 +109,7 @@ class Command(BaseCommand):
             except KeyError:
                 grouped[group_count] = []
 
-            grouped = append_to_group(
+            grouped = self.append_to_group(
                 grouped, group_count, user, list_users_having_projects
             )
 
@@ -126,7 +117,7 @@ class Command(BaseCommand):
         grouped_clean = {}
         for group_id in grouped:
             group = grouped[group_id]
-            if len(group) > min_group_size:
+            if len(group) > options['occurence']:
                 no_potential_spam_users += len(group)
                 grouped_clean[group_id] = group
 
@@ -138,6 +129,5 @@ class Command(BaseCommand):
                 no_users_having_projects
             )
         )
-        save_json(grouped_clean)
-        save_txt(grouped_clean)
-        save_csv(grouped_clean)
+
+        self.save_csv(grouped_clean, options['output_file'])
