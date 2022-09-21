e35a6501c7804e4d2e4a31514228c09235ca41bd
David Wallace <david.wallace@tu-darmstadt.de>
2022-09-21 14:35:59 +0200

    refactor flat_xml_to_elements function

--- rdmo/core/xml.py
+++ rdmo/core/xml.py
@@ -20,37 +20,64 @@ def parse_xml_string(string):
         log.error('Xml parsing error: ' + str(e))
 
 
-def flat_xml_to_elements(treenode):
+def flat_xml_to_elements(treenode, sort_parent_nodes=True):
     elements = []
     ns_map = get_ns_map(treenode)
     uri_attrib = get_ns_tag('dc:uri', ns_map)
 
-    for node in treenode:
+    parent_nodes = get_parent_nodes(treenode)
+    
+    if sort_parent_nodes:
+        parent_nodes = sort_elements_by_key(parent_nodes, 'uri')
 
+    for node in parent_nodes:
         element = {
-            'uri': get_uri(node, ns_map),
-            'type': node.tag
+            'uri': node['uri'],
+            'type': node['type'],
         }
 
-        for subnode in node:
-            tag = strip_ns(subnode.tag, ns_map)
-
-            if uri_attrib in subnode.attrib:
-                # this node has an uri!
-                element[tag] = subnode.attrib[uri_attrib]
-            elif 'lang' in subnode.attrib:
-                # this node has the lang attribute!
-                element['%s_%s' % (tag, subnode.attrib['lang'])] = subnode.text
-            elif list(subnode):
-                # this node is a list!
-                element[tag] = [subsubnode.attrib[uri_attrib] for subsubnode in subnode]
-            else:
-                element[tag] = subnode.text
+        for subnode in node['node']:
+            tag, value = get_tag_and_value_from_subnode(subnode, ns_map, uri_attrib)
+            element[tag] = value
 
         elements.append(element)
-
-    elements = sorted(elements, key=lambda k: k.get('path', k.get('key')))
+        
     return elements
+
+def get_parent_nodes(treenode):
+    parent_nodes = []
+    ns_map = get_ns_map(treenode)
+
+    for node in treenode:
+
+        node_element = {
+            'uri': get_uri(node, ns_map),
+            'type': node.tag,
+            'node' : node
+        }
+        parent_nodes.append(node_element)
+
+    return parent_nodes
+
+    
+def get_tag_and_value_from_subnode(subnode, ns_map, uri_attrib):
+
+    # default tag
+    tag = strip_ns(subnode.tag, ns_map)
+    # default value
+    value = subnode.text
+
+    if uri_attrib in subnode.attrib:
+        # this node has an uri!
+        value = subnode.attrib[uri_attrib]
+    elif 'lang' in subnode.attrib:
+        # this node has the lang attribute!
+        tag = '%s_%s' % (tag, subnode.attrib['lang'])
+    elif list(subnode):
+        # this node is a list!
+        value =  [subsubnode.attrib[uri_attrib] for subsubnode in subnode]
+
+    return tag, value
 
 
 def get_ns_tag(tag, ns_map):
@@ -90,3 +117,6 @@ def filter_elements_by_type(elements, element_type):
     for element in elements:
         if element['type'] == element_type:
             yield element
+
+def sort_elements_by_key(dictlist, key, reverse=False):
+    return sorted(dictlist, key=lambda k: k[key], reverse=reverse)
