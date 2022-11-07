dfee4e28be7ecda413c0ab922efa3750bfdd00df
Jochen Klar <mail@jochenklar.de>
2022-11-07 17:31:53 +0100

    Add sass compilation to webpack and add JavaScriptCatalog for i18n in js

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -1,11 +1,19 @@
 const webpack = require('webpack')
-const path = require('path');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: {
-    management: './rdmo/management/assets/js/management.js'
+    management: [
+      './rdmo/management/assets/js/management.js',
+      './rdmo/management/assets/scss/management.scss'
+    ]
   },
   resolve: {
+    alias: {
+      rdmo: path.resolve(__dirname, '../rdmo/')
+    },
     extensions: ['*', '.js', '.jsx']
   },
   output: {
@@ -20,7 +28,25 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
         options: { presets: ['@babel/env','@babel/preset-react'] }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]/static/management/css/[name].css',
+      chunkFilename: '[name]/static/management/css/[id].css'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 }
