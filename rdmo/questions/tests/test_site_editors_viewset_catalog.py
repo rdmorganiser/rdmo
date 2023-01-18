3d3e6102f7d0cf9d1c1abe7ad65f84422f63b9fd
David Wallace <david.wallace@tu-darmstadt.de>
2023-01-18 10:19:15 +0100

    add questions/tests for site editors

---
+++ rdmo/questions/tests/test_site_editors_viewset_catalog.py
@@ -0,0 +1,273 @@
+import xml.etree.ElementTree as et
+
+import pytest
+from django.urls import reverse
+
+from ..models import Catalog
+
+users = (
+    ('editor', 'editor'),
+    ('reviewer', 'reviewer'),
+    ('user', 'user'),
+    ('api', 'api'),
+    ('anonymous', None),
+)
+
+
+status_map = {
+    'list': {
+        'editor': 200, 'foo-editor': 200, 'bar-editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 401
+    },
+    'detail': {
+        'editor': 200, 'foo-editor': 200, 'bar-editor': 200, 'reviewer': 200, 'api': 200, 'user': 403, 'anonymous': 401
+    },
+    'create': {
+        'editor': 201, 'foo-editor': 201, 'bar-editor': 201, 'reviewer': 403, 'api': 201, 'user': 403, 'anonymous': 401
+    },
+    'update': {
+        'editor': 200, 'reviewer': 403, 'api': 200, 'user': 403, 'anonymous': 401
+    },
+    'delete': {
+        'editor': 204, 'reviewer': 403, 'api': 204, 'user': 403, 'anonymous': 401
+    }
+}
+
+multisite_editor = (
+    ('editor', 'editor'),
+)
+
+site_editor_users = (
+    ('foo-editor', 'foo-editor'),
+    ('bar-editor', 'bar-editor'),
+)
+
+status_map_editor_catalogs = {
+    'update': {
+        'foo-catalog': {
+            'editor': 200, 'foo-editor': 200, 'bar-editor': 403
+        },
+        'bar-catalog': {
+            'editor': 200, 'foo-editor': 403, 'bar-editor': 200
+        }
+    },
+    'delete': {
+        'foo-catalog': {
+            'editor': 204, 'foo-editor': 204, 'bar-editor': 403
+        },
+        'bar-catalog': {
+            'editor': 204, 'foo-editor': 403, 'bar-editor': 204
+        }
+    }
+}
+
+def get_status_editor_catalogs(username, catalog_key, method):
+    test_status_user = status_map[method].get(username)
+    if (username, username) in site_editor_users and test_status_user is None:
+        try:
+            test_status_user = status_map_editor_catalogs[method][catalog_key][username]
+        except KeyError:
+            test_status_user = 403
+    return test_status_user
+
+
+
+urlnames = {
+    'list': 'v1-questions:catalog-list',
+    'nested': 'v1-questions:catalog-nested',
+    'index': 'v1-questions:catalog-index',
+    'export': 'v1-questions:catalog-export',
+    'detail': 'v1-questions:catalog-detail',
+    'detail_export': 'v1-questions:catalog-detail-export',
+    'copy': 'v1-questions:catalog-copy'
+}
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_list(db, client, username, password):
+    client.login(username=username, password=password)
+
+    url = reverse(urlnames['list'])
+    response = client.get(url)
+    assert response.status_code == status_map['list'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_index(db, client, username, password):
+    client.login(username=username, password=password)
+
+    url = reverse(urlnames['index'])
+    response = client.get(url)
+    assert response.status_code == status_map['list'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_export(db, client, username, password):
+    client.login(username=username, password=password)
+
+    url = reverse(urlnames['export'])
+    response = client.get(url)
+    assert response.status_code == status_map['list'][username], response.content
+
+    if response.status_code == 200:
+        root = et.fromstring(response.content)
+        assert root.tag == 'rdmo'
+        for child in root:
+            assert child.tag in ['catalog', 'section', 'questionset', 'question']
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_detail(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['detail'], args=[instance.pk])
+        response = client.get(url)
+        assert response.status_code == status_map['detail'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_nested(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['nested'], args=[instance.pk])
+        response = client.get(url)
+        assert response.status_code == status_map['detail'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_create(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['list'])
+        data = {
+            'uri_prefix': instance.uri_prefix,
+            'key': '%s_new_%s' % (instance.key, username),
+            'comment': instance.comment,
+            'order': instance.order,
+            'title_en': instance.title_lang1,
+            'title_de': instance.title_lang2
+        }
+        response = client.post(url, data)
+        assert response.status_code == status_map['create'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_update(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['detail'], args=[instance.pk])
+        data = {
+            'uri_prefix': instance.uri_prefix,
+            'key': instance.key,
+            'comment': instance.comment,
+            'order': instance.order,
+            'title_en': instance.title_lang1,
+            'title_de': instance.title_lang2
+        }
+        response = client.put(url, data, content_type='application/json')
+        assert response.status_code == status_map['update'].get(username), response.json()
+
+@pytest.mark.parametrize('username,password', site_editor_users+multisite_editor)
+def test_update_editors(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['detail'], args=[instance.pk])
+        data = {
+            'uri_prefix': instance.uri_prefix,
+            'key': instance.key,
+            'comment': instance.comment,
+            'order': instance.order,
+            'title_en': instance.title_lang1,
+            'title_de': instance.title_lang2
+        }
+        
+        test_status_user = get_status_editor_catalogs(username, instance.key, 'update')
+        
+        response = client.put(url, data, content_type='application/json')
+        assert response.status_code == test_status_user, response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_delete(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['detail'], args=[instance.pk])
+        response = client.delete(url)
+        assert response.status_code == status_map['delete'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', site_editor_users+multisite_editor)
+def test_delete_editors(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['detail'], args=[instance.pk])
+        response = client.delete(url)
+        
+        test_status_user = get_status_editor_catalogs(username, instance.key, 'delete')
+        # if not test_status_user == 403:
+        #     breakpoint()
+                
+        assert response.status_code == test_status_user, response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_detail_export(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['detail_export'], args=[instance.pk])
+        response = client.get(url)
+        assert response.status_code == status_map['list'][username], response.content
+
+        if response.status_code == 200:
+            root = et.fromstring(response.content)
+            assert root.tag == 'rdmo'
+            for child in root:
+                assert child.tag in ['catalog', 'section', 'questionset', 'question']
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_copy(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Catalog.objects.all()
+
+    for instance in instances:
+        url = reverse(urlnames['copy'], args=[instance.pk])
+        data = {
+            'uri_prefix': instance.uri_prefix + '-',
+            'key': instance.key + '-',
+        }
+        response = client.put(url, data, content_type='application/json')
+        assert response.status_code == status_map['create'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_copy_wrong(db, client, username, password):
+    client.login(username=username, password=password)
+    instance = Catalog.objects.first()
+
+    url = reverse(urlnames['copy'], args=[instance.pk])
+    data = {
+        'uri_prefix': instance.uri_prefix,
+        'key': instance.key
+    }
+    response = client.put(url, data, content_type='application/json')
+
+    if status_map['create'][username] == 201:
+        assert response.status_code == 400, response.json()
+    else:
+        assert response.status_code == status_map['create'][username], response.json()
