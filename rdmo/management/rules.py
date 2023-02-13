447e1abdc80566c0456dc3e46e3ecd4c82aec54b
David Wallace <david.wallace@tu-darmstadt.de>
2023-02-13 18:53:48 +0100

    add rules to management app

---
+++ rdmo/management/rules.py
@@ -0,0 +1,35 @@
+import rules
+
+from rdmo.questions.rules import is_an_editor
+
+# Model Permissions for sites and group
+rules.add_perm('sites.view_site', is_an_editor)
+rules.add_perm('auth.view_group', is_an_editor)
+
+# Model Permissions for domain
+rules.add_perm('domain.view_attribute', is_an_editor)
+rules.add_perm('domain.add_attribute', is_an_editor)
+rules.add_perm('domain.change_attribute', is_an_editor)
+rules.add_perm('domain.delete_attribute', is_an_editor)
+
+# Model Permissions for options
+rules.add_perm('options.view_option', is_an_editor)
+rules.add_perm('options.add_option', is_an_editor)
+rules.add_perm('options.change_option', is_an_editor)
+rules.add_perm('options.delete_option', is_an_editor)
+
+# Model Permissions for optionsets
+rules.add_perm('options.view_optionset', is_an_editor)
+rules.add_perm('options.add_optionset', is_an_editor)
+rules.add_perm('options.change_optionset', is_an_editor)
+rules.add_perm('options.delete_optionset', is_an_editor)
+
+# Model Permissions for conditions
+rules.add_perm('conditions.view_condition', is_an_editor)
+rules.add_perm('conditions.add_condition', is_an_editor)
+rules.add_perm('conditions.change_condition', is_an_editor)
+rules.add_perm('conditions.delete_condition', is_an_editor)
+
+# Model Permissions for tasks and views
+rules.add_perm('tasks.view_task', is_an_editor)
+rules.add_perm('views.view_view', is_an_editor)
