30dc0cd28a5887d02f9ff050e060d1893d2e7904
David Wallace <david.wallace@tu-darmstadt.de>
2023-02-13 18:55:03 +0100

    update questions.rules for editor

--- rdmo/questions/rules.py
+++ rdmo/questions/rules.py
@@ -6,6 +6,14 @@ import rules
 
 logger = logging.getLogger(__name__)
 
+@rules.predicate
+def is_an_editor(user, obj) -> bool:
+    ''' Checks if any editor role exsits for the user '''
+    if not user.is_authenticated:
+        return False
+    # breakpoint()
+    return user.role.editor.exists()
+
 
 @rules.predicate
 def is_element_editor(user, obj) -> bool:
@@ -39,49 +47,50 @@ def is_multisite_editor(user, obj) -> bool:
 
 # Model permissions for questions app
 # for catalogs
-rules.add_perm('questions.view_catalog', is_multisite_editor)
-rules.add_perm('questions.add_catalog', is_multisite_editor)
-rules.add_perm('questions.change_catalog', is_multisite_editor)
-rules.add_perm('questions.delete_catalog', is_multisite_editor)
+rules.add_perm('questions.view_catalog', is_an_editor)
+rules.add_perm('questions.add_catalog', is_an_editor)
+rules.add_perm('questions.change_catalog', is_an_editor)
+rules.add_perm('questions.delete_catalog', is_an_editor)
 
 # for sections
-rules.add_perm('questions.view_section', is_multisite_editor)
-rules.add_perm('questions.add_section', is_multisite_editor)
-rules.add_perm('questions.change_section', is_multisite_editor)
-rules.add_perm('questions.delete_section', is_multisite_editor)
+rules.add_perm('questions.view_section', is_an_editor)
+rules.add_perm('questions.add_section', is_an_editor)
+rules.add_perm('questions.change_section', is_an_editor)
+rules.add_perm('questions.delete_section', is_an_editor)
 
 # for questionsets
-rules.add_perm('questions.add_questionset', is_multisite_editor)
-rules.add_perm('questions.change_questionset', is_multisite_editor)
-rules.add_perm('questions.delete_questionset', is_multisite_editor)
+rules.add_perm('questions.view_questionset', is_an_editor)
+rules.add_perm('questions.add_questionset', is_an_editor)
+rules.add_perm('questions.change_questionset', is_an_editor)
+rules.add_perm('questions.delete_questionset', is_an_editor)
 
 # for questions
-rules.add_perm('questions.view_question', is_multisite_editor)
-rules.add_perm('questions.add_question', is_multisite_editor)
-rules.add_perm('questions.change_question', is_multisite_editor)
-rules.add_perm('questions.delete_question', is_multisite_editor)
+rules.add_perm('questions.view_question', is_an_editor)
+rules.add_perm('questions.add_question', is_an_editor)
+rules.add_perm('questions.change_question', is_an_editor)
+rules.add_perm('questions.delete_question', is_an_editor)
 
 
 # Object permissions
 # for catalogs
-rules.add_perm('questions.view_catalog_object', is_element_editor)
-rules.add_perm('questions.add_catalog_object', is_element_editor)
-rules.add_perm('questions.change_catalog_object', is_element_editor)
-rules.add_perm('questions.delete_catalog_object', is_element_editor)
+rules.add_perm('questions.view_catalog_object', is_an_editor)
+rules.add_perm('questions.add_catalog_object', is_multisite_editor | is_element_editor)
+rules.add_perm('questions.change_catalog_object', is_multisite_editor | is_element_editor)
+rules.add_perm('questions.delete_catalog_object', is_multisite_editor | is_element_editor)
 
 # for sections
-rules.add_perm('questions.view_section_object', is_element_editor)
-rules.add_perm('questions.add_section_object', is_element_editor)
-rules.add_perm('questions.change_section_object', is_element_editor)
-rules.add_perm('questions.delete_section_object', is_element_editor)
+rules.add_perm('questions.view_section_object', is_an_editor)
+rules.add_perm('questions.add_section_object', is_multisite_editor | is_element_editor)
+rules.add_perm('questions.change_section_object', is_multisite_editor | is_element_editor)
+rules.add_perm('questions.delete_section_object', is_multisite_editor | is_element_editor)
 
 # for questionsets
-rules.add_perm('questions.add_questionset_object', is_element_editor)
-rules.add_perm('questions.change_questionset_object', is_element_editor)
-rules.add_perm('questions.delete_questionset_object', is_element_editor)
+rules.add_perm('questions.add_questionset_object', is_multisite_editor | is_element_editor)
+rules.add_perm('questions.change_questionset_object', is_multisite_editor | is_element_editor)
+rules.add_perm('questions.delete_questionset_object', is_multisite_editor | is_element_editor)
 
 # for questions
-rules.add_perm('questions.view_question_object', is_element_editor)
-rules.add_perm('questions.add_question_object', is_element_editor)
-rules.add_perm('questions.change_question_object', is_element_editor)
-rules.add_perm('questions.delete_question_object', is_element_editor)
+rules.add_perm('questions.view_question_object', is_an_editor)
+rules.add_perm('questions.add_question_object', is_multisite_editor | is_element_editor)
+rules.add_perm('questions.change_question_object', is_multisite_editor | is_element_editor)
+rules.add_perm('questions.delete_question_object', is_multisite_editor | is_element_editor)
