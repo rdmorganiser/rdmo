f1b6140877e1aab844703ab65daa42ab8521dc60
David Wallace <david.wallace@tu-darmstadt.de>
2023-02-17 15:30:10 +0100

    update display of RoleAdmin fields

--- rdmo/accounts/admin.py
+++ rdmo/accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
+from django.db.models import Count, Value
 
 from .models import AdditionalField, AdditionalFieldValue, ConsentFieldValue, Role
 
@@ -20,7 +22,36 @@ class ConsentFieldValueAdmin(admin.ModelAdmin):
 
 class RoleAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'user__email')
-    list_filter = ('member', 'manager')
+    list_filter = ('member', 'manager', 'editor', 'reviewer')
+
+    list_display = ('user', 'member_at', 'member_at', 'manager_at', 'editor_at', 'reviewer_at')
+
+    def get_queryset(self, request):
+        return Role.objects.prefetch_related(
+                'member', 'manager', 'editor', 'reviewer').annotate(
+                    Count('member'), Count('manager'), Count('editor'), Count('reviewer'),
+                    sites_count=Value(Site.objects.count())
+                )
+
+    def member_at(self, obj):
+        if obj.member__count == obj.sites_count:
+            return 'all sites'
+        return ', '.join([site.domain for site in obj.member.all()])
+
+    def manager_at(self, obj):
+        if obj.manager__count == obj.sites_count:
+            return 'all sites'
+        return ', '.join([site.domain for site in obj.manager.all()])
+
+    def editor_at(self, obj):
+        if obj.editor__count == obj.sites_count:
+            return 'all sites'
+        return ', '.join([site.domain for site in obj.editor.all()])
+
+    def reviewer_at(self, obj):
+        if obj.reviewer__count == obj.sites_count:
+            return 'all sites'
+        return ', '.join([site.domain for site in obj.reviewer.all()])
 
 
 admin.site.register(AdditionalField, AdditionalFieldAdmin)
