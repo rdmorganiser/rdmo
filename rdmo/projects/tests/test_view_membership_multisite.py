6fbf8616184dda8256e251baa7d95845b4beeeac
David Wallace <david.wallace@tu-darmstadt.de>
2023-02-21 13:42:08 +0100

    rm redundant variables from test

--- rdmo/projects/tests/test_view_membership_multisite.py
+++ rdmo/projects/tests/test_view_membership_multisite.py
@@ -17,7 +17,7 @@ users = (
     ('anonymous', None),
 )
 
-add_membership_permission_map = change_membership_permission_map = delete_membership_permission_map = {
+add_membership_permission_map = {
     'owner': [1, 2, 3, 4, 5],
     'api': [1, 2, 3, 4, 5],
     'site': [1, 2, 3, 4, 5]
