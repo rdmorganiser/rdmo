c2038d97ccf4c814c005347f18c1ec7da32d65bc
David Wallace <david.wallace@tu-darmstadt.de>
2023-02-24 13:36:28 +0100

    mv preds to management

--- rdmo/questions/rules.py
+++ rdmo/questions/rules.py
@@ -5,92 +5,10 @@ from django.contrib.sites.models import Site
 
 import rules
 
+from rdmo.management.rules import is_an_editor, is_a_reviewer, is_element_editor, \
+                                    is_element_reviewer, is_multisite_editor, is_multisite_reviewer
+
 logger = logging.getLogger(__name__)
-
-@rules.predicate
-def is_an_editor(user, obj) -> bool:
-    ''' Checks if any editor role exsits for the user '''
-    if not user.is_authenticated:
-        return False
-    return user.role.editor.exists()
-
-
-@rules.predicate
-def is_element_editor(user, obj) -> bool:
-    ''' Checks if the user is an editor for the sites to which this element is editable '''
-    if not user.is_authenticated:
-        return False
-
-    if 'project' in obj._meta.model_name:
-        return False
-
-    if not hasattr(obj, 'editors'):
-        logger.debug('rules.is_element_editor: obj %s has no attribute editors', obj)
-        return False
-
-    # if the element has no editors, it is editable by all editors
-    if not obj.editors.exists():
-        return user.role.editor.exists()
-    return user.role.editor.filter(id__in=obj.editors.all()).exists()
-
-
-@rules.predicate
-def is_multisite_editor(user, obj) -> bool:
-    ''' Checks if the user is an editor for all the sites '''
-    if not user.is_authenticated:
-        return False
-
-    if not user.role.editor.exists():
-        logger.debug('rules.is_multisite_editor: obj %s has no role editor', user)
-        return False
-    return user.role.editor.count() == Site.objects.count()
-
-
-@rules.predicate
-def is_a_reviewer(user, obj) -> bool:
-    ''' Checks if any reviewer role exsits for the user '''
-    if not user.is_authenticated:
-        return False
-    return user.role.reviewer.exists()
-
-
-@rules.predicate
-def is_element_reviewer(user, obj) -> bool:
-    ''' Checks if the user is an reviewer for the sites to which this element is editable '''
-    if not user.is_authenticated:
-        return False
-
-    if 'project' in obj._meta.model_name:
-        return False
-
-    if not hasattr(obj, 'editors'):
-        logger.debug('rules.is_element_reviewer: obj %s has no attribute editors', obj)
-        return False
-
-    # if the element has no editors, it is viewables by all reviewers
-    if not obj.editors.exists():
-        return user.role.reviewer.exists()
-    
-    if hasattr(obj, 'sites'):
-        obj_related_to_reviewer = user.role.reviewer.filter(
-        models.Q(id__in=obj.editors.all()) | models.Q(id__in=obj.sites.all())
-        ).exists()
-    else:
-        obj_related_to_reviewer = user.role.reviewer.filter(id__in=obj.editors.all()).exists()
-
-    return obj_related_to_reviewer
-
-@rules.predicate
-def is_multisite_reviewer(user, obj) -> bool:
-    ''' Checks if the user is an reviewer for all the sites '''
-    if not user.is_authenticated:
-        return False
-
-    if not user.role.reviewer.exists():
-        logger.debug('rules.is_multisite_reviewer: obj %s has no role reviewer', user)
-        return False
-    return user.role.reviewer.count() == Site.objects.count()
-
 
 
 # Model permissions for questions app
