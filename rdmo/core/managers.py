7dd43274721bca5df4a7dde8dccb6bc32811ed5a
David Wallace <david.wallace@tu-darmstadt.de>
2023-02-27 18:37:59 +0100

    add EditableElementQuerySetMixin to core managers

--- rdmo/core/managers.py
+++ rdmo/core/managers.py
@@ -1,13 +1,41 @@
 from django.conf import settings
 from django.db import models
 
+from rdmo.management.rules import is_an_editor, is_multisite_editor, \
+                                    is_a_reviewer, is_multisite_reviewer
+
 from .constants import PERMISSIONS
 
-
 class CurrentSiteQuerySetMixin(object):
-
+    
     def filter_current_site(self):
         return self.filter(models.Q(sites=None) | models.Q(sites=settings.SITE_ID))
+
+class EditableElementQuerySetMixin(models.QuerySet):
+
+    def filter_related_to_user_role(self, user):
+        return self.filter(models.Q(editors=None) |
+                           models.Q(editors__in=user.role.editor.all()) |
+                           models.Q(editors__in=user.role.reviewer.all())
+                           )
+
+    def filter_user(self, user):
+        if not user.is_authenticated:
+            return self.none()
+
+        model_name = self.model._meta.model_name
+        app_label = self.model._meta.app_label
+
+        if not user.has_perm('%s.view_%s' % (app_label, model_name)):
+            return self.none()
+
+        if is_multisite_editor(user) or is_multisite_reviewer(user) or \
+                user.is_superuser:
+            return self.all()
+        elif is_an_editor(user) or is_a_reviewer(user):
+            return self.filter_related_to_user_role(user)
+        else:
+            return self.none()
 
 
 class GroupsQuerySetMixin(object):
@@ -33,6 +61,12 @@ class CurrentSiteManagerMixin(object):
 
     def filter_current_site(self):
         return self.get_queryset().filter_current_site()
+
+
+class EditableElementManagerMixin(object):
+
+    def filter_user(self, user):
+        return self.get_queryset().filter_user(user)
 
 
 class GroupsManagerMixin(object):
