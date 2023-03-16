2a5a5fc87d2a8f2383c21273143a722a94072882
David Wallace <david.wallace@tu-darmstadt.de>
2023-03-16 10:55:58 +0100

    revert queryset mixins and object managers

--- rdmo/core/managers.py
+++ rdmo/core/managers.py
@@ -1,70 +1,13 @@
-from functools import reduce
-
-from django.core.exceptions import FieldDoesNotExist
 from django.conf import settings
 from django.db import models
 
-from rdmo.management.rules import is_an_editor, is_multisite_editor, \
-                                    is_a_reviewer, is_multisite_reviewer
-
 from .constants import PERMISSIONS
 
+
 class CurrentSiteQuerySetMixin(object):
-    
+
     def filter_current_site(self):
         return self.filter(models.Q(sites=None) | models.Q(sites=settings.SITE_ID))
-
-class EditableElementQuerySetMixin(models.QuerySet):
-
-    def filter_related_to_user_role(self, user):
-        ''' returns all objects that are related to the user editor or reviewer role'''
-
-
-        related_Q = None
-
-        try:
-            get_field = self.model._meta.get_field('sites')
-            sites_Q = (
-                models.Q(sites__in=user.role.editor.all()) |
-                models.Q(sites__in=user.role.reviewer.all())
-                )
-            related_Q = sites_Q
-        except FieldDoesNotExist:
-            pass
-
-        try:
-            get_field = self.model._meta.get_field('editors')
-            editors_Q = (
-                models.Q(editors=None) |
-                models.Q(editors__in=user.role.editor.all()) |
-                models.Q(editors__in=user.role.reviewer.all())
-            )
-
-            related_Q = related_Q | editors_Q if related_Q is not None else editors_Q
-
-        except FieldDoesNotExist:
-            pass
-        # breakpoint()
-        return related_Q
-
-    def filter_user(self, user):
-        if not user.is_authenticated:
-            return self.none()
-
-        model_name = self.model._meta.model_name
-        app_label = self.model._meta.app_label
-
-        if not user.has_perm('%s.view_%s' % (app_label, model_name)):
-            return self.none()
-
-        if is_multisite_editor(user) or is_multisite_reviewer(user) or \
-                user.is_superuser:
-            return self.all()
-        elif is_an_editor(user) or is_a_reviewer(user):
-            related_Q = self.filter_related_to_user_role(user)
-            return self.filter(related_Q) if related_Q is not None else self.none()
-        else:
-            return self.none()
 
 
 class GroupsQuerySetMixin(object):
@@ -90,12 +33,6 @@ class CurrentSiteManagerMixin(object):
 
     def filter_current_site(self):
         return self.get_queryset().filter_current_site()
-
-
-class EditableElementManagerMixin(object):
-
-    def filter_user(self, user):
-        return self.get_queryset().filter_user(user)
 
 
 class GroupsManagerMixin(object):
