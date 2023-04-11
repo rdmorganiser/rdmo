8e293374e1c53e0807c324904ed1c1d90c11f1cd
Jochen Klar <mail@jochenklar.de>
2023-04-11 15:43:03 +0200

    Fix errors component

--- rdmo/management/assets/js/components/common/Errors.js
+++ rdmo/management/assets/js/components/common/Errors.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-
+import isString from 'lodash/isString'
 
 const ElementErrors = ({ element }) => {
-  return element.errors && (
-    <ul className="list-unstyled text-danger mb-0">
-      {Object.values(element.errors).flat().map((error, index) => <li key={index}>{error}</li>)}
-    </ul>
-  )
+  if (element.errors) {
+    const errorList = Object.values(element.errors).flat().reduce((acc, cur) => {
+      if (isString(cur)) {
+        acc.push(cur)
+      } else {
+        Object.values(cur).forEach(value => {
+          acc = acc.concat(value)
+        })
+      }
+
+      return acc
+    }, [])
+
+    return element.errors && (
+      <ul className="list-unstyled text-danger mb-0">
+        {errorList.map((error, index) => <li key={index}>{error}</li>)}
+      </ul>
+    )
+  } else {
+    return null
+  }
+
+
+
 }
 
 ElementErrors.propTypes = {
