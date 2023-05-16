99d1c1126bef9ee747e1dfe6c15571e949ba7000
Jochen Klar <mail@jochenklar.de>
2023-05-16 16:31:05 +0200

    Refactor tests

--- rdmo/options/tests/test_viewset_options.py
+++ rdmo/options/tests/test_viewset_options.py
@@ -41,6 +41,8 @@ urlnames = {
     'copy': 'v1-options:option-copy'
 }
 
+export_formats = ('xml', 'rtf', 'odt', 'docx', 'html', 'markdown', 'tex', 'pdf')
+
 
 @pytest.mark.parametrize('username,password', users)
 def test_list(db, client, username, password):
@@ -61,14 +63,15 @@ def test_index(db, client, username, password):
 
 
 @pytest.mark.parametrize('username,password', users)
-def test_export(db, client, username, password):
+@pytest.mark.parametrize('export_format', export_formats)
+def test_export(db, client, username, password, export_format):
     client.login(username=username, password=password)
 
-    url = reverse(urlnames['export'])
+    url = reverse(urlnames['export']) + export_format + '/'
     response = client.get(url)
     assert response.status_code == status_map['list'][username], response.content
 
-    if response.status_code == 200:
+    if response.status_code == 200 and export_format == 'xml':
         root = et.fromstring(response.content)
         assert root.tag == 'rdmo'
         for child in root:
@@ -169,20 +172,20 @@ def test_delete(db, client, username, password):
 
 
 @pytest.mark.parametrize('username,password', users)
-def test_detail_export(db, client, username, password):
+@pytest.mark.parametrize('export_format', export_formats)
+def test_detail_export(db, client, username, password, export_format):
     client.login(username=username, password=password)
-    instances = Option.objects.all()
+    instance = Option.objects.first()
 
-    for instance in instances:
-        url = reverse(urlnames['detail_export'], args=[instance.pk])
-        response = client.get(url)
-        assert response.status_code == status_map['list'][username], response.content
+    url = reverse(urlnames['detail_export'], args=[instance.pk]) + export_format + '/'
+    response = client.get(url)
+    assert response.status_code == status_map['list'][username], response.content
 
-        if response.status_code == 200:
-            root = et.fromstring(response.content)
-            assert root.tag == 'rdmo'
-            for child in root:
-                assert child.tag in ['option']
+    if response.status_code == 200 and export_format == 'xml':
+        root = et.fromstring(response.content)
+        assert root.tag == 'rdmo'
+        for child in root:
+            assert child.tag in ['option']
 
 
 @pytest.mark.parametrize('username,password', users)
