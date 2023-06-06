b6664559cc74cc477c8b267e2c5f2f847d89902d
David Wallace <david.wallace@tu-darmstadt.de>
2023-06-06 09:55:05 +0200

    fix object perms in conditions viewset tests

--- rdmo/conditions/tests/test_viewset_condition_multisite.py
+++ rdmo/conditions/tests/test_viewset_condition_multisite.py
@@ -8,13 +8,17 @@ from ..models import Condition
 
 users = (
     ('editor', 'editor'),
+    ('user', 'user'),
+    ('example-reviewer', 'example-reviewer'),
     ('example-editor', 'example-editor'),
     ('foo-user', 'foo-user'),
     ('foo-reviewer', 'foo-reviewer'),
     ('foo-editor', 'foo-editor'),
     ('bar-user', 'bar-user'),
     ('bar-reviewer', 'bar-reviewer'),
+
     ('bar-editor', 'bar-editor'),
+    ('anonymous', None),
 )
 
 
@@ -22,28 +26,45 @@ status_map = {
     'list': {
         'foo-user': 403, 'foo-reviewer': 200, 'foo-editor': 200,
         'bar-user': 403, 'bar-reviewer': 200, 'bar-editor': 200,
-        'example-editor': 200, 'editor': 200
+        'user': 403, 'example-reviewer': 200, 'example-editor': 200,
+        'editor': 200,
+        'anonymous': 401
     },
     'detail': {
-        'foo-user': 403, 'foo-reviewer': 200, 'foo-editor': 200,
-        'bar-user': 403, 'bar-reviewer': 200, 'bar-editor': 200,
-        'example-editor': 200, 'editor': 200
+        'foo-user': 404, 'foo-reviewer': 200, 'foo-editor': 200,
+        'bar-user': 404, 'bar-reviewer': 200, 'bar-editor': 200,
+        'user': 404, 'example-reviewer': 200, 'example-editor': 200,
+        'editor': 200,
+        'anonymous': 401
     },
     'create': {
         'foo-user': 403, 'foo-reviewer': 403, 'foo-editor': 201,
         'bar-user': 403, 'bar-reviewer': 403, 'bar-editor': 201,
-        'example-editor': 201, 'editor': 201
+        'user': 403, 'example-reviewer': 403, 'example-editor': 201,
+        'editor': 201,
+        'anonymous': 401
     },
     'update': {
-        'foo-user': 403, 'foo-reviewer': 403, 'foo-editor': 403,
-        'bar-user': 403, 'bar-reviewer': 403, 'bar-editor': 403,
-        'example-editor': 200, 'editor': 200
+        'foo-user': 404, 'foo-reviewer': 403, 'foo-editor': 200,
+        'bar-user': 404, 'bar-reviewer': 403, 'bar-editor': 200,
+        'user': 404, 'example-reviewer': 403, 'example-editor': 200,
+        'editor': 200,
+        'anonymous': 401
     },
     'delete': {
-        'foo-user': 403, 'foo-reviewer': 403, 'foo-editor': 403,
-        'bar-user': 403, 'bar-reviewer': 403, 'bar-editor': 403,
-        'example-editor': 204, 'editor': 204
+        'foo-user': 404, 'foo-reviewer': 403, 'foo-editor': 204,
+        'bar-user': 404, 'bar-reviewer': 403, 'bar-editor': 204,
+        'user': 404, 'example-reviewer': 403, 'example-editor': 204,
+        'editor': 204,
+        'anonymous': 401
     }
+}
+
+status_map_multisite_editors = {
+    'example.com': ['example-editor', 'editor'],
+    'foo.com': ['foo-editor'],
+    'bar.com': ['bar-editor'],
+    
 }
 
 urlnames = {
@@ -121,19 +142,16 @@ def test_create(db, client, username, password):
 
 
 @pytest.mark.parametrize('username,password', users)
-def test_update_with_editors_currentsite(db, client, username, password):
+def test_update(db, client, username, password):
     client.login(username=username, password=password)
     instances = Condition.objects.all()
 
     for instance in instances:
         
-        # set current site for object permissions, example-com
-        instance.editors.set([Site.objects.get_current()])
-        
         url = reverse(urlnames['detail'], args=[instance.pk])
         data = {
             'uri_prefix': instance.uri_prefix,
-            'key': instance.key,
+            'key': instance.key + '-' + username,
             'comment': instance.comment,
             'source': instance.source.pk,
             'relation': instance.relation,
@@ -145,7 +163,43 @@ def test_update_with_editors_currentsite(db, client, username, password):
 
 
 @pytest.mark.parametrize('username,password', users)
-def test_delete_with_editors_currentsite(db, client, username, password):
+def _skip_test_update_with_editors_currentsite(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Condition.objects.all()
+
+    for instance in instances:
+        
+        # set current site for object permissions, example-com
+        instance.editors.set([Site.objects.get_current()])
+        
+        url = reverse(urlnames['detail'], args=[instance.pk])
+        data = {
+            'uri_prefix': instance.uri_prefix,
+            'key': instance.key + '-' + username,
+            'comment': instance.comment,
+            'source': instance.source.pk,
+            'relation': instance.relation,
+            'target_text': instance.target_text,
+            'target_option': instance.target_option.pk if instance.target_option else None
+        }
+        response = client.put(url, data, content_type='application/json')
+        assert response.status_code == status_map['update'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def test_delete(db, client, username, password):
+    client.login(username=username, password=password)
+    instances = Condition.objects.all()
+
+    for instance in instances:
+
+        url = reverse(urlnames['detail'], args=[instance.pk])
+        response = client.delete(url)
+        assert response.status_code == status_map['delete'][username], response.json()
+
+
+@pytest.mark.parametrize('username,password', users)
+def _skip_test_delete_with_editors_currentsite(db, client, username, password):
     client.login(username=username, password=password)
     instances = Condition.objects.all()
 
@@ -167,7 +221,7 @@ def test_detail_export(db, client, username, password):
     for instance in instances:
         url = reverse(urlnames['detail_export'], args=[instance.pk])
         response = client.get(url)
-        assert response.status_code == status_map['list'][username], response.content
+        assert response.status_code == status_map['detail'][username], response.content
 
         if response.status_code == 200:
             root = et.fromstring(response.content)
@@ -180,6 +234,9 @@ def test_detail_export(db, client, username, password):
 def test_copy(db, client, username, password):
     client.login(username=username, password=password)
     instances = Condition.objects.all()
+    status_map_method = 'create'
+    if not 'editor' in username:
+        status_map_method = 'update'
 
     for instance in instances:
         url = reverse(urlnames['copy'], args=[instance.pk])
@@ -188,13 +245,20 @@ def test_copy(db, client, username, password):
             'key': instance.key + '-'
         }
         response = client.put(url, data, content_type='application/json')
-        assert response.status_code == status_map['create'][username], response.json()
+        
+            
+        assert response.status_code == status_map[status_map_method][username], response.json()
 
 
 @pytest.mark.parametrize('username,password', users)
 def test_copy_wrong(db, client, username, password):
     client.login(username=username, password=password)
     instance = Condition.objects.first()
+
+    status_map_method = 'create'
+    if not 'editor' in username:
+        status_map_method = 'update'
+
 
     url = reverse(urlnames['copy'], args=[instance.pk])
     data = {
@@ -206,4 +270,4 @@ def test_copy_wrong(db, client, username, password):
     if status_map['create'][username] == 201:
         assert response.status_code == 400, response.json()
     else:
-        assert response.status_code == status_map['create'][username], response.json()
+        assert response.status_code == status_map[status_map_method][username], response.json()
