04d2bfa55af07db61c7a0ab45d0b7ff246f9e012
David Wallace <david.wallace@tu-darmstadt.de>
2023-06-12 20:04:57 +0200

    fix tests for options viewsets

--- rdmo/options/tests/test_viewset_optionsets_multisite.py
+++ rdmo/options/tests/test_viewset_optionsets_multisite.py
@@ -7,8 +7,11 @@ from django.urls import reverse
 from ..models import OptionSet
 
 
+
 users = (
     ('editor', 'editor'),
+    ('user', 'user'),
+    ('example-reviewer', 'example-reviewer'),
     ('example-editor', 'example-editor'),
     ('foo-user', 'foo-user'),
     ('foo-reviewer', 'foo-reviewer'),
@@ -23,41 +26,38 @@ status_map = {
     'list': {
         'foo-user': 403, 'foo-reviewer': 200, 'foo-editor': 200,
         'bar-user': 403, 'bar-reviewer': 200, 'bar-editor': 200,
-        'example-editor': 200, 'editor': 200
+        'user': 403, 'example-reviewer': 200, 'example-editor': 200,
+        'editor': 200
     },
     'detail': {
-        'foo-user': 403, 'foo-reviewer': 200, 'foo-editor': 200,
-        'bar-user': 403, 'bar-reviewer': 200, 'bar-editor': 200,
-        'example-editor': 200, 'editor': 200
+        'foo-user': 404, 'foo-reviewer': 200, 'foo-editor': 200,
+        'bar-user': 404, 'bar-reviewer': 200, 'bar-editor': 200,
+        'user': 404, 'example-reviewer': 200, 'example-editor': 200,
+        'editor': 200
     },
     'create': {
         'foo-user': 403, 'foo-reviewer': 403, 'foo-editor': 201,
         'bar-user': 403, 'bar-reviewer': 403, 'bar-editor': 201,
-        'example-editor': 201, 'editor': 201
+        'user': 403, 'example-reviewer': 403, 'example-editor': 201,
+        'editor': 201
+    },
+    'copy': {
+        'foo-user': 404, 'foo-reviewer': 403, 'foo-editor': 201,
+        'bar-user': 404, 'bar-reviewer': 403, 'bar-editor': 201,
+        'user': 404, 'example-reviewer': 403, 'example-editor': 201,
+        'editor': 201
     },
     'update': {
-        'foo-user': 403, 'foo-reviewer': 403, 'foo-editor': 403,
-        'bar-user': 403, 'bar-reviewer': 403, 'bar-editor': 403,
-        'example-editor': 200, 'editor': 200
+        'foo-user': 404, 'foo-reviewer': 404, 'foo-editor': 404,
+        'bar-user': 404, 'bar-reviewer': 404, 'bar-editor': 404,
+        'user': 404, 'example-reviewer': 403, 'example-editor': 200,
+        'editor': 200
     },
     'delete': {
-        'foo-user': 403, 'foo-reviewer': 403, 'foo-editor': 204,
-        'bar-user': 403, 'bar-reviewer': 403, 'bar-editor': 204,
-        'example-editor': 204, 'editor': 204
-    }
-}
-
-
-status_map_obj_permissions = { # these are permissions for an object with editors set to current site (=example.com)
-    'update': {
-        'foo-user': 403, 'foo-reviewer': 403, 'foo-editor': 403,
-        'bar-user': 403, 'bar-reviewer': 403, 'bar-editor': 403,
-        'example-editor': 200, 'editor': 200
-    },
-    'delete': {
-        'foo-user': 403, 'foo-reviewer': 403, 'foo-editor': 403,
-        'bar-user': 403, 'bar-reviewer': 403, 'bar-editor': 403,
-        'example-editor': 204, 'editor': 204
+        'foo-user': 404, 'foo-reviewer': 404, 'foo-editor': 404,
+        'bar-user': 404, 'bar-reviewer': 404, 'bar-editor': 404,
+        'user': 404, 'example-reviewer': 403, 'example-editor': 204,
+        'editor': 204
     }
 }
 
@@ -71,9 +71,6 @@ urlnames = {
     'detail_export': 'v1-options:optionset-detail-export',
     'copy': 'v1-options:optionset-copy'
 }
-
-
-
 
 
 @pytest.mark.parametrize('username,password', users)
@@ -166,18 +163,8 @@ def test_update_with_obj_permission_set_editors_currentsite(db, client, username
             'conditions': [condition.pk for condition in instance.conditions.all()],
         }
         response = client.put(url, data, content_type='application/json')
-        assert response.status_code == status_map_obj_permissions['update'][username], response.json()
+        assert response.status_code == status_map['update'][username], response.json()
 
-
-@pytest.mark.parametrize('username,password', users)
-def test_delete(db, client, username, password):
-    client.login(username=username, password=password)
-    instances = OptionSet.objects.all()
-
-    for instance in instances:
-        url = reverse(urlnames['detail'], args=[instance.pk])
-        response = client.delete(url)
-        assert response.status_code == status_map['delete'][username]
 
 
 @pytest.mark.parametrize('username,password', users)
@@ -192,7 +179,7 @@ def test_delete_with_obj_permissions_set_editors_currentsite(db, client, usernam
 
         url = reverse(urlnames['detail'], args=[instance.pk])
         response = client.delete(url)
-        assert response.status_code == status_map_obj_permissions['delete'][username]
+        assert response.status_code == status_map['delete'][username]
 
 
 @pytest.mark.parametrize('username,password', users)
@@ -224,7 +211,7 @@ def test_copy(db, client, username, password):
             'key': instance.key + '-'
         }
         response = client.put(url, data, content_type='application/json')
-        assert response.status_code == status_map['create'][username], response.json()
+        assert response.status_code == status_map['copy'][username], response.json()
 
 
 @pytest.mark.parametrize('username,password', users)
@@ -239,7 +226,7 @@ def test_copy_wrong(db, client, username, password):
     }
     response = client.put(url, data, content_type='application/json')
 
-    if status_map['create'][username] == 201:
+    if status_map['copy'][username] == 201:
         assert response.status_code == 400, response.json()
     else:
-        assert response.status_code == status_map['create'][username], response.json()
+        assert response.status_code == status_map['copy'][username], response.json()
