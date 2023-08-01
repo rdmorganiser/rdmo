aea34287ccb312f40958b54eccb4f6a4a232ffa0
olaf michaelis <o.mic@web.de>
2023-08-01 21:03:45 +0200

    Bump version to 1.11.0

--- rdmo/__init__.py
+++ rdmo/__init__.py
@@ -1,7 +1,7 @@
-__title__ = 'rdmo'
-__version__ = '1.10.0'
-__author__ = 'RDMO Arbeitsgemeinschaft'
-__email__ = 'rdmo-team@listserv.dfn.de'
-__license__ = 'Apache-2.0'
+__title__ = "rdmo"
+__version__ = "1.11.0"
+__author__ = "RDMO Arbeitsgemeinschaft"
+__email__ = "rdmo-team@listserv.dfn.de"
+__license__ = "Apache-2.0"
 
 VERSION = __version__
