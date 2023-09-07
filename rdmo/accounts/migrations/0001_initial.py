8e40b01be43fb3528ed5d48b5268fcb17951462c
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-09-07 10:45:19 +0200

    chore: remove third-party jsonfield dependency
    Refs: #692

--- rdmo/accounts/migrations/0001_initial.py
+++ rdmo/accounts/migrations/0001_initial.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import jsonfield.fields
 from django.conf import settings
 
 
@@ -21,7 +20,7 @@ class Migration(migrations.Migration):
                 ('label', models.CharField(max_length=256)),
                 ('type', models.CharField(choices=[('text', 'Input field'), ('textarea', 'Textarea field'), ('checkbox', 'Checkbox'), ('radio', 'Radio button'), ('select', 'Select field'), ('multiselect', 'Multiselect field')], max_length=8)),
                 ('hint', models.TextField(blank=True)),
-                ('options', jsonfield.fields.JSONField(null=True, blank=True)),
+                ('options', models.JSONField(null=True, blank=True)),
                 ('required', models.BooleanField()),
             ],
         ),
@@ -29,7 +28,7 @@ class Migration(migrations.Migration):
             name='Profile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
-                ('details', jsonfield.fields.JSONField(null=True, blank=True)),
+                ('details', models.JSONField(null=True, blank=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
             ],
             options={
