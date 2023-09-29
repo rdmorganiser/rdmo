78a2a0532981d0fd0592c8fed8a1d50a73df3aff
David Wallace <david.wallace@tu-darmstadt.de>
2023-09-29 13:34:35 +0200

    test: add test for order in page in interview

--- rdmo/projects/tests/test_viewset_project_page.py
+++ rdmo/projects/tests/test_viewset_project_page.py
@@ -45,3 +45,28 @@ def test_detail(db, client, username, password, project_id, page_id):
         assert response.json().get('id') == page_id
     else:
         assert response.status_code == 404
+
+
+def test_detail_order_in_page(db, client):
+    project_id = 1
+    username = 'user'
+    ordered_page = 16
+    ordered_page_question_ids = {
+        16: [18, 19, 32, 33, 34, 89, 35, 36, 82]
+    }
+
+    client.login(username=username, password=username)
+
+    url = reverse(urlnames['detail'], args=[project_id, ordered_page])
+    response = client.get(url)
+
+    if project_id in view_questionset_permission_map.get(username, []):
+        data = response.json()
+        questions = [i for i in data['elements'] if i['model'] == "questions.question"]
+        question_ids = [i['id'] for i in questions]
+
+        assert response.status_code == 200
+        assert response.json().get('id') == ordered_page
+        assert question_ids == ordered_page_question_ids.get(ordered_page)
+    else:
+        assert response.status_code == 404
