77ff232de7c2ee4f4a2405be33458858888d8835
David Wallace <david.wallace@tu-darmstadt.de>
2023-10-05 16:36:33 +0200

    test: add test for page with nested questionsets

--- rdmo/projects/tests/test_viewset_project_page.py
+++ rdmo/projects/tests/test_viewset_project_page.py
@@ -49,7 +49,7 @@ def test_detail(db, client, username, password, project_id, page_id):
 
 def test_detail_order_in_page(db, client):
     project_id = 1
-    username = 'user'
+    username = 'owner'
     ordered_page = 16
     ordered_page_question_ids = {
         16: [18, 19, 32, 33, 34, 89, 35, 36, 82]
@@ -60,13 +60,31 @@ def test_detail_order_in_page(db, client):
     url = reverse(urlnames['detail'], args=[project_id, ordered_page])
     response = client.get(url)
 
-    if project_id in view_questionset_permission_map.get(username, []):
-        data = response.json()
-        questions = [i for i in data['elements'] if i['model'] == "questions.question"]
-        question_ids = [i['id'] for i in questions]
+    data = response.json()
+    questions = [i for i in data['elements'] if i['model'] == "questions.question"]
+    question_ids = [i['id'] for i in questions]
 
-        assert response.status_code == 200
-        assert response.json().get('id') == ordered_page
-        assert question_ids == ordered_page_question_ids.get(ordered_page)
-    else:
-        assert response.status_code == 404
+    assert response.status_code == 200
+    assert response.json().get('id') == ordered_page
+    assert question_ids == ordered_page_question_ids.get(ordered_page)
+
+
+def test_detail_page_with_nested_questionsets(db, client):
+    project_id = 1
+    username = 'owner'
+    page_id = 87
+    nested_questionsets_id = [90]
+    nested_element_ids = [95, 96, 94, 89]
+
+    client.login(username=username, password=username)
+
+    url = reverse(urlnames['detail'], args=[project_id, page_id])
+    response = client.get(url)
+
+    assert response.status_code == 200
+    data = response.json()
+    questionsets = [i for i in data['elements'] if i['model'] == "questions.questionset"]
+    questionsets_ids = [i['id'] for i in questionsets]
+    assert questionsets_ids == nested_questionsets_id
+    element_ids = [i['id'] for qs in questionsets for i in qs['elements']]
+    assert element_ids == nested_element_ids
