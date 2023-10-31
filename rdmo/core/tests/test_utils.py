01889d2ff6f7d3c3a2879e49ec18cf7854eee34e
Jochen Klar <mail@jochenklar.de>
2023-10-31 11:55:27 +0100

    Fix human2bytes utils function

--- rdmo/core/tests/test_utils.py
+++ rdmo/core/tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from rdmo.core.utils import join_url, sanitize_url
+from rdmo.core.utils import human2bytes, join_url, sanitize_url
 
 urls = (
     ('', ''),
@@ -23,3 +23,15 @@ def test_sanitize_url(url, sanitized_url):
 
 def test_join_url():
     assert join_url('https://example.com//', '/terms', 'foo') == 'https://example.com/terms/foo'
+
+
+def test_human2bytes():
+    assert human2bytes('1Gb') == 1e+9
+
+
+def test_human2bytes_none():
+    assert human2bytes(None) == 0
+
+
+def test_human2bytes_zero():
+    assert human2bytes('0') == 0
