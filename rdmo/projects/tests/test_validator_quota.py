c956aea5067597984172a345ed7fed0739655d78
Jochen Klar <mail@jochenklar.de>
2023-10-31 18:37:03 +0100

    Fix ValueQuotaValidator and add tests

---
+++ rdmo/projects/tests/test_validator_quota.py
@@ -0,0 +1,65 @@
+import pytest
+
+from rest_framework.exceptions import ValidationError as RestFameworkValidationError
+
+from ..serializers.v1 import ValueSerializer
+from ..validators import ValueQuotaValidator
+
+project_id = 1
+attribute_path = attribute__path='individual/single/text'
+
+
+def test_serializer_create_file(db):
+    class MockedProject:
+        file_size = 1
+
+    class MockedView:
+        action = 'create'
+        project = MockedProject()
+
+    validator = ValueQuotaValidator()
+    serializer = ValueSerializer()
+    serializer.context['view'] = MockedView()
+
+    validator({
+        'value_type': 'file'
+    }, serializer)
+
+
+def test_serializer_create_file_error(db, settings):
+    class MockedProject:
+        file_size = 1
+
+    class MockedView:
+        action = 'create'
+        project = MockedProject()
+
+    settings.PROJECT_FILE_QUOTA = '0'
+
+    validator = ValueQuotaValidator()
+    serializer = ValueSerializer()
+    serializer.context['view'] = MockedView()
+
+    with pytest.raises(RestFameworkValidationError):
+        validator({
+            'value_type': 'file'
+        }, serializer)
+
+
+def test_serializer_create_text(db, settings):
+    class MockedProject:
+        file_size = 1
+
+    class MockedView:
+        action = 'create'
+        project = MockedProject()
+
+    settings.PROJECT_FILE_QUOTA = '0'
+
+    validator = ValueQuotaValidator()
+    serializer = ValueSerializer()
+    serializer.context['view'] = MockedView()
+
+    validator({
+        'value_type': 'text'
+    }, serializer)
