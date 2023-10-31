e9566498249438126c5ecf49671a841f732f3eda
Jochen Klar <mail@jochenklar.de>
2023-10-31 18:43:22 +0100

    Update HUMAN2BYTES_MAPPER and add test

--- rdmo/projects/tests/test_validator_quota.py
+++ rdmo/projects/tests/test_validator_quota.py
@@ -9,13 +9,15 @@ project_id = 1
 attribute_path = attribute__path='individual/single/text'
 
 
-def test_serializer_create_file(db):
+def test_serializer_create_file(db, settings):
     class MockedProject:
         file_size = 1
 
     class MockedView:
         action = 'create'
         project = MockedProject()
+
+    settings.PROJECT_FILE_QUOTA = '1b'
 
     validator = ValueQuotaValidator()
     serializer = ValueSerializer()
@@ -62,4 +64,23 @@ def test_serializer_create_text(db, settings):
 
     validator({
         'value_type': 'text'
+    }, serializer)
+
+
+def test_serializer_update(db, settings):
+    class MockedProject:
+        file_size = 1
+
+    class MockedView:
+        action = 'update'
+        project = MockedProject()
+
+    settings.PROJECT_FILE_QUOTA = '0'
+
+    validator = ValueQuotaValidator()
+    serializer = ValueSerializer()
+    serializer.context['view'] = MockedView()
+
+    validator({
+        'value_type': 'file'
     }, serializer)
