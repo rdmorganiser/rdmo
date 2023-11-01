d65be3a8875221eb26bb744013d53c3019e34076
David Wallace <david.wallace@tu-darmstadt.de>
2023-11-01 16:46:20 +0100

    chore: refactor core import_elements

--- rdmo/management/imports.py
+++ rdmo/management/imports.py
@@ -7,6 +7,20 @@ from rdmo.questions.imports import import_catalog, import_page, import_question,
 from rdmo.tasks.imports import import_task
 from rdmo.views.imports import import_view
 
+ELEMENT_IMPORT_METHODS = {
+    "conditions.condition": import_condition,
+    "domain.attribute": import_attribute,
+    "options.optionset": import_optionset,
+    "options.option": import_option,
+    "questions.catalog": import_catalog,
+    "questions.section": import_section,
+    "questions.page": import_page,
+    "questions.questionset": import_questionset,
+    "questions.question": import_question,
+    "tasks.task": import_task,
+    "views.view": import_view,
+}
+
 
 def import_elements(elements, save=True, user=None):
     for element in elements:
@@ -19,38 +33,8 @@ def import_elements(elements, save=True, user=None):
             'updated': False
         })
 
-        if model == 'conditions.condition':
-            import_condition(element, save, user)
-
-        elif model == 'domain.attribute':
-            import_attribute(element, save, user)
-
-        elif model == 'options.optionset':
-            import_optionset(element, save, user)
-
-        elif model == 'options.option':
-            import_option(element, save, user)
-
-        elif model == 'questions.catalog':
-            import_catalog(element, save, user)
-
-        elif model == 'questions.section':
-            import_section(element, save, user)
-
-        elif model == 'questions.page':
-            import_page(element, save, user)
-
-        elif model == 'questions.questionset':
-            import_questionset(element, save, user)
-
-        elif model == 'questions.question':
-            import_question(element, save, user)
-
-        elif model == 'tasks.task':
-            import_task(element, save, user)
-
-        elif model == 'views.view':
-            import_view(element, save, user)
+        import_method = ELEMENT_IMPORT_METHODS[model]
+        import_method(element, save, user)
 
         element = filter_warnings(element, elements)
 
