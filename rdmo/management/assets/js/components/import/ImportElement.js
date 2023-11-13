779098f09a712814522b6edbf640d9bee112e9b8
David Wallace <david.wallace@tu-darmstadt.de>
2023-11-13 21:26:45 +0100

    chore: mv all import components to ImportElement

---
+++ rdmo/management/assets/js/components/import/ImportElement.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { CodeLink, WarningLink, ErrorLink, ShowLink } from '../common/Links'
+
+import Errors from './common/Errors'
+import Fields from './common/Fields'
+import Form from './common/Form'
+import Warnings from './common/Warnings'
+
+import { codeClass, verboseNames } from '../../constants/elements'
+
+const ImportElement = ({ config, instance, importActions }) => {
+  const showFields = () => importActions.updateElement(instance, {show: !instance.show})
+  const toggleImport = () => importActions.updateElement(instance, {import: !instance.import})
+  const updateInstance = (key, value) => importActions.updateElement(instance, {[key]: value})
+
+  return (
+    <li className="list-group-item">
+      <div className="pull-right">
+        <WarningLink element={instance} onClick={showFields} />
+        <ErrorLink element={instance} onClick={showFields} />
+        <ShowLink element={instance} onClick={showFields} />
+        {
+          instance.updated && !instance.created &&
+          <p className="element-link fa fa-pencil"></p>
+        }
+      </div>
+      <div className="checkbox">
+        <label className="mr-5">
+          <input type="checkbox" checked={instance.import} onChange={toggleImport} />
+          <strong>{verboseNames[instance.model]}{' '}</strong>
+        </label>
+        <CodeLink className={codeClass[instance.model]} uri={instance.uri} onClick={showFields} />
+      </div>
+      {
+        instance.show && <>
+          <Form config={config} element={instance} updateElement={updateInstance} />
+          <Fields element={instance} />
+          <Warnings element={instance} />
+          <Errors element={instance} />
+        </>
+      }
+    </li>
+  )
+}
+
+ImportElement.propTypes = {
+  config: PropTypes.object.isRequired,
+  instance: PropTypes.object.isRequired,
+  importActions: PropTypes.object.isRequired
+}
+
+export default ImportElement
