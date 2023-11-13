e6cb7b7ca808c18f6bddcbdf073a5d71aa1a0971
David Wallace <david.wallace@tu-darmstadt.de>
2023-11-13 21:26:45 +0100

    chore: mv all import components to ImportElement

--- rdmo/management/assets/js/components/main/Import.js
+++ rdmo/management/assets/js/components/main/Import.js
@@ -8,17 +8,7 @@ import { getUriPrefixes } from '../../utils/filter'
 import { FilterString, FilterUriPrefix } from '../common/Filter'
 import { Checkbox } from '../common/Checkboxes'
 
-import ImportAttribute from '../import/ImportAttribute'
-import ImportCatalog from '../import/ImportCatalog'
-import ImportCondition from '../import/ImportCondition'
-import ImportOption from '../import/ImportOption'
-import ImportOptionSet from '../import/ImportOptionSet'
-import ImportPage from '../import/ImportPage'
-import ImportQuestion from '../import/ImportQuestion'
-import ImportQuestionSet from '../import/ImportQuestionSet'
-import ImportSection from '../import/ImportSection'
-import ImportTask from '../import/ImportTask'
-import ImportView from '../import/ImportView'
+import ImportElement from '../import/ImportElement'
 
 import { codeClass, verboseNames } from '../../constants/elements'
 
@@ -93,32 +83,7 @@ const Import = ({ config, imports, importActions }) => {
               </li>
             )
           } else {
-            switch (element.model) {
-              case 'questions.catalog':
-                return <ImportCatalog key={index} config={config} catalog={element} importActions={importActions} />
-              case 'questions.section':
-                return <ImportSection key={index} config={config} section={element} importActions={importActions} />
-              case 'questions.page':
-                return <ImportPage key={index} config={config} page={element} importActions={importActions} />
-              case 'questions.questionset':
-                return <ImportQuestionSet key={index} config={config} questionset={element} importActions={importActions} />
-              case 'questions.question':
-                return <ImportQuestion key={index} config={config} question={element} importActions={importActions} />
-              case 'domain.attribute':
-                return <ImportAttribute key={index} config={config} attribute={element} importActions={importActions} />
-              case 'options.optionset':
-                return <ImportOptionSet key={index} config={config} optionset={element} importActions={importActions} />
-              case 'options.option':
-                return <ImportOption key={index} config={config} option={element} importActions={importActions} />
-              case 'conditions.condition':
-                return <ImportCondition key={index} config={config} condition={element} importActions={importActions} />
-              case 'tasks.task':
-                return <ImportTask key={index} config={config} task={element} importActions={importActions} />
-              case 'views.view':
-                return <ImportView key={index} config={config} view={element} importActions={importActions} />
-              default:
-                return null
-            }
+              return <ImportElement key={index} config={config} instance={element} importActions={importActions} />
           }
         })
       }
