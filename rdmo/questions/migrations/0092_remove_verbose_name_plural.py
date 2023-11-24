b74ee4ad48074ad44c72b069400b61898ac1e6ca
Jochen Klar <mail@jochenklar.de>
2023-11-24 12:19:33 +0100

    Fix migrations

--- rdmo/questions/migrations/0092_remove_verbose_name_plural.py
+++ rdmo/questions/migrations/0092_remove_verbose_name_plural.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('questions', '0090_add_editors'),
+        ('questions', '0091_alter_questionset_options'),
     ]
 
     operations = [
