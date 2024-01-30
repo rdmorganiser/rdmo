07963e235903c59869a7dc7b0d10a82f3be2b67b
David Wallace <david.wallace@tu-darmstadt.de>
2024-01-30 10:53:05 +0100

    refactor: move all import function calls to management, leave only ElementImportHelper
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/domain/imports.py
+++ rdmo/domain/imports.py
@@ -1,42 +1,18 @@
 import logging
-from typing import Callable, Tuple
 
 from rdmo.core.imports import (
     ElementImportHelper,
-    validate_instance,
 )
 
-from .models import Attribute
 from .serializers.v1 import BaseAttributeSerializer
 from .validators import AttributeLockedValidator, AttributeParentValidator, AttributeUniqueURIValidator
 
 logger = logging.getLogger(__name__)
 
-
-def import_attribute(
-    instance: Attribute, element: dict,
-    validators: Tuple[Callable],
-    save: bool = False) -> Attribute:
-
-    # set_foreign_field are already set in management/import.py
-    # check_permissions already done in management/import.py
-    instance.path = instance.build_path(instance.key, instance.parent)
-    validate_instance(instance, element, *validators)
-
-    if element.get('errors'):
-        return instance
-
-    if save:
-        instance.save()
-        # sites and editors are added in management/import.py
-
-    return instance
-
-
 import_helper_attribute = ElementImportHelper(
     model="domain.attribute",
-    import_func=import_attribute,
     validators=(AttributeLockedValidator, AttributeParentValidator, AttributeUniqueURIValidator),
     foreign_fields=('parent',),
+    extra_fields=('path',),
     serializer=BaseAttributeSerializer,
 )
--- rdmo/options/imports.py
+++ rdmo/options/imports.py
@@ -1,15 +1,7 @@
-import logging
-from typing import Callable, Tuple
-
 from rdmo.core.imports import (
     ElementImportHelper,
-    set_m2m_instances,
-    set_m2m_through_instances,
-    set_reverse_m2m_through_instance,
-    validate_instance,
 )
 
-from .models import Option, OptionSet
 from .serializers.v1 import OptionSerializer, OptionSetSerializer
 from .validators import (
     OptionLockedValidator,
@@ -18,67 +10,26 @@ from .validators import (
     OptionUniqueURIValidator,
 )
 
-logger = logging.getLogger(__name__)
-
-
-def import_option(
-        instance: Option,
-        element: dict,
-        validators: Tuple[Callable],
-        save: bool = False,
-    ):
-    # check_permissions already done in management/import.py
-    # extra_fields are set in in management/import.py
-    validate_instance(instance, element, *validators)
-
-    if element.get('errors'):
-        return instance
-
-    if save:
-        instance.save()
-        set_m2m_instances(instance, 'conditions', element)
-        set_m2m_through_instances(instance, 'options', element, 'optionset', 'option', 'optionset_options')
-        # sites and editors are added in management/import.py
-
-    return instance
-
-
 import_helper_option = ElementImportHelper(
     model="options.option",
-    import_func=import_option,
     validators=(OptionLockedValidator, OptionUniqueURIValidator),
     lang_fields=('text',),
     serializer = OptionSerializer,
-    extra_fields = ('order', 'provider_key', 'additional_input')
+    extra_fields = ('order', 'provider_key', 'additional_input'),
+    m2m_instance_fields = ('conditions', ),
+    m2m_through_instance_fields = [
+        {'field_name': 'options', 'source_name': 'optionset',
+         'target_name': 'option', 'through_name': 'optionset_options'}
+    ]
 )
-
-
-def import_optionset(
-        instance: OptionSet,
-        element: dict,
-        validators: Tuple[Callable],
-        save: bool = False,
-    ):
-    # lang_fields are already set in management/import.py
-    # check_permissions already done in management/import.py
-    # extra_fields are set in in management/import.py
-    validate_instance(instance, element, *validators)
-
-    if element.get('errors'):
-        return instance
-
-    if save:
-        instance.save()
-        set_reverse_m2m_through_instance(instance, 'optionset', element, 'option', 'optionset', 'option_optionsets')
-        # sites and editors are added in management/import.py
-
-    return instance
-
 
 import_helper_optionset = ElementImportHelper(
     model="options.optionset",
-    import_func=import_optionset,
     validators=(OptionSetLockedValidator, OptionSetUniqueURIValidator),
     serializer = OptionSetSerializer,
-    extra_fields=('additional_input',)
+    extra_fields=('additional_input',),
+    reverse_m2m_through_instance_fields=[
+        {'field_name': 'optionset', 'source_name': 'option',
+         'target_name': 'optionset', 'through_name': 'option_optionsets'}
+    ]
 )
