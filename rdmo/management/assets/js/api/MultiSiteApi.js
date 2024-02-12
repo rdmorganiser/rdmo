bf61f0be6ced52e8c738031c4d0f0cac535d7a6d
David Wallace <david.wallace@tu-darmstadt.de>
2024-02-12 17:32:00 +0100

    js: remove console.log

--- rdmo/management/assets/js/api/MultiSiteApi.js
+++ rdmo/management/assets/js/api/MultiSiteApi.js
@@ -6,7 +6,6 @@ class MultiSiteApi extends BaseApi {
   static UpdateElementAction(element, action) {
       const model_api_path = element.model.replace('.', '/').concat('s')
       let url = `/api/v1/${model_api_path}/${element.id}/${action}/`
-      console.log('UpdateElementAction', url, element, action, this)
       return this.put(url, element)
     }
   }
