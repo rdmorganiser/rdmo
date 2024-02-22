16c52b3f9d1213536b5a89032ecb21676021c4ea
David Wallace <david.wallace@tu-darmstadt.de>
2024-02-22 18:32:36 +0100

    chore: add custom common_fields to Attribute import helper
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/domain/imports.py
+++ rdmo/domain/imports.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 import_helper_attribute = ElementImportHelper(
     model=Attribute,
     model_path="domain.attribute",
+    common_fields=('uri_prefix', 'key', 'comment'),
     validators=(AttributeLockedValidator, AttributeParentValidator, AttributeUniqueURIValidator),
     foreign_fields=('parent',),
     extra_fields=('path',),
