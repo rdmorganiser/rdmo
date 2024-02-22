32ad357177a4e40ef5de7908d6c0960cae80432a
David Wallace <david.wallace@tu-darmstadt.de>
2024-02-22 18:28:45 +0100

    tests: refactor frontend tests and add frontend_import test
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

---
+++ rdmo/management/tests/fixtures_frontend.py
@@ -0,0 +1,50 @@
+import pytest
+
+from django.core.management import call_command
+
+from playwright.sync_api import BrowserType, Page, expect
+from pytest_django.live_server_helper import LiveServer
+
+from rdmo.accounts.utils import set_group_permissions
+
+
+@pytest.fixture(scope="function")
+def e2e_tests_django_db_setup(django_db_setup, django_db_blocker, fixtures):
+    """Set up database and populate with fixtures, that get restored for every test case."""
+    with django_db_blocker.unblock():
+        call_command("loaddata", *fixtures)
+        set_group_permissions()
+
+
+
+@pytest.fixture
+def base_url_page(live_server: LiveServer, browser: BrowserType) -> Page:
+    """Enable playwright to address URLs with base URL automatically prefixed."""
+    context = browser.new_context(base_url=live_server.url)
+    page = context.new_page()
+    yield page
+    context.close()
+
+
+# helper function for logging in the user
+def login_user(page: Page, username: str, password: str) -> Page:
+    page.goto("/account/login")
+    page.get_by_label("Username").fill(username, timeout=5000)
+    page.get_by_label("Password").fill(password)
+    page.get_by_role("button", name="Login").click()
+    page.goto("/management")
+    return page
+
+def logout_user(page: Page):
+    page.goto("/account/logout")
+    page.get_by_role("button", name="Logout").click()
+    expect(page).to_have_url('/')
+    return page
+
+@pytest.fixture(scope="function")
+def logged_in_user(e2e_tests_django_db_setup, base_url_page, username:str, password: str) -> Page:
+    """Log in as admin user through Django login UI, returns logged in page for e2e tests."""
+    # breakpoint()
+    page = login_user(base_url_page, username, password)
+    yield page
+    logout_user(page)
