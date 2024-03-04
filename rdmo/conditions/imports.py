dce66eba99aeef0a847c516d6f516523c7c3b5c3
David Wallace <david.wallace@tu-darmstadt.de>
2024-03-04 15:11:18 +0100

    chore: remove up serializer arg
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/conditions/imports.py
+++ rdmo/conditions/imports.py
@@ -1,7 +1,6 @@
 from rdmo.core.imports import ElementImportHelper
 
 from .models import Condition
-from .serializers.v1 import ConditionSerializer
 from .validators import ConditionLockedValidator, ConditionUniqueURIValidator
 
 import_helper_condition = ElementImportHelper(
@@ -9,6 +8,5 @@ import_helper_condition = ElementImportHelper(
     model_path="conditions.condition",
     validators=(ConditionLockedValidator, ConditionUniqueURIValidator),
     foreign_fields=('source', 'target_option'),
-    serializer=ConditionSerializer,
     extra_fields=('relation', 'target_text')
 )
