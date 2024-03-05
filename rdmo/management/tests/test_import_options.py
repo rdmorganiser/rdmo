5ee6b720aa5c9c319c5821df9e69651272df680d
David Wallace <david.wallace@tu-darmstadt.de>
2024-03-05 17:51:13 +0100

    tests: add test for update options in optionset
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/management/tests/test_import_options.py
+++ rdmo/management/tests/test_import_options.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import pytest
@@ -62,6 +63,46 @@ def test_update_optionsets_with_changed_fields(db, settings, updated_fields):
         assert test['updated_and_changed'] == imported['updated_and_changed']
 
 
+def test_update_optionsets_from_changed_xml(db, settings):
+    # Arrange, start test with fresh options in db
+    delete_all_objects([OptionSet, Option])
+
+    xml_file = Path(settings.BASE_DIR) / 'xml' / 'elements' / 'optionsets.xml'
+    elements, root = read_xml_and_parse_to_elements(xml_file)
+    imported_elements = import_elements(elements)
+    assert len(root) == len(imported_elements) == 13
+    # Act, import from xml that has changes
+    xml_file_1 = Path(settings.BASE_DIR) / 'xml' / 'elements' / 'updated-and-changed' / 'optionsets-1.xml'
+    elements_1, root_1 = read_xml_and_parse_to_elements(xml_file_1)
+    imported_elements_1 = import_elements(elements_1, save=False)
+
+    # Assert, compare with pre-defined json
+    json_file_1 = xml_file_1.with_suffix(".json")
+    _from_json_imported_elements_1 = json.loads(json_file_1.read_text())
+
+    # assert imported_elements_1 == from_json_imported_elements_1
+    changed_elements = [i for i in imported_elements_1 if i['changed']]
+    warnings_elements = [i for i in imported_elements_1 if i['warnings']]
+    assert len(changed_elements) == 5
+    assert len(warnings_elements) == 1
+
+    optionset_uri = "http://example.com/terms/options/one_two_three"
+    changed_uris = {i['uri']: i for i in changed_elements}
+    assert optionset_uri in changed_uris
+
+    # now save the elements_1
+    _imported_elements_1_save = import_elements(elements_1, save=True)
+    test_ordered_options = [
+        'http://example.com/terms/options/one_two_three/three',
+        'http://example.com/terms/options/one_two_three/two',
+        'http://example.com/terms/options/one_two_three/one',
+    ]
+    # get the ordered options (via .optionset_options) for this optionset from the db
+    optionset_1 = OptionSet.objects.get(uri=optionset_uri)
+    optionset_1_options = optionset_1.optionset_options.order_by('order').values_list('option__uri',flat=True)
+    for _test, _db in zip(test_ordered_options, optionset_1_options):
+        assert _test == _db
+
 def test_create_options(db, settings):
     Option.objects.all().delete()
 
@@ -106,6 +147,8 @@ def test_update_options_with_changed_fields(db, settings, updated_fields):
     # compare two ordered lists with "updated_and_changed" dicts
     for test, imported in zip(changed_elements, imported_and_changed):
         assert test['updated_and_changed'] == imported['updated_and_changed']
+
+
 
 
 def test_create_legacy_options(db, settings):
