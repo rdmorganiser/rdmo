9b62e67d03c2d4875332acec9ad9984334591c05
David Wallace <david.wallace@tu-darmstadt.de>
2024-03-05 17:52:54 +0100

    tests: refactor helper function
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/management/tests/helpers_import_elements.py
+++ rdmo/management/tests/helpers_import_elements.py
@@ -3,7 +3,7 @@ from functools import partial
 from typing import Dict, List, Optional, Tuple
 
 from rdmo.core.imports import track_changes_on_element
-from rdmo.management.imports import set_updated_and_changed_meta_info
+from rdmo.management.imports import _initialize_import_element_dict, set_element_diff_field_meta_info
 
 UPDATE_FIELD_FUNCS = {
     'comment': lambda text: f"this is a test comment {text}",
@@ -23,22 +23,23 @@ def _test_helper_filter_updated_and_changed(elements: List[Dict], updated_fields
     changed_elements = filter(filter_func, elements)
     return list(changed_elements)
 
-def _test_helper_change_fields_elements(elements, fields_to_update: Optional[Tuple]=None, n=3) -> Tuple[Dict, List]:
+
+def _test_helper_change_fields_elements(elements,
+                                        fields_to_update: Optional[Tuple] = None,
+                                        n=3) -> OrderedDict:
     """ elements test preparation function """
 
     if len(elements) < n:
         raise ValueError("Length of elements should not be smaller than n.")
     _new_elements = OrderedDict()
-    for _n,(_uri, _element) in enumerate(elements.items()):
-        if _n <= n-1:
-            _element['updated_and_changed'] = {}
-            _element['changed'] = False
-            _element['changed_fields'] = []
+    for _n, (_uri, _element) in enumerate(elements.items()):
+        if _n <= n - 1:
+            _initialize_import_element_dict(_element)
             for field in fields_to_update:
                 original_value = _element[field] or ''
                 new_val = UPDATE_FIELD_FUNCS[field](_n)
                 track_changes_on_element(_element, field, new_val, original_value=original_value)
                 _element[field] = new_val
-            set_updated_and_changed_meta_info(_element)
+            set_element_diff_field_meta_info(_element)
         _new_elements[_uri] = _element
     return _new_elements
