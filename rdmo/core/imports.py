513a7647ae05d860472cc1a41f74de63fbb871ff
David Wallace <david.wallace@tu-darmstadt.de>
2024-03-07 15:16:54 +0100

    chore: at import add a message for validation error on full_clean
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/core/imports.py
+++ rdmo/core/imports.py
@@ -529,16 +529,34 @@ def set_reverse_m2m_through_instance(instance, element, field_name=None, source_
     track_changes_on_m2m_through_instances(element, field_name, original_instance_data, new_instance_data)
 
 
+def format_message_from_validation_error(exception: ValidationError) -> str:
+    message = '; '.join(['{}: {}'.format(key, ', '.join(messages)) for key, messages in exception.message_dict.items()])
+    return message
+
+
 def validate_instance(instance, element, *validators):
     exception_message = None
-    instance.full_clean()
+    try:
+        instance.full_clean()
+    except ValidationError as e:
+        exception_message = format_message_from_validation_error(e)
+        message = '{instance_model} {instance_uri} cannot be imported ({exception}).'.format(
+            instance_model=instance._meta.object_name,
+            instance_uri=element.get('uri'),
+            exception=exception_message
+        )
+        logger.info(message)
+        _key = "FullClean"
+        element['errors'].append(message)
+        track_messages_on_element(element, _key, error=message)
+        return
+
     for validator in validators:
         try:
             validator(instance if instance.id else None)(vars(instance))
         except ValidationError as e:
             try:
-                exception_message = '; '.join(['{}: {}'.format(key, ', '.join(messages))
-                                               for key, messages in e.message_dict.items()])
+                exception_message = format_message_from_validation_error(e)
             except AttributeError:
                 exception_message = ''.join(e.messages)
         except ObjectDoesNotExist as e:
