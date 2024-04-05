877afd021cf8611f0dc7e7fcf5e4d7a71f5dd7bb
David Wallace <david.wallace@tu-darmstadt.de>
2024-04-05 17:50:01 +0200

    chore: refactor import helpers extra fields

--- rdmo/questions/imports.py
+++ rdmo/questions/imports.py
@@ -1,6 +1,7 @@
-from rdmo.core.imports import ElementImportHelper, ThroughInstanceMapper
-
+from ..core.constants import VALUE_TYPE_TEXT
+from ..core.import_helpers import ElementImportHelper, ExtraFieldDefaultHelper, ThroughInstanceMapper
 from .models import Catalog, Page, Question, QuestionSet, Section
+from .utils import get_widget_type_or_default
 from .validators import (
     CatalogLockedValidator,
     CatalogUniqueURIValidator,
@@ -19,7 +20,10 @@ import_helper_catalog = ElementImportHelper(
     model_path="questions.catalog",
     validators=(CatalogLockedValidator, CatalogUniqueURIValidator),
     lang_fields=('help', 'title'),
-    extra_fields = ('order', 'available'),
+    extra_fields = (
+        ExtraFieldDefaultHelper(field_name='order'),
+        ExtraFieldDefaultHelper(field_name='available'),
+    ),
     m2m_through_instance_fields=[
         ThroughInstanceMapper(
             field_name='sections', source_name='catalog',
@@ -54,7 +58,9 @@ import_helper_page = ElementImportHelper(
     validators=(PageLockedValidator, PageUniqueURIValidator),
     lang_fields=('help', 'title', 'verbose_name'),
     foreign_fields=('attribute',),
-    extra_fields = ('is_collection',),
+    extra_fields = (
+        ExtraFieldDefaultHelper(field_name='is_collection'),
+    ),
     m2m_instance_fields = ('conditions', ),
     m2m_through_instance_fields=[
         ThroughInstanceMapper(
@@ -74,13 +80,45 @@ import_helper_page = ElementImportHelper(
     ]
 )
 
+import_helper_question = ElementImportHelper(
+    model=Question,
+    model_path="questions.question",
+    validators=(QuestionLockedValidator, QuestionUniqueURIValidator),
+    lang_fields=('text', 'help', 'default_text', 'verbose_name'),
+    foreign_fields=('attribute', 'default_option'),
+    extra_fields=(
+        ExtraFieldDefaultHelper(field_name='is_collection'),
+        ExtraFieldDefaultHelper(field_name='is_optional'),
+        ExtraFieldDefaultHelper(field_name='default_external_id', value=''),
+        ExtraFieldDefaultHelper(field_name='widget_type', callback=get_widget_type_or_default),
+        ExtraFieldDefaultHelper(field_name='value_type', value=VALUE_TYPE_TEXT),
+        ExtraFieldDefaultHelper(field_name='minimum'),
+        ExtraFieldDefaultHelper(field_name='maximum'),
+        ExtraFieldDefaultHelper(field_name='step'),
+        ExtraFieldDefaultHelper(field_name='unit', value=''),
+        ExtraFieldDefaultHelper(field_name='width'),
+    ),
+    m2m_instance_fields=('conditions', 'optionsets'),
+    reverse_m2m_through_instance_fields=[
+        ThroughInstanceMapper(
+            field_name='page', source_name='question',
+            target_name='page', through_name='question_pages'
+        ),
+        ThroughInstanceMapper(
+            field_name='questionset', source_name='question',
+            target_name='questionset', through_name='question_questionsets'
+        )
+    ]
+)
 import_helper_questionset = ElementImportHelper(
     model = QuestionSet,
     model_path="questions.questionset",
     validators=(QuestionSetLockedValidator, QuestionSetUniqueURIValidator),
     lang_fields=('help', 'title', 'verbose_name'),
     foreign_fields=('attribute',),
-    extra_fields = ('is_collection',),
+    extra_fields=(
+        ExtraFieldDefaultHelper(field_name='is_collection'),
+    ),
     m2m_instance_fields=('conditions', ),
 
     m2m_through_instance_fields=[
@@ -101,28 +139,6 @@ import_helper_questionset = ElementImportHelper(
         ThroughInstanceMapper(
             field_name='questionset', source_name='questionset',
             target_name='parent', through_name='questionset_parents'
-        )
-    ]
-)
-
-import_helper_question = ElementImportHelper(
-    model=Question,
-    model_path="questions.question",
-    validators=(QuestionLockedValidator, QuestionUniqueURIValidator),
-    lang_fields=('text', 'help', 'default_text', 'verbose_name'),
-    foreign_fields=('attribute', 'default_option'),
-    extra_fields=('is_collection', 'is_optional', 'default_external_id',
-                  'widget_type', 'value_type', 'maximum', 'minimum', 'step',
-                  'unit', 'width'),
-    m2m_instance_fields=('conditions', 'optionsets'),
-    reverse_m2m_through_instance_fields=[
-        ThroughInstanceMapper(
-            field_name='page', source_name='question',
-            target_name='page', through_name='question_pages'
-        ),
-        ThroughInstanceMapper(
-            field_name='questionset', source_name='question',
-            target_name='questionset', through_name='question_questionsets'
         )
     ]
 )
