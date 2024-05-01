7dbfa684bdcde9b50a8b9dffc72a803b49e8cda5
Jochen Klar <mail@jochenklar.de>
2024-05-01 17:56:32 +0200

    Add error handling to react front end

--- rdmo/projects/assets/js/interview/components/main/question/widgets/SelectWidget.js
+++ rdmo/projects/assets/js/interview/components/main/question/widgets/SelectWidget.js
@@ -6,6 +6,7 @@ import { gatherOptions } from '../../../../utils/options'
 
 import QuestionAddValue from '../QuestionAddValue'
 import QuestionDefault from '../QuestionDefault'
+import QuestionError from '../QuestionError'
 import QuestionRemoveValue from '../QuestionRemoveValue'
 
 import SelectInput from './SelectInput'
@@ -36,6 +37,7 @@ const SelectWidget = ({ question, values, currentSet, disabled, createValue, upd
                 isDefault={isDefault}
                 updateValue={updateValue}
               />
+              <QuestionError value={value} />
             </div>
           )
         })
