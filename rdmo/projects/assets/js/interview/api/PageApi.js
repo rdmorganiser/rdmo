4d6bc42dba679ffd3004726ff8d45969d03927da
Jochen Klar <mail@jochenklar.de>
2024-05-03 17:17:26 +0200

    Add back flag to fetchPage action

--- rdmo/projects/assets/js/interview/api/PageApi.js
+++ rdmo/projects/assets/js/interview/api/PageApi.js
@@ -1,9 +1,15 @@
+import { isNil } from 'lodash'
+
 import BaseApi from 'rdmo/core/assets/js/api/BaseApi'
 
 class ProjectsApi extends BaseApi {
 
-  static fetchPage(projectId, pageId) {
-    return this.get(`/api/v1/projects/projects/${projectId}/pages/${pageId}`)
+  static fetchPage(projectId, pageId, back) {
+    if (isNil(back)) {
+      return this.get(`/api/v1/projects/projects/${projectId}/pages/${pageId}/`)
+    } else {
+      return this.get(`/api/v1/projects/projects/${projectId}/pages/${pageId}/?back=true`)
+    }
   }
 
   static fetchContinue(projectId) {
