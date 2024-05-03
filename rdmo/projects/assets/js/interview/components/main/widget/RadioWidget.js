8dec4312c370eb4497459091135d335274e588d4
Jochen Klar <mail@jochenklar.de>
2024-05-03 16:32:56 +0200

    Refactor styles in interview and fix range widget

--- rdmo/projects/assets/js/interview/components/main/widget/RadioWidget.js
+++ rdmo/projects/assets/js/interview/components/main/widget/RadioWidget.js
@@ -14,14 +14,14 @@ import RadioInput from './RadioInput'
 
 const RadioWidget = ({ question, values, currentSet, disabled, createValue, updateValue, deleteValue }) => {
   return (
-    <div className="interview-collection">
+    <div className="interview-widgets">
       {
         values.map((value, valueIndex) => {
           const isDefault = isDefaultValue(question, value)
 
           return (
-            <div key={valueIndex} className="interview-input">
-              <div className="interview-input-options">
+            <div key={valueIndex} className="interview-widget">
+              <div className="options">
                 <QuestionDefault isDefault={isDefault} />
                 <QuestionEraseValue value={value} disabled={disabled} updateValue={updateValue}/>
                 <QuestionRemoveValue
