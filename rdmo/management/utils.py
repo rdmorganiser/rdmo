4c245899a0bf63c43a703d55aa6e1fafed841e86
David Wallace <david.wallace@tu-darmstadt.de>
2024-05-21 12:12:20 +0200

    chore: refactor merge attributes command for #990
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/management/utils.py
+++ rdmo/management/utils.py
@@ -2,20 +2,15 @@ from rdmo.domain.models import Attribute
 
 
 def replace_uri_in_template_string(template: str, source: Attribute, target: Attribute) -> str:
-    result = []
+    new_lines = []
     path_changed = source.path != target.path
-    for n, line in enumerate(template.splitlines()):
+    for line in template.splitlines():
         new_line = line
         if source.uri in line:
             new_line = new_line.replace(source.uri, target.uri)
         if path_changed:
             if source.path in new_line:
                 new_line = new_line.replace(source.path, target.path)
-        line_result = {
-            'line_no': n,
-            'new_line': new_line,
-            'original_line': line
-            }
-        result.append(line_result)
-    new_template = "\n".join(i['new_line'] for i in result)
+        new_lines.append(new_line)
+    new_template = "\n".join(new_lines)
     return new_template
