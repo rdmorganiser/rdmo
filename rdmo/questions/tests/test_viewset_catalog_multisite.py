54c7bad8517dccc66d7b4ff17551eaa51f9a6496
David Wallace <david.wallace@tu-darmstadt.de>
2024-05-22 09:28:12 +0200

    tests: add locked parameter to update ultisite toggle tests
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/questions/tests/test_viewset_catalog_multisite.py
+++ rdmo/questions/tests/test_viewset_catalog_multisite.py
@@ -214,7 +214,8 @@ def test_detail_export(db, client, username, password, export_format):
 
 @pytest.mark.parametrize('username,password', users)
 @pytest.mark.parametrize('add_or_remove, has_current_site_check', [('add', True), ('remove', False)])
-def test_update_catalog_toggle_site(db, client, username, password, add_or_remove, has_current_site_check):
+@pytest.mark.parametrize('locked', [True, False])
+def test_update_catalog_toggle_site(db, client, username, password, add_or_remove, has_current_site_check, locked):
     client.login(username=username, password=password)
     instances = Catalog.objects.all()
     current_site = Site.objects.get_current()
@@ -225,17 +226,21 @@ def test_update_catalog_toggle_site(db, client, username, password, add_or_remov
         elif add_or_remove == 'remove':
             instance.sites.add(current_site)
 
-        before_has_current_site = instance.sites.filter(id=current_site.id).exists()
+        # locked state should not affect this toggle
+        instance.locked = locked
+        instance.save()
+
+        before_put_has_current_site = instance.sites.filter(id=current_site.id).exists()
 
         url = reverse(urlnames['catalog-toggle-site'], kwargs={'pk': instance.pk})
 
         response = client.put(url, {}, content_type='application/json')
         assert response.status_code == get_obj_perms_status_code(instance, username, 'toggle-site'), response.json()
         instance.refresh_from_db()
-        after_has_current_site = instance.sites.filter(id=current_site.id).exists()
+        after_put_has_current_site = instance.sites.filter(id=current_site.id).exists()
         if response.status_code == 200:
             # check if instance now has the current site or not
-            assert after_has_current_site is has_current_site_check
+            assert after_put_has_current_site is has_current_site_check
         else:
             # check that the instance was not updated
-            assert after_has_current_site is before_has_current_site
+            assert after_put_has_current_site is before_put_has_current_site
