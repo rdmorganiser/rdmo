9b706900fa9da2288021918501d921053ed88d32
David Wallace <david.wallace@tu-darmstadt.de>
2024-05-22 09:28:12 +0200

    tests: add locked parameter to update ultisite toggle tests
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/tasks/tests/test_viewset_task_multisite.py
+++ rdmo/tasks/tests/test_viewset_task_multisite.py
@@ -141,7 +141,8 @@ def test_detail_export(db, client, username, password, export_format):
 
 @pytest.mark.parametrize('username,password', users)
 @pytest.mark.parametrize('add_or_remove, has_current_site_check', [('add', True), ('remove', False)])
-def test_update_task_toggle_site(db, client, username, password, add_or_remove, has_current_site_check):
+@pytest.mark.parametrize('locked', [True, False])
+def test_update_task_toggle_site(db, client, username, password, add_or_remove, has_current_site_check, locked):
     client.login(username=username, password=password)
     instances = Task.objects.all()
     current_site = Site.objects.get_current()
@@ -151,6 +152,10 @@ def test_update_task_toggle_site(db, client, username, password, add_or_remove, 
             instance.sites.remove(current_site)
         elif add_or_remove == 'remove':
             instance.sites.add(current_site)
+
+        # locked state should not affect this toggle
+        instance.locked = locked
+        instance.save()
 
         before_has_current_site = instance.sites.filter(id=current_site.id).exists()
 
--- rdmo/views/tests/test_viewset_view_multisite.py
+++ rdmo/views/tests/test_viewset_view_multisite.py
@@ -131,7 +131,8 @@ def test_detail_export(db, client, username, password, export_format):
 
 @pytest.mark.parametrize('username,password', users)
 @pytest.mark.parametrize('add_or_remove, has_current_site_check', [('add', True), ('remove', False)])
-def test_update_view_toggle_site(db, client, username, password, add_or_remove, has_current_site_check):
+@pytest.mark.parametrize('locked', [True, False])
+def test_update_view_toggle_site(db, client, username, password, add_or_remove, has_current_site_check, locked):
     client.login(username=username, password=password)
     instances = View.objects.all()
     current_site = Site.objects.get_current()
@@ -141,6 +142,10 @@ def test_update_view_toggle_site(db, client, username, password, add_or_remove, 
             instance.sites.remove(current_site)
         elif add_or_remove == 'remove':
             instance.sites.add(current_site)
+
+        # locked state should not affect this toggle
+        instance.locked = locked
+        instance.save()
 
         before_has_current_site = instance.sites.filter(id=current_site.id).exists()
 
