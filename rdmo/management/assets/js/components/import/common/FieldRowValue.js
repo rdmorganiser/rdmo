bc3a77313fac9a67474e0afb4ab4bc2220bab7f5
David Wallace <david.wallace@tu-darmstadt.de>
2024-06-03 17:18:35 +0200

    chore(js): refactor Field components for import
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

---
+++ rdmo/management/assets/js/components/import/common/FieldRowValue.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import uniqueId from 'lodash/uniqueId'
+import isString from 'lodash/isString'
+import isUndefined from 'lodash/isUndefined'
+import truncate from 'lodash/truncate'
+import { codeClass } from '../../../constants/elements'
+
+const FieldRowValue = ({ value }) => (
+  <div className="col-sm-12">
+    {Array.isArray(value) && (
+      <ul className="list-unstyled mb-0">
+        {value.map((el) => (
+          <li key={uniqueId()}>
+            <code className={codeClass[el.type]}>{el.uri}</code>
+          </li>
+        ))}
+      </ul>
+    )}
+    {!isUndefined(value.uri) && <code className={codeClass[value.type]}>{value.uri}</code>}
+    {isString(value) && <span>{truncate(value, { length: 512 })}</span>}
+  </div>
+)
+
+FieldRowValue.propTypes = {
+  value: PropTypes.any.isRequired,
+}
+
+export default FieldRowValue
