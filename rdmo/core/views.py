e6356813b05b7c0f62fbc3662fc69aab9bf41d99
Jochen Klar <mail@jochenklar.de>
2024-06-03 17:50:16 +0200

    Add StoreIdViewMixin and add samesite to storeid cookie (#904)

--- rdmo/core/views.py
+++ rdmo/core/views.py
@@ -1,3 +1,4 @@
+import hashlib
 import logging
 
 from django.conf import settings
@@ -81,6 +82,18 @@ class CSRFViewMixin(View):
     @method_decorator(ensure_csrf_cookie)
     def get(self, request, *args, **kwargs):
         return super().get(self, request, *args, **kwargs)
+
+
+class StoreIdViewMixin(View):
+
+    def render_to_response(self, context, **response_kwargs):
+        response = super().render_to_response(context, **response_kwargs)
+        response.set_cookie('storeid', self.get_store_id(), samesite='Lax')
+        return response
+
+    def get_store_id(self):
+        session_key = self.request.session.session_key or 'anonymous'
+        return hashlib.sha256(session_key.encode()).hexdigest()
 
 
 class RedirectViewMixin(View):
