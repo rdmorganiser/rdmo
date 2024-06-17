1bf0d46d9f8f499ae65a17bdd0c16024f590c864
David Wallace <david.wallace@tu-darmstadt.de>
2024-06-17 17:33:53 +0200

    refactor(merge attribute): make replace_uri_in_template_string more readable
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/management/utils.py
+++ rdmo/management/utils.py
@@ -1,9 +1,9 @@
 
 def replace_uri_in_template_string(template: str, source_uri: str, target_uri: str) -> str:
-    patterns = [f"'{source_uri}'", f'"{source_uri}"']
-    replacements = [f"'{target_uri}'", f'"{target_uri}"']
-
-    new_template = template
-    for pattern, replacement in zip(patterns, replacements):
-        new_template = new_template.replace(pattern, replacement)
-    return new_template
+    replacements = [
+        (f"'{source_uri}'", f"'{target_uri}'"),
+        (f'"{source_uri}"', f'"{target_uri}"'),
+    ]
+    for pattern, replacement in replacements:
+        template = template.replace(pattern, replacement)
+    return template
