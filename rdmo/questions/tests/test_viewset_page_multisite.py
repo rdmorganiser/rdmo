173f3f0a78c3fc78b2a54b46824def321d6e5a2e
David Wallace <david.wallace@tu-darmstadt.de>
2024-06-17 09:37:39 +0200

    tests(multisite): update imports for multisite tests
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/questions/tests/test_viewset_page_multisite.py
+++ rdmo/questions/tests/test_viewset_page_multisite.py
@@ -5,9 +5,9 @@ import pytest
 from django.db.models import Max
 from django.urls import reverse
 
-from rdmo.core.tests import get_obj_perms_status_code
-from rdmo.core.tests import multisite_status_map as status_map
-from rdmo.core.tests import multisite_users as users
+from rdmo.core.tests.constants import multisite_status_map as status_map
+from rdmo.core.tests.constants import multisite_users as users
+from rdmo.core.tests.utils import get_obj_perms_status_code
 
 from ..models import Page
 from .test_viewset_page import export_formats, urlnames
