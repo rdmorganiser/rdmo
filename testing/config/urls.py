24df76b766eb6f877c967d22ce0e1cb6d1449b1e
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2024-07-11 18:14:35 +0200

    feat: replace django-rest-swagger with drf-spectacular

--- testing/config/urls.py
+++ testing/config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from rdmo.core.views import about, home
 
@@ -8,8 +8,10 @@ urlpatterns = [
     path('about/', about, name='about'),
 
     path('', include('rdmo.core.urls')),
+    # re_path(r'^api/(?P<version>(v1))/', include('rdmo.core.urls.v1')),
     path('api/v1/', include('rdmo.core.urls.v1')),
-    path('api/v1/', include('rdmo.core.urls.swagger')),
+    re_path(r'^api/(?P<version>(v1))/', include('rdmo.core.urls.openapi')),
+    # path('api/v1/', include('rdmo.core.urls.openapi', namespace='v1')),
 
     path('admin/', admin.site.urls),
 ]
