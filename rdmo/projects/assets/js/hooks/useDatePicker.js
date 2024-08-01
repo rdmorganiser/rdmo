fa62ba9ae47888e9f2aa210cf2e81122700fbcb9
Jochen Klar <mail@jochenklar.de>
2024-08-01 10:09:28 +0200

    Use lodash camelCase in date picker hook

--- rdmo/projects/assets/js/hooks/useDatePicker.js
+++ rdmo/projects/assets/js/hooks/useDatePicker.js
@@ -1,51 +1,49 @@
 import { useState, useMemo, useCallback } from 'react'
 import { de } from 'date-fns/locale'
+import { camelCase } from 'lodash'
 import { language } from 'rdmo/core/assets/js/utils'
 
 const useDatePicker = () => {
-    const [dateRange, setDateRange] = useState({
-        createdStart: null,
-        createdEnd: null,
-        lastChangedStart: null,
-        lastChangedEnd: null
-    })
+  const [dateRange, setDateRange] = useState({
+    createdStart: null,
+    createdEnd: null,
+    lastChangedStart: null,
+    lastChangedEnd: null
+  })
 
-    const dateFormat = useMemo(() => {
-        return language === 'de' ? 'dd.MM.yyyy' : 'MM/dd/yyyy'
-    }, [])
+  const dateFormat = useMemo(() => {
+    return language === 'de' ? 'dd.MM.yyyy' : 'MM/dd/yyyy'
+  }, [])
 
-    const getLocale = useCallback((lang) => {
-        switch (lang) {
-            case 'de':
-                return de
-            default:
-                return undefined // defaults to English
-        }
-    }, [])
-
-    const convertToCamelCase = (input) => {
-      return input.replace(/_([a-z])/g, (match, letter) => letter.toUpperCase())
-  }
+  const getLocale = useCallback((lang) => {
+    switch (lang) {
+      case 'de':
+        return de
+      default:
+        return undefined // defaults to English
+    }
+  }, [])
 
   const setStartDate = useCallback((type, date) => {
-      const camelCaseType = convertToCamelCase(type)
-      const startKey = `${camelCaseType}Start`
-      setDateRange(prev => ({ ...prev, [startKey]: date }))
+    const camelCaseType = camelCase(type)
+    console.log(camelCaseType)
+    const startKey = `${camelCaseType}Start`
+    setDateRange(prev => ({ ...prev, [startKey]: date }))
   }, [])
 
   const setEndDate = useCallback((type, date) => {
-      const camelCaseType = convertToCamelCase(type)
-      const endKey = `${camelCaseType}End`
-      setDateRange(prev => ({ ...prev, [endKey]: date }))
+    const camelCaseType = camelCase(type)
+    const endKey = `${camelCaseType}End`
+    setDateRange(prev => ({ ...prev, [endKey]: date }))
   }, [])
 
-    return {
-        dateRange,
-        dateFormat,
-        getLocale,
-        setStartDate,
-        setEndDate
-    }
+  return {
+    dateRange,
+    dateFormat,
+    getLocale,
+    setStartDate,
+    setEndDate
+  }
 }
 
 export default useDatePicker
