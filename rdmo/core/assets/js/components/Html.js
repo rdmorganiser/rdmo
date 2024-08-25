b2815b5a463e7ead545908c77ac737dbfee281f5
Jochen Klar <mail@jochenklar.de>
2024-08-25 13:24:24 +0200

    Remove defaultProps from Html.js

--- rdmo/core/assets/js/components/Html.js
+++ rdmo/core/assets/js/components/Html.js
@@ -2,14 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { isEmpty } from 'lodash'
 
-const Html = ({ html }) => {
+const Html = ({ html = '' }) => {
   return !isEmpty(html) && (
     <div dangerouslySetInnerHTML={{ __html: html }} />
   )
-}
-
-Html.defaultProps = {
-  className: ''
 }
 
 Html.propTypes = {
