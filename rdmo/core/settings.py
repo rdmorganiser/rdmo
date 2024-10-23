1cd467562a06172634a3872a90bd77fdd1e50416
Axel Nieder-Vahrenholz <nieder-vahrenholz@dipf.de>
2024-10-23 09:11:02 +0200

    set data type for MARKDOWN_TEMPLATES

--- rdmo/core/settings.py
+++ rdmo/core/settings.py
@@ -552,4 +552,6 @@ VENDOR = {
 # necessary since django 3.2, explicitly set primary key type to avoid warnings
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-MARKDOWN_TEMPLATES = {}
+MARKDOWN_TEMPLATES: dict[str, str] = {
+    # for example: 'not_empty': 'core/text_blocks/template_for_not_empty.html',
+}
