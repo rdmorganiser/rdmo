0a6abff6f319b5f9745257ad4ab0793952ccbdba
Jochen Klar <mail@jochenklar.de>
2024-11-10 18:50:47 +0100

    Fix QuestionSetCopySet component

--- rdmo/projects/assets/js/interview/components/main/questionset/QuestionSetRemoveSet.js
+++ rdmo/projects/assets/js/interview/components/main/questionset/QuestionSetRemoveSet.js
@@ -8,23 +8,23 @@ import QuestionSetDeleteModal from './QuestionSetDeleteModal'
 
 const QuestionRemoveSet = ({ questionset, currentSet, deleteSet }) => {
 
-  const {show: showDeleteModal, open: openDeleteModal, close: closeDeleteModal} = useModal()
+  const modal = useModal()
 
   const handleDeleteSet = () => {
     deleteSet(currentSet)
-    closeDeleteModal()
+    modal.close()
   }
 
   return questionset.is_collection && (
     <>
-      <button type="button" className="btn btn-link btn-remove-set" onClick={openDeleteModal}>
+      <button type="button" className="btn btn-link btn-remove-set" onClick={modal.open}>
         <i className="fa fa-times fa-btn"></i>
       </button>
 
       <QuestionSetDeleteModal
         title={capitalize(questionset.verbose_name)}
-        show={showDeleteModal}
-        onClose={closeDeleteModal}
+        show={modal.show}
+        onClose={modal.close}
         onSubmit={handleDeleteSet}
       />
     </>
