fbbe3dc52d9a69318c5e660ce14e7f17bac72e98
Jochen Klar <mail@jochenklar.de>
2024-11-14 14:55:29 +0100

    Add project_interview_sidebar template

--- rdmo/core/settings.py
+++ rdmo/core/settings.py
@@ -233,6 +233,7 @@ TEMPLATES_API = [
     'projects/project_interview_progress_help.html',
     'projects/project_interview_question_help.html',
     'projects/project_interview_questionset_help.html',
+    'projects/project_interview_sidebar.html',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
