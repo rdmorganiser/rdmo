f199bc61519af1849a1784c0b358e6bbd6125515
Jochen Klar <mail@jochenklar.de>
2025-01-17 16:13:30 +0100

    Change contact badge to button

--- rdmo/projects/assets/js/interview/components/main/question/QuestionContact.js
+++ rdmo/projects/assets/js/interview/components/main/question/QuestionContact.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 
 const QuestionContact = ({ settings, question, values, fetchContact }) => {
   return settings.project_contact && (
-    <button className="btn btn-link badge badge-contact" title={gettext('Contact support.')}
+    <button className="btn btn-link btn-contact" title={gettext('Contact support.')}
             onClick={() => fetchContact({ question, values })}>
-      <i className="fa fa-question" aria-hidden="true"></i>
+      <i className="fa fa-commenting-o" aria-hidden="true"></i>
     </button>
   )
 }
