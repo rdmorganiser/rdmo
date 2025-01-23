bdb6f7e955c8c5126f74eda17bb62c81fcb88f91
David Wallace <david.wallace@tu-darmstadt.de>
2025-01-23 09:54:15 +0100

    refactor(project,handlers): simplify pre and post save
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/projects/handlers/project_save_tasks.py
+++ rdmo/projects/handlers/project_save_tasks.py
@@ -8,26 +8,24 @@ DEFERRED_SYNC_TASKS_KEY = '_deferred_sync_tasks'
 
 @receiver(pre_save, sender=Project)
 def pre_save_project_sync_tasks_from_catalog(sender, instance, raw, update_fields, **kwargs):
-    if (raw or
-        instance.id is None or
-        (update_fields and 'catalog' not in update_fields)
-    ):
+    if raw or (update_fields and 'catalog' not in update_fields):
         return
 
-    # Fetch the original catalog from the database
-    original_instance = sender.objects.get(id=instance.id)
-    if original_instance.catalog == instance.catalog:
-        # Do nothing if the catalog has not changed
-        return
+    if instance.id is not None:
+        # Fetch the original catalog from the database
+        if sender.objects.get(id=instance.id).catalog == instance.catalog:
+            # Do nothing if the catalog has not changed
+            return
 
     # Defer synchronization of views
     setattr(instance, DEFERRED_SYNC_TASKS_KEY, True)
 
+
 @receiver(post_save, sender=Project)
-def post_save_project_sync_tasks_from_catalog(sender, instance, created, raw, **kwargs):
-    if not hasattr(instance, DEFERRED_SYNC_TASKS_KEY):
+def post_save_project_sync_tasks_from_catalog(sender, instance, created, raw, update_fields, **kwargs):
+    if raw or (update_fields and 'catalog' not in update_fields):
         return
-    if getattr(instance, DEFERRED_SYNC_TASKS_KEY, None) or (created and not raw):
-        # For existing projects with catalog changes, use deferred views
-        instance.views.set(Task.objects.filter_available_tasks_for_project(instance))
+
+    if hasattr(instance, DEFERRED_SYNC_TASKS_KEY):
+        instance.tasks.set(Task.objects.filter_available_tasks_for_project(instance).values_list('id', flat=True))
         delattr(instance, DEFERRED_SYNC_TASKS_KEY)
--- rdmo/projects/handlers/project_save_views.py
+++ rdmo/projects/handlers/project_save_views.py
@@ -8,26 +8,23 @@ DEFERRED_SYNC_VIEWS_KEY = '_deferred_sync_views'
 
 @receiver(pre_save, sender=Project)
 def pre_save_project_sync_views_from_catalog(sender, instance, raw, update_fields, **kwargs):
-    if (raw or
-        instance.id is None or
-        (update_fields and 'catalog' not in update_fields)
-    ):
+    if raw or (update_fields and 'catalog' not in update_fields):
         return
 
-    # Fetch the original catalog from the database
-    original_instance = sender.objects.get(id=instance.id)
-    if original_instance.catalog == instance.catalog:
-        # Do nothing if the catalog has not changed
-        return
+    if instance.id is not None:
+        # Fetch the original catalog from the database
+        if sender.objects.get(id=instance.id).catalog == instance.catalog:
+            # Do nothing if the catalog has not changed
+            return
 
     # Defer synchronization of views
     setattr(instance, DEFERRED_SYNC_VIEWS_KEY, True)
 
 @receiver(post_save, sender=Project)
 def post_save_project_sync_views_from_catalog(sender, instance, created, raw, update_fields, **kwargs):
-    if not hasattr(instance, DEFERRED_SYNC_VIEWS_KEY):
+    if raw or (update_fields and 'catalog' not in update_fields):
         return
-    if getattr(instance, DEFERRED_SYNC_VIEWS_KEY, None) or (created and not raw):
-        # For existing projects with catalog changes, use deferred views
-        instance.views.set(View.objects.filter_available_views_for_project(instance))
+
+    if hasattr(instance, DEFERRED_SYNC_VIEWS_KEY):
+        instance.views.set(View.objects.filter_available_views_for_project(instance).values_list('id', flat=True))
         delattr(instance, DEFERRED_SYNC_VIEWS_KEY)
