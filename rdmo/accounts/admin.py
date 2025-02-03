0d694f7036829d55137050729ea944d4f6915ec5
David Wallace <david.wallace@tu-darmstadt.de>
2025-02-03 11:00:13 +0100

    feat(accounts,admin): update list display for ConsentFieldValue
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/accounts/admin.py
+++ rdmo/accounts/admin.py
@@ -19,6 +19,10 @@ class AdditionalFieldValueAdmin(admin.ModelAdmin):
 class ConsentFieldValueAdmin(admin.ModelAdmin):
     readonly_fields = ('user', 'consent', 'updated', 'created')
 
+    search_fields = ('user__username', 'user__email')
+    list_display = ('user',  'consent', 'updated')
+    list_filter = ('consent',)
+
     def has_add_permission(self, request, obj=None):
         return False
 
