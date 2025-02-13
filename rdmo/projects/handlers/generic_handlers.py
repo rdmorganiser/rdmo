ecefbafc156cbbcc52df02f37d6526cde76ce966
David Wallace <david.wallace@tu-darmstadt.de>
2025-02-13 16:11:28 +0100

    refactor(project,handlers): move add and remove inline to the generic handlers
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/projects/handlers/generic_handlers.py
+++ rdmo/projects/handlers/generic_handlers.py
@@ -2,9 +2,7 @@ from django.contrib.auth.models import Group, User
 from django.contrib.sites.models import Site
 
 from rdmo.projects.models import Membership, Project
-
-from ...questions.models import Catalog
-from .utils import add_instance_to_projects, remove_instance_from_projects
+from rdmo.questions.models import Catalog
 
 
 def m2m_catalogs_changed_projects_sync_signal_handler(action, pk_set, instance, project_field):
@@ -22,18 +20,21 @@ def m2m_catalogs_changed_projects_sync_signal_handler(action, pk_set, instance, 
         projects_to_change = Project.objects.filter_catalogs(catalogs=related_instances).filter(
             **{project_field: instance}
         )
-        remove_instance_from_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # remove instance from project
+            getattr(project, project_field).remove(instance)
 
     elif action == 'post_clear':
         projects_to_change = Project.objects.filter(**{project_field: instance})
-        remove_instance_from_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # remove instance from project
+            getattr(project, project_field).remove(instance)
 
     elif action == 'post_add' and pk_set:
         related_instances = Catalog.objects.filter(pk__in=pk_set)
         projects_to_change = Project.objects.filter_catalogs(catalogs=related_instances).exclude(
             **{project_field: instance}
         )
-        add_instance_to_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # add instance to project
+            getattr(project, project_field).add(instance)
 
 
 def m2m_sites_changed_projects_sync_signal_handler(action, pk_set, instance, project_field):
@@ -54,11 +55,13 @@ def m2m_sites_changed_projects_sync_signal_handler(action, pk_set, instance, pro
             site__in=related_sites,
             **{project_field: instance}
         )
-        remove_instance_from_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # remove instance from project
+            getattr(project, project_field).remove(instance)
 
     elif action == 'post_clear':
         projects_to_change = Project.objects.filter_catalogs().filter(**{project_field: instance})
-        remove_instance_from_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # remove instance from project
+            getattr(project, project_field).remove(instance)
 
     elif action == 'post_add' and pk_set:
         related_sites = Site.objects.filter(pk__in=pk_set)
@@ -67,7 +70,8 @@ def m2m_sites_changed_projects_sync_signal_handler(action, pk_set, instance, pro
         projects_to_change = Project.objects.filter_catalogs(catalogs=catalogs).filter(
             site__in=related_sites
         ).exclude(**{project_field: instance})
-        add_instance_to_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # add instance to project
+            getattr(project, project_field).add(instance)
 
 
 def m2m_groups_changed_projects_sync_signal_handler(action, pk_set, instance, project_field):
@@ -90,12 +94,14 @@ def m2m_groups_changed_projects_sync_signal_handler(action, pk_set, instance, pr
             memberships__in=memberships,
             **{project_field: instance}
         )
-        remove_instance_from_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # remove instance from project
+            getattr(project, project_field).remove(instance)
 
     elif action == 'post_clear':
         # Remove all linked projects regardless of catalogs
         projects_to_change = Project.objects.filter_catalogs().filter(**{project_field: instance})
-        remove_instance_from_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # remove instance from project
+            getattr(project, project_field).remove(instance)
 
     elif action == 'post_add' and pk_set:
         related_groups = Group.objects.filter(pk__in=pk_set)
@@ -106,4 +112,5 @@ def m2m_groups_changed_projects_sync_signal_handler(action, pk_set, instance, pr
         projects_to_change = Project.objects.filter_catalogs(catalogs=catalogs).filter(
             memberships__in=memberships
         ).exclude(**{project_field: instance})
-        add_instance_to_projects(projects_to_change, project_field, instance)
+        for project in projects_to_change:  # add instance to project
+            getattr(project, project_field).add(instance)
