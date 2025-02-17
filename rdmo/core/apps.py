3aa760a1bc57f4826cffd8a3eb175d53cc7e61c0
David Wallace <david.wallace@tu-darmstadt.de>
2025-02-17 15:16:47 +0100

    refactor(core): rename to CoreConfig and remove ready method
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/core/apps.py
+++ rdmo/core/apps.py
@@ -2,9 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class AccountsConfig(AppConfig):
+class CoreConfig(AppConfig):
     name = 'rdmo.core'
     verbose_name = _('Core')
-
-    def ready(self):
-        pass
