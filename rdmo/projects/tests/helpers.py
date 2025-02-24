fdb479c6e929a8c60a5a83c212c6838e01283011
David Wallace <david.wallace@tu-darmstadt.de>
2025-02-24 15:46:43 +0100

    style(ruff): remove noqa for PT004
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/projects/tests/helpers.py
+++ rdmo/projects/tests/helpers.py
@@ -9,12 +9,12 @@ from rdmo.views.models import View
 
 
 @pytest.fixture
-def enable_project_views_sync(settings):  # noqa:PT004
+def enable_project_views_sync(settings):
     settings.PROJECT_VIEWS_SYNC = True
     apps.get_app_config('projects').ready()
 
 @pytest.fixture
-def enable_project_tasks_sync(settings):  # noqa:PT004
+def enable_project_tasks_sync(settings):
     settings.PROJECT_TASKS_SYNC = True
     apps.get_app_config('projects').ready()
 
