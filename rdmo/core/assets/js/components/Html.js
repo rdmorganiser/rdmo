201abfe9bad54850fe13d2b194d01b3ed26d18a2
Jochen Klar <mail@jochenklar.de>
2025-02-25 16:57:45 +0100

    Update Html component

--- rdmo/core/assets/js/components/Html.js
+++ rdmo/core/assets/js/components/Html.js
@@ -10,19 +10,21 @@ const Html = ({ html = '' }) => {
 
   // if html contains a <script> tag, and settings.TEMPLATES_EXECUTE_SCRIPT_TAGS is True,
   // it will be executed using the method in https://macarthur.me/posts/script-tags-in-react/
-  useLayoutEffect(() => {
-    if (ref.current && executeScriptTags) {
-      // create a range objectm, set it contain the referenced node
-      // and create a new fragment with the html code within that range
-      const range = document.createRange()
-      range.selectNode(ref.current)
-      const documentFragment = range.createContextualFragment(html)
+  if (executeScriptTags) {
+    useLayoutEffect(() => {
+      if (ref.current) {
+        // create a range objectm, set it contain the referenced node
+        // and create a new fragment with the html code within that range
+        const range = document.createRange()
+        range.selectNode(ref.current)
+        const documentFragment = range.createContextualFragment(html)
 
-      // remove the rendered html and inject it again, triggering a re-run of the JS code
-      ref.current.innerHTML = ''
-      ref.current.append(documentFragment)
-    }
-  }, [html])
+        // remove the rendered html and inject it again, triggering a re-run of the JS code
+        ref.current.innerHTML = ''
+        ref.current.append(documentFragment)
+      }
+    }, [html])
+  }
 
   return !isEmpty(html) && (
     <div ref={ref} dangerouslySetInnerHTML={{ __html: html }} />
