8936de4795894d0a42d69961b44551e41431f43d
Jochen Klar <mail@jochenklar.de>
2025-03-20 10:11:03 +0100

    Improve messages management script

--- rdmo/core/management/commands/messages.py
+++ rdmo/core/management/commands/messages.py
@@ -7,11 +7,29 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('command', choices=['make', 'compile'])
+        parser.add_argument('-l', '--locale')
 
     def handle(self, *args, **options):
         if options['command'] == 'make':
-            subprocess.check_call(['django-admin', 'makemessages', '--all'], cwd='rdmo')
-            subprocess.check_call(['django-admin', 'makemessages', '--all', '-d', 'djangojs'], cwd='rdmo')
-
+            self.handle_make(self, *args, **options)
         elif options['command'] == 'compile':
-            subprocess.check_call(['django-admin', 'compilemessages'], cwd='rdmo')
+            self.handle_compile(self, *args, **options)
+
+    def handle_make(self, *args, **options):
+        call_args = ['django-admin', 'makemessages']
+
+        if options['locale'] is None:
+            call_args.append('--all')
+        else:
+            call_args.append('-l {locale}'.format(**options))
+
+        subprocess.check_call(call_args, cwd='rdmo')
+        subprocess.check_call([*call_args, '-d', 'djangojs'], cwd='rdmo')
+
+    def handle_compile(self, *args, **options):
+        call_args = ['django-admin', 'compilemessages']
+
+        if options['locale'] is not None:
+            call_args.append('-l {locale}'.format(**options))
+
+        subprocess.check_call(call_args, cwd='rdmo')
