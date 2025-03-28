fc672514da94855b61fc9e40e2053fff2584efde
Jochen Klar <mail@jochenklar.de>
2025-03-28 10:56:26 +0100

    Remove non-working locale option from messages management script

--- rdmo/core/management/commands/messages.py
+++ rdmo/core/management/commands/messages.py
@@ -7,7 +7,6 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('command', choices=['make', 'compile'])
-        parser.add_argument('-l', '--locale')
 
     def handle(self, *args, **options):
         if options['command'] == 'make':
@@ -16,20 +15,10 @@ class Command(BaseCommand):
             self.handle_compile(self, *args, **options)
 
     def handle_make(self, *args, **options):
-        call_args = ['django-admin', 'makemessages']
-
-        if options['locale'] is None:
-            call_args.append('--all')
-        else:
-            call_args.append('-l {locale}'.format(**options))
-
+        call_args = ['django-admin', 'makemessages', '--all']
         subprocess.check_call(call_args, cwd='rdmo')
         subprocess.check_call([*call_args, '-d', 'djangojs'], cwd='rdmo')
 
     def handle_compile(self, *args, **options):
         call_args = ['django-admin', 'compilemessages']
-
-        if options['locale'] is not None:
-            call_args.append('-l {locale}'.format(**options))
-
         subprocess.check_call(call_args, cwd='rdmo')
