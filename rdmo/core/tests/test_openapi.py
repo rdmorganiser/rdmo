dc69ac831c2f175e80b300c40349b5f7494faa7f
Jochen Klar <mail@jochenklar.de>
2025-04-29 18:16:15 +0200

    Fix test

--- rdmo/core/tests/test_openapi.py
+++ rdmo/core/tests/test_openapi.py
@@ -21,7 +21,7 @@ def test_openapi_schema(db, client, login, settings, username):
         assert response.status_code == 200
         schema = yaml.safe_load(response.content)
         assert schema['openapi'] == '3.0.3'
-        assert len(schema['paths']) == 123
+        assert len(schema['paths']) == 124
     else:
         assert response.status_code == 302
 
