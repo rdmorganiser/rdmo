50cf3ca29760c477e7e76d6f5c5863a088611d3b
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-05-08 12:42:56 +0200

    * add errors * add TODOs

--- rdmo/projects/assets/js/project/components/pages/ProjectData.js
+++ rdmo/projects/assets/js/project/components/pages/ProjectData.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 import { Tile } from '../helper'
 import ProjectForm from './ProjectForm'
 import ProjectDelete from './ProjectDelete'
+import { getUserRoles, userIsManager } from 'rdmo/projects/assets/js/common/utils'
 
 const ProjectData = () => {
+  const { project } = useSelector((state) => state.project.project)
+  const currentUser = useSelector((state) => state.user.currentUser)
+  const allowed = userIsManager(currentUser) || getUserRoles(project, currentUser.id, ['owners']).isProjectOwner
+
   return (
 
     <div className="container-fluid px-3">
@@ -14,11 +19,13 @@ const ProjectData = () => {
       </Tile>
     </div>
 
-    <div className="row g-3">
-      <Tile size="fullWidth" style="warning">
-        <ProjectDelete />
-      </Tile>
-    </div>
+    {allowed && (
+        <div className="row g-3">
+          <Tile size="fullWidth" style="warning">
+            <ProjectDelete />
+          </Tile>
+        </div>
+    )}
   </div>
 
   )
