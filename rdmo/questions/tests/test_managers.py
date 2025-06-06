92e565620f215909281330999eb4d362a9d8f42d
Jochen Klar <mail@jochenklar.de>
2025-06-06 11:38:18 +0200

    Update test (two more questions, two more values per set)

--- rdmo/questions/tests/test_managers.py
+++ rdmo/questions/tests/test_managers.py
@@ -4,4 +4,4 @@ from ..models import Catalog, Question
 def test_questions_filter_by_catalog(db):
     catalog = Catalog.objects.prefetch_elements().first()
     questions = Question.objects.filter_by_catalog(catalog)
-    assert questions.count() == 104
+    assert questions.count() == 106
