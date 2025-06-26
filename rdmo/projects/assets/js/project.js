7af6f4c2e9a4ddc97bbdf114960cb21b2fbda421
Jochen Klar <mail@jochenklar.de>
2025-06-26 10:00:06 +0200

    Refactor directory layout for project react app and refactor sidebar

--- rdmo/projects/assets/js/project.js
+++ rdmo/projects/assets/js/project.js
@@ -7,14 +7,14 @@ import configureStore from './project/store/configureStore'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-import Main from './project/containers/Main'
+import Project from './project/components/Project'
 
 const store = configureStore()
 
 createRoot(document.getElementById('main')).render(
   <DndProvider backend={HTML5Backend}>
     <Provider store={store}>
-      <Main />
+      <Project />
     </Provider>
   </DndProvider>
 )
---
+++ rdmo/projects/assets/js/project/components/ProjectSidebar.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+
+import ProjectBadge from './helper/ProjectBadge'
+
+const ProjectSidebar = ({ menuItems, onNavigate, activePage }) => {
+  return (
+    <div className="project-sidebar p-2">
+      <ProjectBadge />
+
+      <ul className="nav nav-pills flex-column mb-auto">
+        {menuItems.map((section) => (
+          <div key={section.title}>
+            {section.title && <h6 className="text-muted mt-3 mb-2">{section.title}</h6>}
+
+            {section.items.map((item) => (
+              <li key={item.id} className="nav-item">
+                <button
+                  className={classnames('nav-link w-100 text-start d-flex align-items-center gap-2', {
+                    active: activePage === item.id
+                  })}
+                  onClick={() => {
+                    onNavigate(item.id)
+                  }}
+                >
+                  <i className={`bi ${item.icon}`}></i>
+                  {item.name}
+                </button>
+              </li>
+            ))}
+          </div>
+        ))}
+      </ul>
+
+      <div className="p-3 mt-auto">
+        {/* <a href="/projects" className="nav-link text-dark w-100 text-start d-flex align-items-center gap-2"> */}
+        <a href="/projects" className="nav-link text-light w-100 text-start d-flex align-items-center gap-2">
+
+          <i className="bi bi-arrow-left"></i> {gettext('Back to projects overview')}
+        </a>
+      </div>
+    </div>
+  )
+}
+
+ProjectSidebar.propTypes = {
+  menuItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      items: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          name: PropTypes.string.isRequired,
+          icon: PropTypes.string,
+        })
+      ).isRequired,
+    })
+  ).isRequired,
+  onNavigate: PropTypes.func.isRequired,
+  activePage: PropTypes.string.isRequired,
+}
+
+export default ProjectSidebar
