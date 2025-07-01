d6e91c1a93d02f4f78e10418b8ce8c7b64b07f41
Jochen Klar <mail@jochenklar.de>
2025-07-01 17:44:15 +0200

    Split base and bootstrap js and css

--- rdmo/core/assets/js/_bs53/base.js
+++ rdmo/core/assets/js/_bs53/base.js
@@ -1,1 +1,14 @@
-import 'bootstrap'
+window.loopHeaderBackgroundImage = (timeout) => {
+    const images = document.querySelectorAll('#header .header-image')
+
+    let index = 0
+
+    const setHeaderBackgroundImage = () => {
+        images[index].classList.remove('header-image-visible')
+        index = (index == images.length - 1) ? 0 : index + 1
+        images[index].classList.toggle('header-image-visible')
+        setTimeout(() => setHeaderBackgroundImage(), timeout)
+    }
+
+    setTimeout(() => setHeaderBackgroundImage(), timeout)
+}
