7184c626f0a9c50688ca4d2fc6efe6ecf699fe2d
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-08-19 20:52:50 +0200

    * add invites and memberships API calls * use it in Membership components

--- rdmo/projects/assets/js/project/actions/projectActions.js
+++ rdmo/projects/assets/js/project/actions/projectActions.js
@@ -12,36 +12,22 @@ import {
   UPDATE_PROJECT_ERROR,
   DELETE_PROJECT_INIT,
   DELETE_PROJECT_SUCCESS,
-  DELETE_PROJECT_ERROR
+  DELETE_PROJECT_ERROR,
+  FETCH_PROJECT_INVITES_INIT,
+  FETCH_PROJECT_INVITES_SUCCESS,
+  FETCH_PROJECT_INVITES_ERROR,
+  ADD_PROJECT_MEMBER_INIT,
+  ADD_PROJECT_MEMBER_SUCCESS,
+  ADD_PROJECT_MEMBER_ERROR,
+  SEND_INVITE_INIT,
+  SEND_INVITE_SUCCESS,
+  SEND_INVITE_ERROR
 } from './actionTypes'
 
 import { addToPending, removeFromPending } from 'rdmo/core/assets/js/actions/pendingActions'
 import { updateConfig } from 'rdmo/core/assets/js/actions/configActions'
 
 import { updateLocation } from '../utils/location'
-
-// export function fetchProject() {
-//   return function(dispatch) {
-//     dispatch(addToPending('fetchProject'))
-//     dispatch(fetchProjectInit())
-
-//     return Promise.all([
-//       ProjectApi.fetchProject(projectId),
-//       ProjectApi.fetchProjectSnapshots(projectId)
-
-//     ])
-//     .then(([project, snapshots]) => {
-//       project.snapshots = snapshots
-//       dispatch(removeFromPending('fetchProject'))
-//       dispatch(fetchProjectSuccess(project))
-//     })
-//     .catch(error => {
-//       dispatch(removeFromPending('fetchProject'))
-//       dispatch(fetchProjectError(error))
-//     })
-//   }
-// }
-
 
 export function setPage(page) {
   return function(dispatch) {
@@ -59,15 +45,17 @@ export function fetchProject() {
       ProjectApi.fetchProject(projectId),
       ProjectApi.fetchProjectSnapshots(projectId),
       ProjectApi.fetchProjectTasks(projectId),
+      ProjectApi.fetchProjectMemberships(projectId),
       CatalogsApi.fetchCatalogs()
     ])
-    .then(([project, snapshots, tasks, catalogs]) => {
+    .then(([project, snapshots, tasks, memberships, catalogs]) => {
       // project.snapshots = snapshots
       // project.tasks = tasks
       const projectData = {
         project: project,
         snapshots: snapshots,
         tasks: tasks,
+        memberships: memberships,
         catalogs: catalogs
       }
 
@@ -126,7 +114,6 @@ export function updateProject(data) {
   }
 }
 
-
 export function updateProjectInit() {
   return { type: UPDATE_PROJECT_INIT }
 }
@@ -167,4 +154,93 @@ export function deleteProjectSuccess(projectId) {
 
 export function deleteProjectError(error) {
   return { type: DELETE_PROJECT_ERROR, error }
+}
+
+export function fetchProjectInvites(projectId) {
+  return function(dispatch) {
+    dispatch(addToPending('fetchProjectInvites'))
+    dispatch(fetchProjectInvitesInit())
+
+    return ProjectApi.fetchProjectInvites(projectId)
+      .then(invites => {
+        dispatch(removeFromPending('fetchProjectInvites'))
+        dispatch(fetchProjectInvitesSuccess(invites))
+      })
+      .catch(error => {
+        dispatch(removeFromPending('fetchProjectInvites'))
+        dispatch(fetchProjectInvitesError(error))
+      })
+  }
+}
+
+export function fetchProjectInvitesInit() {
+  return {type: FETCH_PROJECT_INVITES_INIT}
+}
+
+export function fetchProjectInvitesSuccess(invites) {
+  return {type: FETCH_PROJECT_INVITES_SUCCESS, invites}
+}
+
+export function fetchProjectInvitesError(error) {
+  return {type: FETCH_PROJECT_INVITES_ERROR, error}
+}
+
+export function addProjectMember(data) {
+  return function(dispatch) {
+    dispatch(addToPending('addProjectMember'))
+    dispatch(addProjectMemberInit())
+
+    return ProjectApi.addMember(projectId, data)
+      .then(member => {
+        dispatch(removeFromPending('addProjectMember'))
+        dispatch(addProjectMemberSuccess(member))
+      })
+      .catch(error => {
+        dispatch(removeFromPending('addProjectMember'))
+        dispatch(addProjectMemberError(error))
+        throw error
+      })
+  }
+}
+
+export function addProjectMemberInit() {
+  return {type: ADD_PROJECT_MEMBER_INIT}
+}
+
+export function addProjectMemberSuccess(member) {
+  return {type: ADD_PROJECT_MEMBER_SUCCESS, member}
+}
+
+export function addProjectMemberError(error) {
+  return {type: ADD_PROJECT_MEMBER_ERROR, error}
+}
+
+export function sendProjectInvite(data) {
+  return function(dispatch) {
+    dispatch(addToPending('sendInvite'))
+    dispatch(sendProjectInviteInit())
+
+    return ProjectApi.sendInvite(projectId, data)
+      .then(invite => {
+        dispatch(removeFromPending('sendInvite'))
+        dispatch(sendProjectInviteSuccess(invite))
+      })
+      .catch(error => {
+        dispatch(removeFromPending('sendInvite'))
+        dispatch(sendProjectInviteError(error))
+        throw error
+      })
+  }
+}
+
+export function sendProjectInviteInit() {
+  return {type: SEND_INVITE_INIT}
+}
+
+export function sendProjectInviteSuccess(invite) {
+  return {type: SEND_INVITE_SUCCESS, invite}
+}
+
+export function sendProjectInviteError(error) {
+  return {type: SEND_INVITE_ERROR, error}
 }
