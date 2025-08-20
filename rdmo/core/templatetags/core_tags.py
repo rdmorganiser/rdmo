dbe0860a753c3baaf57796e7107a998a7642a6d8
David Wallace <david.wallace@tu-darmstadt.de>
2025-08-20 16:22:55 +0200

    Simplify back_to_project_link tag
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/core/templatetags/core_tags.py
+++ rdmo/core/templatetags/core_tags.py
@@ -3,8 +3,7 @@ from django.conf import settings
 from django.template import TemplateDoesNotExist
 from django.template.defaultfilters import stringfilter
 from django.template.loader import render_to_string, select_template
-from django.urls import resolve, reverse
-from django.urls.resolvers import Resolver404
+from django.urls import reverse
 from django.utils import translation
 from django.utils.html import escape, format_html, format_html_join  # new
 from django.utils.safestring import mark_safe
@@ -111,24 +110,18 @@ def bootstrap_delete_form(context, **kwargs):
 
 
 @register.simple_tag(takes_context=True)
-def back_to_project_link(context):
-    try:
-        resolver_match = resolve(context.request.path_info)
-        if resolver_match.url_name != 'project':
-            project = context.get('project')
-            if project:
-                project_id = project.id
-            else:
-                project_id = resolver_match.kwargs.get('project_id')
+def back_to_project_link(context) -> str:
+    match = getattr(context.request, 'resolver_match', None)
+    if not match or match.url_name == 'project':
+        return ''
 
-            if project_id is not None:
-                return render_to_string('core/back_to_project_link.html', {
-                    'project_id': project_id
-                }, request=context.request)
-    except Resolver404:
-        pass
+    project = context.get('project')
+    project_id = getattr(project, 'id', None) or match.kwargs.get('project_id')
+    if project_id is None:
+        return ''
 
-    return ''
+    return render_to_string('core/back_to_project_link.html', {'project_id': project_id}, request=context.request)
+
 
 
 @register.filter(is_safe=True)
