b260e63988666181778d4aa4e699b27dc03c2f15
David Wallace <david.wallace@tu-darmstadt.de>
2025-09-19 11:49:57 +0200

    core(tests): fix openapi schema paths len
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/core/tests/test_openapi.py
+++ rdmo/core/tests/test_openapi.py
@@ -21,7 +21,9 @@ def test_openapi_schema(db, client, login, settings, username):
         assert response.status_code == 200
         schema = yaml.safe_load(response.content)
         assert schema['openapi'] == '3.0.3'
-        assert len(schema['paths']) == 125
+        schema_paths = dict(schema['paths'])
+        schema_paths.pop('/api/v1/schema/', None)  # ignore self-inclusion for stability
+        assert len(schema_paths) == 125
     else:
         assert response.status_code == 302
 
