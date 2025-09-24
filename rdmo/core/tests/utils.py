99b900518103e20384d4ad2eacd3a25a3da02429
David Wallace <david.wallace@tu-darmstadt.de>
2025-09-24 10:37:48 +0200

    core(tests,multisite): update perms map and get function
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/core/tests/utils.py
+++ rdmo/core/tests/utils.py
@@ -1,32 +1,59 @@
 import hashlib
 
-from rdmo.core.models import Model
+from django.db.models import Model
+
 from rdmo.core.tests.constants import multisite_status_map, status_map_object_permissions
 
 
-def get_obj_perms_status_code(instance, username, method):
+def get_obj_perms_status_code(instance, username, method, editors=None):
     ''' looks for the object permissions of the instance and returns the status code '''
 
-    if isinstance(instance, Model):
-        try:
-            if not instance.editors.exists():
-                return multisite_status_map[method][username]
-        except AttributeError as e:
-            raise AttributeError(f'instance {instance} should have an editors attribute') from e
+    if (isinstance(instance, Model) or hasattr(instance, 'editors')) and hasattr(instance, 'uri'):
+        instance_uri = instance.uri
+        instance_editors = instance.editors.values_list('domain', flat=True)
     elif isinstance(instance, str):
-        pass
-
-    if 'foo-' in str(instance):
-        instance_obj_perms_key = 'foo-element'
-    elif 'bar-' in str(instance):
-        instance_obj_perms_key = 'bar-element'
+        instance_uri = instance
+        instance_editors = []
     else:
+        raise TypeError(f'instance {instance} should be a str or a Model (and have an uri)')
+
+    if editors is not None and method == 'delete':
+        # override in case of deleted instance
+        instance_editors = editors
+
+    if 'foo-' in instance_uri:
+        instance_obj_perms_key = 'foo-element'
+        assert_editors = ['foo.com']
+    elif 'bar-' in instance_uri:
+        instance_obj_perms_key = 'bar-element'
+        assert_editors = ['bar.com']
+    elif 'example-' in instance_uri:
+        instance_obj_perms_key = 'example-element'
+        assert_editors = ['example.com']
+    else:
+        if instance_editors:
+            raise ValueError(f"uri {instance_uri} should contain the domain for {instance_editors}")
         instance_obj_perms_key = 'all-element'
+        assert_editors = ['foo.com', 'bar.com', 'example.com']
+
+    if not instance_editors and instance_obj_perms_key != 'all-element':
+        raise ValueError(f"instance_editors should be specified on {instance_uri}")
+    elif instance_editors:
+        assert all(
+            i in instance_editors for i in assert_editors
+        ), f"{assert_editors} should be specified on {instance_uri}"
+
+
+    if not instance_editors:
+        return multisite_status_map[method][username]
 
     try:
         method_instance_obj_perms_map = status_map_object_permissions[method][instance_obj_perms_key]
     except KeyError as e:
-        raise KeyError(f'instance ({instance_obj_perms_key}) should be defined in status_map_object_permissions') from e
+        raise KeyError(
+            f'instance (uri={instance_uri}, editors={instance_editors}) for {method} ({instance_obj_perms_key})'
+            f' should be defined in status_map_object_permissions'
+        ) from e
     try:
         return method_instance_obj_perms_map[username]
     except KeyError:
