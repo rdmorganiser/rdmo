21d2611aaad7e8d83eaf73b3ba331559616bd39f
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2025-09-30 16:58:49 +0200

    * add project hierarchy

--- rdmo/projects/assets/js/project/components/helper/index.js
+++ rdmo/projects/assets/js/project/components/helper/index.js
@@ -1,3 +1,4 @@
 export { default as ProjectBadge } from './ProjectBadge'
 export { default as Tile } from './Tile'
 export { default as TileGrid } from './TileGrid'
+export { default as HierarchyTree } from './HierarchyTree'
