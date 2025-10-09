33e8bc2ecd0265282c8b7e6cdd8a9c78fd9a2b71
Jochen Klar <mail@jochenklar.de>
2025-10-09 21:09:20 +0200

    Refactor Projects component

--- rdmo/projects/assets/js/common/constants/defaultRoleOptions.js
+++
@@ -1,8 +0,0 @@
-  export const defaultRoleOptions = [
-    { value: 'owner',   label: gettext('Owner') },
-    { value: 'manager', label: gettext('Manager') },
-    { value: 'author',  label: gettext('Author') },
-    { value: 'guest',   label: gettext('Guest') }
-  ]
-
-  export const defaultRoleArrays = ['authors', 'guests', 'managers', 'owners']
