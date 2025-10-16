5c7014e3d87efddc5de63e7aa5353811e0270ddc
David Wallace <david.wallace@tu-darmstadt.de>
2025-10-16 09:10:52 +0200

    config(tests): initial add tests for plugins
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

---
+++ testing/plugins/optionset_plugin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class TestOptionSetProviderConfig(AppConfig):
+    name = "rdmo.testing.plugins.optionset_provider"
+    label = "test_optionset_provider"
+    verbose_name = "Test OptionSet Provider"
