f7644495b92f01296bbc6712f36444e3cf10e49d
David Wallace <david.wallace@tu-darmstadt.de>
2025-10-16 09:10:52 +0200

    config(tests): initial add tests for plugins
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

---
+++ testing/plugins/export_plugin/apps.py
@@ -0,0 +1,7 @@
+
+from django.apps import AppConfig
+
+class ExportPluginConfig(AppConfig):
+    name = 'rdmo.testing.plugins.export_plugin'
+    label = 'test_export_plugin'
+    verbose_name = 'Test Export Plugin'
