4ebc2129efa6f285f622f7121c74c0b5e11dda20
David Wallace <david.wallace@tu-darmstadt.de>
2025-10-27 15:15:28 +0100

    config(tests): update and fix tests
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/config/tests/conftest.py
+++ rdmo/config/tests/conftest.py
@@ -4,23 +4,23 @@ import pytest
 @pytest.fixture
 def enable_plugins(settings):
     settings.INSTALLED_APPS += [
-        "rdmo.testing.plugins.export_plugin",
-        "rdmo.testing.plugins.import_plugin",
-        "rdmo.testing.plugins.optionset_provider",
+        "plugins.export_plugin",
+        "plugins.import_plugin",
+        "plugins.optionset_provider_plugin",
     ]
     # also insert into the relevant plugin-tuples in settings,
     # e.g. PROJECT_EXPORTS, PROJECT_IMPORTS, OPTIONSET_PROVIDERS
-    # you need to mirror what your PR expects; for example:
+    # to support the backwards compatible settings
     settings.PROJECT_EXPORTS = list(getattr(settings, "PROJECT_EXPORTS", []))
     settings.PROJECT_EXPORTS.append(
-        ("test_export", "Test Export", "rdmo.testing.plugins.export_plugin.plugin.TestExportPlugin")
+        ("test_export", "Test Export", "plugins.export_plugin.plugin.TestExportPlugin")
     )
     settings.PROJECT_IMPORTS = list(getattr(settings, "PROJECT_IMPORTS", []))
     settings.PROJECT_IMPORTS.append(
-        ("test_import", "Test Import", "rdmo.testing.plugins.import_plugin.plugin.TestImportPlugin")
+        ("test_import", "Test Import", "plugins.import_plugin.plugin.TestImportPlugin")
     )
     settings.OPTIONSET_PROVIDERS = list(getattr(settings, "OPTIONSET_PROVIDERS", []))
     settings.OPTIONSET_PROVIDERS.append(
-        ("test_optionset", "Test OptionSet", "rdmo.testing.plugins.optionset_provider.plugin.TestOptionSetProvider")
+        ("test_optionset", "Test OptionSet", "plugins.optionset_provider_plugin.plugin.TestOptionSetProvider")
     )
     return settings
