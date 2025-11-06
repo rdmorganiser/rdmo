14ac5b951090790eac63220004795ea75f786f50
David Wallace <david.wallace@tu-darmstadt.de>
2025-11-06 12:39:18 +0100

    config(plugins): refactor from core and update utils
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/config/tests/test_plugins.py
+++ rdmo/config/tests/test_plugins.py
@@ -5,15 +5,15 @@ from rdmo.projects.models import Project
 
 
 @pytest.mark.django_db
-def test_plugin_create_and_render(enable_plugins, settings):
+def test_plugin_create_and_render():
     # Arrange: create the Plugin model instance
-    settings.LANGUAGE_CODE = 'en'
     project = Project.objects.get(id=1)
     instance = Plugin.objects.create(
         uri_prefix="https://example.org/terms",
         uri_path="test-plugins-export",
-        python_path="plugins.export_plugin.plugin.TestExportPlugin",
+        python_path="plugins.project_export.exports.SimpleExportPlugin",
         title_lang1="Test Export Plugin",
+        title_lang2="Test Export Plugin(lang2)",
         available=True,
         plugin_settings={"foo": "bar"},
     )
@@ -23,6 +23,7 @@ def test_plugin_create_and_render(enable_plugins, settings):
     export_plugin.project = project
     export_plugin.snapshot = None
     # Call export (render) and assert behavior
+    assert instance.plugin_type == "project_export"
     response = export_plugin.render()
     assert response.status_code == 200
     text = response.content.decode()
