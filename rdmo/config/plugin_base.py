6f45ab95833a30f88a292f7da7a7baf2eac5ac9f
David Wallace <david.wallace@tu-darmstadt.de>
2025-11-06 12:48:45 +0100

    core(plugins): revert refactor to config
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/config/plugin_base.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-
-class PluginBase:
-
-    def __init__(self, key, label, class_name):
-        self.key = key
-        self.label = label
-        self.class_name = class_name
--- rdmo/core/plugins.py
+++ rdmo/core/plugins.py
@@ -3,6 +3,14 @@ from django.conf import settings
 from rdmo.core.utils import import_class
 
 
+class Plugin:
+
+    def __init__(self, key, label, class_name):
+        self.key = key
+        self.label = label
+        self.class_name = class_name
+
+
 def get_plugins(plugin_settings):
     plugins = {}
     for key, label, class_name in getattr(settings, plugin_settings, []):
