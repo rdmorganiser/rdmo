860dcfc7a21ad8b3060738e26d6183779c7500e9
David Wallace <david.wallace@tu-darmstadt.de>
2025-11-06 12:51:38 +0100

    Move mock SimpleProviders to testing
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/options/providers.py
+++ rdmo/options/providers.py
@@ -11,27 +11,3 @@ class Provider(Plugin):
 
     def get_options(self, project, search=None, user=None, site=None):
         raise NotImplementedError
-
-
-class SimpleProvider(Provider):
-
-    refresh = True
-
-    def get_options(self, project, search=None, user=None, site=None):
-        return [
-            {
-                'id': 'simple_1',
-                'text': 'Simple answer 1',
-                'help': 'One'
-            },
-            {
-                'id': 'simple_2',
-                'text': 'Simple answer 2',
-                'help': 'Two'
-            },
-            {
-                'id': 'simple_3',
-                'text': 'Simple answer 3',
-                'help': 'Three'
-            }
-        ]
