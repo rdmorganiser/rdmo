ccaab58a013231c7673e6256f40a80690ab27bdf
David Wallace <david.wallace@tu-darmstadt.de>
2025-11-14 14:59:13 +0100

    config(plugin): add for_context to PluginManager
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/config/managers.py
+++ rdmo/config/managers.py
@@ -33,6 +33,22 @@ class PluginQuerySet(
             .filter_availability(user)
         )
 
+    def for_context(self, project=None, plugin_type=None, user=None):
+        qs = self
+
+        if project is not None:
+            qs = qs.filter_for_project(project)
+
+        if user is not None:
+            qs = qs.filter_current_available(user)
+        else:
+            qs = qs.filter(available=True).filter_current_site()
+
+        if plugin_type is not None:
+            qs = qs.filter(plugin_type=plugin_type)
+
+        return qs
+
 class PluginManager(CurrentSiteManagerMixin, GroupsManagerMixin, AvailabilityManagerMixin, models.Manager):
 
     def get_queryset(self) -> PluginQuerySet:
@@ -46,3 +62,10 @@ class PluginManager(CurrentSiteManagerMixin, GroupsManagerMixin, AvailabilityMan
 
     def filter_current_available(self, user):
         return self.get_queryset().filter_current_available(user)
+
+    def for_context(self, project=None, plugin_type=None, user=None):
+        return self.get_queryset().for_context(
+            project=project,
+            plugin_type=plugin_type,
+            user=user,
+        )
