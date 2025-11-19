9f9dff40babe77a9ce88708765d7991b2c294b1b
David Wallace <david.wallace@tu-darmstadt.de>
2025-11-19 11:37:13 +0100

    config(tests): add fixture to enable legacy settings
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/config/tests/conftest.py
+++ rdmo/config/tests/conftest.py
@@ -1,33 +1,38 @@
+import pytest
 
-#
-# @pytest.fixture
-# def enable_plugins(settings):
-#     settings.INSTALLED_APPS += [
-#         "plugins",
-#     ]
-#     # # insert the relevant legacy plugin-tuples in settings,
-#     # # e.g. PROJECT_EXPORTS, PROJECT_IMPORTS, OPTIONSET_PROVIDERS
-#     # # to support the backwards compatible settings
-#     # settings.PROJECT_EXPORTS = list(getattr(settings, "PROJECT_EXPORTS", []))
-#     # settings.PROJECT_EXPORTS.append(
-#     #     ("test_export", "Test Export", "plugins.project_export.exports.SimpleExportPlugin")
-#     # )
-#     # settings.PROJECT_SNAPSHOT_EXPORTS = list(getattr(settings, "PROJECT_SNAPSHOT_EXPORTS", []))
-#     # settings.PROJECT_SNAPSHOT_EXPORTS.append(
-#     #     ("test_export", "Test Export", "plugins.project_snapshot_export.exports.SimpleSnapshotExportPlugin")
-#     # )
-#     #
-#     # settings.PROJECT_IMPORTS = list(getattr(settings, "PROJECT_IMPORTS", []))
-#     # settings.PROJECT_IMPORTS.append(
-#     #     ("test_import", "Test Import", "plugins.project_import.imports.TestImportPlugin")
-#     # )
-#     # settings.OPTIONSET_PROVIDERS = list(getattr(settings, "OPTIONSET_PROVIDERS", []))
-#     # settings.OPTIONSET_PROVIDERS.append(
-#     #     ("test_optionset", "Test OptionSet", "plugins.optionset_providers.providers.SimpleProvider")
-#     # )
-#     # settings.PROJECT_ISSUE_PROVIDERS = list(getattr(settings, "PROJECT_ISSUE_PROVIDERS", []))
-#     # settings.PROJECT_ISSUE_PROVIDERS.append(
-#     #     ("test_optionset", "Test OptionSet", "plugins.project_snapshot_export.exports.SimpleIssueProvider")
-#     # )
-#
-#     return settings
+from django.utils.translation import gettext_lazy as _
+
+
+@pytest.fixture
+def enable_legacy_plugins(settings):
+
+    delattr(settings, 'PLUGINS')
+    # settings.INSTALLED_APPS has 'plugins' already
+    # insert the relevant legacy plugin-tuples in settings,
+    # e.g. PROJECT_EXPORTS, PROJECT_IMPORTS, OPTIONSET_PROVIDERS
+    # to support the backwards compatible settings
+    settings.PROJECT_EXPORTS = [
+        ('xml', _('RDMO XML'), 'rdmo.projects.exports.RDMOXMLExport'),
+        ('csvcomma', _('CSV (comma separated)'), 'rdmo.projects.exports.CSVCommaExport'),
+        ('csvsemicolon', _('CSV (semicolon separated)'), 'rdmo.projects.exports.CSVSemicolonExport'),
+        ('json', _('JSON'), 'rdmo.projects.exports.JSONExport'),
+        ("simple_export", "Test Export", "plugins.project_export.exports.SimpleExportPlugin"),
+    ]
+    settings.PROJECT_IMPORTS = [
+        ('xml', _('RDMO XML'), 'rdmo.projects.imports.RDMOXMLImport'),
+        ('url', _('from URL'), 'rdmo.projects.imports.URLImport'),
+    ]
+
+    settings.PROJECT_SNAPSHOT_EXPORTS = [
+        ("simple_snapshot_export", "Snapshot RDMO XML", "plugins.project_snapshot_export.exports.SimpleSnapshotExportPlugin"),  # noqa: E501
+    ]
+
+    settings.OPTIONSET_PROVIDERS = [
+        ("simple_optionset_provider", "Simple OptionSet Provider", "plugins.optionset_providers.providers.SimpleProvider"),  # noqa: E501
+    ]
+
+    settings.PROJECT_ISSUE_PROVIDERS = [
+        ("simple_issue_provider", "Simple Issue Provider", "plugins.project_snapshot_export.exports.SimpleIssueProvider"),  # noqa: E501
+    ]
+
+    return settings
