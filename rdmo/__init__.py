5b3407ea8dae67bd77065af1462d66b6195077f2
Jochen Klar <mail@jochenklar.de>
2025-11-20 13:46:26 +0100

    Use setuptools_scm to dynamically create the version

--- rdmo/__init__.py
+++ rdmo/__init__.py
@@ -1,1 +1,4 @@
-__version__ = "2.4.0"
+try:
+    from ._version import __version__, __version_tuple__, version, version_tuple
+except ImportError:
+    __version__ = __version_tuple__ = version = version_tuple = None
