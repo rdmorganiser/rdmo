deb69d69e7efd950f54fda67f6c77f60d29e5751
Jochen Klar <mail@jochenklar.de>
2025-11-20 13:33:24 +0100

    Add release management script (for rdmo-admin)

--- rdmo/core/management/commands/build.py
+++ rdmo/core/management/commands/build.py
@@ -2,12 +2,17 @@ import subprocess
 import sys
 
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        try:
+            import build  # noqa: F401
+        except ImportError as e:
+            raise CommandError('build is not installed.') from e
+
         call_command('npm', 'ci')
         call_command('npm', 'run', 'build:prod')
         subprocess.call(['/bin/bash', '-c', f'{sys.executable} -m build'])
