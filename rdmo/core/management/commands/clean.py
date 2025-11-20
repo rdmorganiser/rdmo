f3217679fd9f0605968cd9622a230e2e7e1b9976
Jochen Klar <mail@jochenklar.de>
2025-11-20 13:39:09 +0100

    Update clean management command

--- rdmo/core/management/commands/clean.py
+++ rdmo/core/management/commands/clean.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -21,36 +22,22 @@ class Command(BaseCommand):
         if options['command'] in ['all', 'dist']:
             self.remove_path('dist')
             self.remove_path('rdmo.egg-info')
+        if options['command'] in ['all', 'static']:
+            self.remove_path(settings.STATIC_ROOT)
         if options['command'] in ['all', 'media']:
             self.remove_path(settings.MEDIA_ROOT)
         if options['command'] in ['all', 'npm']:
             self.remove_path('node_modules')
-        if options['command'] in ['all', 'static']:
-            self.clean_static()
         if options['command'] in ['all', 'python']:
             self.clean_python()
+        if options['command'] in ['all', 'git']:
+            subprocess.call(['git', 'clean', '-fd'])
 
     def clean_python(self):
         for root, dirs, _ in os.walk('.'):
             for dir_name in dirs:
                 if dir_name == '__pycache__':
                     self.remove_path(os.path.join(root, dir_name), quiet=True)
-
-    def clean_static(self):
-        self.remove_path(settings.STATIC_ROOT)
-
-        # hint: `git clean -dfXn` can be used to show all git-ignored files
-        for path in [
-            'rdmo/core/static/core/css/base.css',
-            'rdmo/core/static/core/fonts/',
-            'rdmo/core/static/core/js/base.js',
-            'rdmo/core/static/core/js/base.js.LICENSE.txt',
-            'rdmo/management/static/',
-            'rdmo/projects/static/projects/css/interview.css',
-            'rdmo/projects/static/projects/css/projects.css',
-            'rdmo/projects/static/projects/js/',
-        ]:
-            self.remove_path(path)
 
     def remove_path(self, path, quiet=False):
         if path and os.path.exists(path):
