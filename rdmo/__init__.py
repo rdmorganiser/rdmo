6b5aac27a67d2c551c25c295c4e2823d528f5ea1
Jochen Klar <mail@jochenklar.de>
2025-11-22 14:56:18 +0100

    Use version from importlib.metadata

--- rdmo/__init__.py
+++ rdmo/__init__.py
@@ -1,4 +1,7 @@
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _version
+
 try:
-    from ._version import __version__, __version_tuple__, version, version_tuple
-except ImportError:
-    __version__ = __version_tuple__ = version = version_tuple = None
+    VERSION = __version__ = _version(__package__)
+except PackageNotFoundError:
+    VERSION = __version__ = "0.0.0+unknown"
