d0b01e2451da5eaab750ab863989ed6ccfb74bca
David Wallace <david.wallace@tu-darmstadt.de>
2025-11-24 21:11:01 +0100

    Add filters for format and settings to Plugin manager
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/config/managers.py
+++ rdmo/config/managers.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.conf import settings
+from django.db import connections, models
 
 from rdmo.core.managers import (
     AvailabilityManagerMixin,
@@ -21,7 +22,6 @@ class PluginQuerySet(
     def filter_for_project(self, project):
         return (
             self
-                .filter(available=True)
                 .filter_for_site(project.site)
                 .filter_for_catalog(project.catalog)
                 .filter_for_groups(project.groups)
@@ -33,8 +33,37 @@ class PluginQuerySet(
             .filter_availability(user)
         )
 
-    def for_context(self, project=None, plugin_type=None, user=None):
+    def filter_for_settings_plugins(self):
+        if not settings.PLUGINS:
+            return self
+
+        return self.filter(python_path__in=settings.PLUGINS)
+
+    def filter_for_format(self, file_format: str):
+        if not file_format:
+            return self
+
+        q = (
+            models.Q(url_name=file_format)
+            | models.Q(uri_path=file_format)
+            | models.Q(uri_path__endswith='/' + file_format)
+        )
+
+        # add a JSONField contains lookup if the current DB supports it
+        connection = connections[self.db]
+        if getattr(connection.features, 'supports_json_field_contains', False):
+            q |= models.Q(plugin_settings__contains={'format': format})
+        elif connection.vendor == 'sqlite':
+            # SQLite: JSONField is stored as TEXT; emulate contains by string search.
+            q |= models.Q(plugin_settings__icontains=f'"format": "{format}"')
+
+        return self.filter(q)
+
+
+    def for_context(self, project=None, plugin_type=None, user=None, format=None):
         qs = self
+
+        qs = qs.filter_for_settings_plugins()
 
         if project is not None:
             qs = qs.filter_for_project(project)
@@ -46,6 +75,9 @@ class PluginQuerySet(
 
         if plugin_type is not None:
             qs = qs.filter(plugin_type=plugin_type)
+
+        if format is not None:
+            qs = qs.filter_for_format(format)
 
         return qs
 
@@ -63,9 +95,16 @@ class PluginManager(CurrentSiteManagerMixin, GroupsManagerMixin, AvailabilityMan
     def filter_current_available(self, user):
         return self.get_queryset().filter_current_available(user)
 
-    def for_context(self, project=None, plugin_type=None, user=None):
+    def filter_for_settings_plugins(self):
+        return self.get_queryset().filter_for_settings_plugins()
+
+    def filter_for_format(self, file_format):
+        return self.get_queryset().filter_for_format(file_format)
+
+    def for_context(self, project=None, plugin_type=None, user=None, format=None):
         return self.get_queryset().for_context(
             project=project,
             plugin_type=plugin_type,
             user=user,
+            format=format
         )
