de9b360ece279e622d17e50cd07da37ff7a2ae5b
David Wallace <david.wallace@tu-darmstadt.de>
2025-11-24 10:46:37 +0100

    Add projects catalog tests for #1481
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/projects/tests/conftest.py
+++ rdmo/projects/tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from django.apps import apps
 
+from .helpers.project_catalog import clear_sites_from_other_catalogs  # noqa: F401
+
 
 @pytest.fixture
 def enable_multisite(settings):
--- rdmo/projects/tests/test_viewset_catalog_multisite.py
+++ rdmo/projects/tests/test_viewset_catalog_multisite.py
@@ -11,6 +11,10 @@ users = (
     ('user', 'user'),
     ('site', 'site'),
     ('anonymous', None),
+    ('foo-user','foo-user'),
+    ('foo-editor', 'foo-editor'),
+    ('bar-user','bar-user'),
+    ('bar-editor', 'bar-editor'),
 )
 
 view_project_catalog_permission_map = {  # id, available
@@ -22,17 +26,20 @@ view_project_catalog_permission_map = {  # id, available
     'editor': [(1, True)],
     'reviewer': [(1, True)],
     'api': [(1, True),(2, False)],
-    'site': [(1, True)]
+    'site': [(1, True)],
+    'foo-user': [(1, True)],
+    'foo-editor': [(1, True)],
+    'bar-user': [(1, True)],
+    'bar-editor': [(1, True)],
 }
 
 urlnames = {
     'list': 'v1-projects:catalog-list',
 }
 
-other_sites_catalogs = [(3, True), (4, True)]
 
 @pytest.mark.parametrize('username,password', users)
-def test_list(db, client, username, password):
+def test_list(db, settings, enable_multisite, client, username, password):
     client.login(username=username, password=password)
 
     url = reverse(urlnames['list'])
@@ -48,7 +55,8 @@ def test_list(db, client, username, password):
 
 
 @pytest.mark.parametrize('username,password', users)
-def test_list_with_cleared_sites(db, client, clear_sites_from_other_catalogs, username, password):
+def test_list_with_cleared_sites(db, settings, enable_multisite, clear_sites_from_other_catalogs,
+                                 client, username, password):
     client.login(username=username, password=password)
 
     url = reverse(urlnames['list'])
@@ -58,12 +66,6 @@ def test_list_with_cleared_sites(db, client, clear_sites_from_other_catalogs, us
         assert response.status_code == 200
         data = response.json()
         assert isinstance(data, list)
-        catalogs = view_project_catalog_permission_map[username] + other_sites_catalogs
-        if any(not available for _id,available in catalogs):  # api sees an available=False catalog
-            catalogs = sorted(
-                catalogs,
-                key=lambda i: (not i[1], i[0]),
-            )
-        assert catalogs == [(i['id'],i['available']) for i in data]
+        assert view_project_catalog_permission_map[username] == [(i['id'],i['available']) for i in data]
     else:
         assert response.status_code == 401
