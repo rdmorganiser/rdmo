bd274af7cf119c16aa979b4afe7c9ed172e60559
Jochen Klar <mail@jochenklar.de>
2026-01-23 10:51:35 +0100

    Refactor project react app structure

--- rdmo/projects/assets/js/project.js
+++ rdmo/projects/assets/js/project.js
@@ -7,14 +7,29 @@ import configureStore from './project/store/configureStore'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-import Project from './project/components/Project'
+import Pending from '../../../core/assets/js/containers/Pending'
+
+import Main from './project/components/Main'
+import Sidebar from './project/components/Sidebar'
 
 const store = configureStore()
 
 createRoot(document.getElementById('main')).render(
   <DndProvider backend={HTML5Backend}>
     <Provider store={store}>
-      <Project />
+      <Main />
     </Provider>
   </DndProvider>
+)
+
+createRoot(document.getElementById('sidebar')).render(
+  <Provider store={store}>
+    <Sidebar />
+  </Provider>
+)
+
+createRoot(document.getElementById('pending')).render(
+  <Provider store={store}>
+    <Pending />
+  </Provider>
 )
