73a2413d3a06a3e537d9c281674f9a4edbaac6bd
Jochen Klar <mail@jochenklar.de>
2026-01-23 10:20:56 +0100

    Fix useSelector calls

--- rdmo/projects/assets/js/project/hooks/useFieldErrors.js
+++ rdmo/projects/assets/js/project/hooks/useFieldErrors.js
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux'
 
 export const useFieldErrors = () => {
-  const errors = useSelector((state) => state.project.errors?.[0]?.errors ?? {})
+  const errors = useSelector((state) => state.project.errors?.[0]?.errors) || {}
   return Object.fromEntries(
     Object.entries(errors).map(([field, err]) => [
       field,
