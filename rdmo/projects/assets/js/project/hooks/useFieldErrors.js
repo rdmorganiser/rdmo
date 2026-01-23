0997f4744eec19da6ea5a36df5daef0179e2fb80
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-01-23 14:00:39 +0100

    fix console warnings

--- rdmo/projects/assets/js/project/hooks/useFieldErrors.js
+++ rdmo/projects/assets/js/project/hooks/useFieldErrors.js
@@ -6,7 +6,6 @@ const EMPTY_ERRORS = {}
 export const useFieldErrors = () => {
   const rawErrors = useSelector((state) => state.project.errors?.[0]?.errors) || EMPTY_ERRORS
 
-  // Optional but recommended: avoid allocating a new object if rawErrors didn't change
   return useMemo(() => (
     Object.fromEntries(
       Object.entries(rawErrors).map(([field, err]) => [
