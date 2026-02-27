bdf498222de0748972f212fbd957e867e2214c02
Jochen Klar <mail@jochenklar.de>
2026-02-27 11:41:14 +0100

    Add ProjectRoleFilterBackend

--- rdmo/projects/viewsets.py
+++ rdmo/projects/viewsets.py
@@ -38,6 +38,7 @@ from .filters import (
     OptionFilterBackend,
     ProjectDateFilterBackend,
     ProjectOrderingFilter,
+    ProjectRoleFilterBackend,
     ProjectSearchFilterBackend,
     ProjectUserFilterBackend,
     SnapshotFilterBackend,
@@ -115,6 +116,7 @@ class ProjectViewSet(ModelViewSet):
     filter_backends = (
         DjangoFilterBackend,
         ProjectUserFilterBackend,
+        ProjectRoleFilterBackend,
         ProjectDateFilterBackend,
         ProjectOrderingFilter,
         ProjectSearchFilterBackend,
