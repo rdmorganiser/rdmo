4a8f169b8082d6ff40ebc8dcee9f4789b760514e
Jochen Klar <mail@jochenklar.de>
2026-03-13 14:59:06 +0100

    Update npm management script

--- rdmo/core/management/commands/npm.py
+++ rdmo/core/management/commands/npm.py
@@ -19,4 +19,4 @@ class Command(BaseCommand):
             raise CommandError('NVM_DIR does not exist, is nvm.sh installed?')
 
         command = ' '.join(options['command'])
-        subprocess.check_call(['/bin/bash', '-c', f'source {nvm_dir}/nvm.sh; nvm use; npm {command}'])
+        subprocess.check_call(['/bin/bash', '-c', f'source {nvm_dir}/nvm.sh && nvm use && npm {command}'])
