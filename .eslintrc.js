ff1ef658fa386d620579f52e0383ab38d5bf07b5
Jochen Klar <mail@jochenklar.de>
2026-03-19 19:44:38 +0100

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,20 +26,79 @@ module.exports = {
   ],
   'ignorePatterns': ['**/static/**/*.js'],
   'rules': {
-    'indent': 'off',
+    // Disable unused rules
     'no-empty-pattern': 'off',
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
+
+    // Unix linebreaks only
+    'linebreak-style': ['error', 'unix'],
+
+    // Single quotes, allow double to avoid escaping
     'quotes': [
       'error',
-      'single'
+      'single',
+      { 'avoidEscape': true }
     ],
-    'semi': [
+
+    // No semicolons
+    'semi': ['error', 'never'],
+
+    // Maximum line length of 120 characters
+    'max-len': [
       'error',
-      'never'
-    ]
+      {
+        'code': 120,
+        'ignoreUrls': true,
+        'ignoreRegExpLiterals': true
+      }
+    ],
+
+    // Indent by 2 spaces
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1,
+        'offsetTernaryExpressions': true
+      }
+    ],
+
+    // Disallow newlines between the operands of a ternary expression
+    'multiline-ternary': ['error', 'never'],
+
+    // JSX: wrap multiline expressions in parens, opening paren on a new line
+    'react/jsx-wrap-multilines': [
+      'error',
+      {
+        'declaration': 'parens-new-line',
+        'assignment': 'parens-new-line',
+        'return': 'parens-new-line',
+        'arrow': 'parens-new-line',
+        'condition': 'parens-new-line',
+        'logical': 'parens-new-line',
+        'prop': 'parens-new-line'
+      }
+    ],
+
+    // JSX curly braces: require newlines inside for multiline expressions
+    'react/jsx-curly-newline': [
+      'error',
+      {
+        'multiline': 'require',
+        'singleline': 'consistent'
+      }
+    ],
+
+    // JSX curly brace spacing: no spaces inside { }
+    'react/jsx-curly-spacing': [
+      'error',
+      {
+        'when': 'never',
+        'children': true
+      }
+    ],
+
+    // JSX double quotes for props
+    'jsx-quotes': ['error', 'prefer-double'],
   },
   'settings': {
     'react': {
