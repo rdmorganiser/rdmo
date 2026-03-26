69635e4630761d6b1f3c43429f3d0b6d2b6a4c1d
Jochen Klar <mail@jochenklar.de>
2026-03-26 18:15:53 +0100

    Add import and export rules to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     'sourceType': 'module'
   },
   'plugins': [
+    'simple-import-sort',
     'react'
   ],
   'ignorePatterns': ['**/static/**/*.js'],
@@ -62,8 +63,39 @@ module.exports = {
       }
     ],
 
+    // Import order
+    'simple-import-sort/imports': ['error', {
+      groups: [
+        // external imports in the given order
+        ['^react$', '^prop-types$', '^react', '^[a-z]', '^lodash'],
+        // rdmo imports: lowercase first, then capitalized components
+        ['^rdmo/.*\\/[a-z]'],
+        ['^rdmo/.*\\/[A-Z]'],
+        // parent imports: lowercase first, then capitalized components
+        ['^\\.\\..*\\/[a-z]'],
+        ['^\\.\\..*\\/[A-Z]'],
+        // sibling imports: lowercase first, then capitalized  components
+        ['^\\./.*\\/[a-z]'],
+        ['^\\./.*\\/[A-Z]'],
+      ],
+    }],
+
+    // Export order
+    'simple-import-sort/exports': 'error',
+
     // Disallow newlines between the operands of a ternary expression
     'multiline-ternary': ['error', 'never'],
+
+    // Ensure spaces after commas
+    'comma-spacing': ['error', { before: false, after: true }],
+
+    // Ensure correct multiline imports
+    'object-curly-newline': ['error', {
+      ImportDeclaration: {
+        multiline: true,
+        consistent: true
+      }
+    }],
 
     // JSX: wrap multiline expressions in parens, opening paren on a new line
     'react/jsx-wrap-multilines': [
