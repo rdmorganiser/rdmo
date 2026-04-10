2d32c758331c6294644dad0816340c68a1488a6b
Jochen Klar <mail@jochenklar.de>
2026-04-10 13:22:40 +0200

    Fix projects urls order

--- rdmo/projects/urls/__init__.py
+++ rdmo/projects/urls/__init__.py
@@ -5,12 +5,12 @@ from ..views import ProjectDetailView, ProjectErrorView, ProjectInterviewView, P
 urlpatterns = [
     re_path(r'^$',
             ProjectsView.as_view(), name='projects'),
-    re_path(r'^(?P<pk>[0-9]+)/',
-            ProjectDetailView.as_view(), name='project'),
-    re_path(r'^join/(?P<token>.+)/$',
-            ProjectJoinView.as_view(), name='project_join'),
     re_path(r'^(?P<pk>[0-9]+)/interview/',
             ProjectInterviewView.as_view(), name='project_interview'),
     re_path(r'^(?P<pk>[0-9]+)/error/',
             ProjectErrorView.as_view(), name='project_error'),
+    re_path(r'^(?P<pk>[0-9]+)/',
+            ProjectDetailView.as_view(), name='project'),
+    re_path(r'^join/(?P<token>.+)/$',
+            ProjectJoinView.as_view(), name='project_join'),
 ]
