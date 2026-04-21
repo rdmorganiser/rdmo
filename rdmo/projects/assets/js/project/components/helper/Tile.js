335d1719eebb310c979c369228049b3ec7dc7881
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-04-21 14:54:29 +0200

    add card click and invisible tasks

--- rdmo/projects/assets/js/project/components/helper/Tile.js
+++ rdmo/projects/assets/js/project/components/helper/Tile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Tile = ({ title, label, buttonLabel, children, className = '', size = 'normal', onClick }) => {
+const Tile = ({ title, label, buttonLabel, children, className = '', size = 'normal', onClick, onCardClick }) => {
   const sizeClasses = {
     compact: 'col-12 col-md-4',  // 3 tiles per row
     normal: 'col-12 col-md-6',   // 2 tiles per row
@@ -10,7 +10,9 @@ const Tile = ({ title, label, buttonLabel, children, className = '', size = 'nor
 
   return (
     <div
-      className={`card card-tile mb-4 rounded-3 ${sizeClasses[size]} ${className}`}>
+      className={`card card-tile mb-4 rounded-3 ${sizeClasses[size]} ${className}`}
+      onClick={onCardClick}
+      style={onCardClick ? { cursor: 'pointer' } : undefined} >
       <div className="card-body d-flex flex-column">
         {label && <div className="text-success fw-semibold small mb-1">{label}</div>}
         {title && <h3 className="card-title mb-2">{title}</h3>}
@@ -22,7 +24,13 @@ const Tile = ({ title, label, buttonLabel, children, className = '', size = 'nor
               <button
                 type="button"
                 className="btn btn-outline-secondary"
-                onClick={onClick}
+                // onClick={onClick}
+                onClick={
+                  (e) => {
+                    e.stopPropagation()
+                    onClick()
+                  }
+                }
               >
                 {buttonLabel} <span className="ms-1">→</span>
               </button>
@@ -42,6 +50,7 @@ Tile.propTypes = {
   label: PropTypes.node,
   size: PropTypes.oneOf(['compact', 'normal', 'fullWidth']),
   onClick: PropTypes.func,
+  onCardClick: PropTypes.func
 }
 
 export default Tile
