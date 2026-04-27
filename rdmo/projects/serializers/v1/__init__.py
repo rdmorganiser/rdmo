1a3c1f7b1546ed7b34db5cd78cf0ca22e3d4e109
Jochen Klar <mail@jochenklar.de>
2026-04-27 11:41:32 +0200

    Add ProjectIssueTaskSerializer and IssueTaskSerializer

--- rdmo/projects/serializers/v1/__init__.py
+++ rdmo/projects/serializers/v1/__init__.py
@@ -13,6 +13,7 @@ from rdmo.core.serializers import TranslationSerializerMixin
 from rdmo.domain.models import Attribute
 from rdmo.questions.models import Catalog
 from rdmo.services.validators import ProviderValidator
+from rdmo.tasks.models import Task
 from rdmo.views.models import View
 
 from ...constants import ROLE_CHOICES
@@ -507,6 +508,18 @@ class ProjectInviteUpdateSerializer(serializers.ModelSerializer):
         )
 
 
+class ProjectIssueTaskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = (
+            'id',
+            'order',
+            'title',
+            'text',
+        )
+
+
 class ProjectIssueResourceSerializer(serializers.ModelSerializer):
 
     integration = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -522,7 +535,7 @@ class ProjectIssueResourceSerializer(serializers.ModelSerializer):
 
 class ProjectIssueSerializer(serializers.ModelSerializer):
 
-    task = serializers.PrimaryKeyRelatedField(read_only=True)
+    task = ProjectIssueTaskSerializer(read_only=True)
     resources = ProjectIssueResourceSerializer(read_only=True, many=True)
 
     class Meta:
@@ -686,6 +699,19 @@ class UserInviteSerializer(InviteSerializer):
             'token',
         )
 
+
+class IssueTaskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = (
+            'id',
+            'order',
+            'title',
+            'text',
+        )
+
+
 class IssueResourceSerializer(serializers.ModelSerializer):
 
     integration = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -702,7 +728,7 @@ class IssueResourceSerializer(serializers.ModelSerializer):
 class IssueSerializer(serializers.ModelSerializer):
 
     project = serializers.PrimaryKeyRelatedField(read_only=True)
-    task = serializers.PrimaryKeyRelatedField(read_only=True)
+    task = IssueTaskSerializer(read_only=True)
     resources = IssueResourceSerializer(read_only=True, many=True)
 
     class Meta:
