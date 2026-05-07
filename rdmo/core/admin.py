75ccdfbe8cc4686a5d22a3efd1ab08e8214d09b7
David Wallace <david.wallace@tu-darmstadt.de>
2026-05-07 09:44:34 +0200

    Add CustomSiteAdmin and add id to list display
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/core/admin.py
+++ rdmo/core/admin.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib import admin
+from django.contrib.sites.admin import SiteAdmin
+from django.contrib.sites.models import Site
 
 
 class ElementAdminForm(forms.ModelForm):
@@ -6,3 +9,13 @@ class ElementAdminForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['uri_path'].required = True
+
+
+if admin.site.is_registered(Site):
+    admin.site.unregister(Site)
+
+
+@admin.register(Site)
+class CustomSiteAdmin(SiteAdmin):
+    list_display = ('id', 'domain', 'name')
+    search_fields = ('domain', 'name')
