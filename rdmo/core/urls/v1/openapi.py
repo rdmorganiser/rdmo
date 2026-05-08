318d5fbb29139b858da6ae75046b7cc461ef7ac1
Jochen Klar <mail@jochenklar.de>
2026-05-09 00:03:37 +0200

    Use DRF token or session authentication for the schema

--- rdmo/core/urls/v1/openapi.py
+++ rdmo/core/urls/v1/openapi.py
@@ -2,13 +2,22 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path
 from django.views.generic.base import RedirectView
 
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 app_name = 'v1-openapi'
 
+
+class AuthenticatedSpectacularAPIView(SpectacularAPIView):
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
 urlpatterns = [
-    path('schema/', login_required(SpectacularAPIView.as_view()), name='schema'),
+    path('schema/', AuthenticatedSpectacularAPIView.as_view(), name='schema'),
     path('swagger/', login_required(SpectacularSwaggerView.as_view(url_name='v1-openapi:schema')), name='swagger'),
     path('redoc/', login_required(SpectacularRedocView.as_view(url_name='v1-openapi:schema')), name='redoc'),
-    path('', RedirectView.as_view(pattern_name='api', permanent=False))
+    path('', RedirectView.as_view(pattern_name='api', permanent=False)),
 ]
