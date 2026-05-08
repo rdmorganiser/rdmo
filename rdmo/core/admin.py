3edd5497f53e8b0ff2ced12b50c55d1f19c34baf
David Wallace <david.wallace@tu-darmstadt.de>
2026-05-08 07:49:30 +0200

    Rename import to DjangoSiteAdmin, add link and search and order by id
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/core/admin.py
+++ rdmo/core/admin.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib import admin
-from django.contrib.sites.admin import SiteAdmin
+from django.contrib.sites.admin import SiteAdmin as DjangoSiteAdmin
 from django.contrib.sites.models import Site
 
 
@@ -10,12 +10,14 @@ class ElementAdminForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['uri_path'].required = True
 
-
+# the original Django SiteAdmin needs to be unregistered first
 if admin.site.is_registered(Site):
     admin.site.unregister(Site)
 
 
 @admin.register(Site)
-class CustomSiteAdmin(SiteAdmin):
+class SiteAdmin(DjangoSiteAdmin):
     list_display = ('id', 'domain', 'name')
-    search_fields = ('domain', 'name')
+    list_display_links = ('domain',)
+    search_fields = ('id', 'domain', 'name')
+    ordering = ('id',)
