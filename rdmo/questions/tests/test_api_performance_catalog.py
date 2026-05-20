1d95756d03de5f076dbd896b5ea945a25c34485a
David Wallace <david.wallace@tu-darmstadt.de>
2026-05-20 10:37:25 +0200

    Also refactor catalog performance test
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/questions/tests/test_api_performance_catalog.py
+++ rdmo/questions/tests/test_api_performance_catalog.py
@@ -4,26 +4,18 @@ from django.urls import reverse
 
 from .test_viewset_catalog import urlnames
 
-max_query_map = {
-    # action: max_queries url_kwargs
-    'list': {'max_queries': 8},
-    'index': {'max_queries': 3},
-    'export': {'max_queries': 30, 'url_kwargs': {'export_format': 'xml'}},
-    'detail': {'max_queries': 8, 'url_kwargs': {'pk': 1}},
-    'detail_export': {
-        'max_queries': 29, 'url_kwargs': {'pk': 1, 'export_format': 'xml'},
-    },
-}
+max_queries = [
+    # action, max_queries url_kwargs
+    ('list', 8, {}),
+    ('index', 3, {}),
+    ('export', 30, {'export_format': 'xml'}),
+    ('detail', 8, {'pk': 1}),
+    ('detail_export', 29, {'pk': 1, 'export_format': 'xml'}),
+]
 
 
 @pytest.mark.performance
-@pytest.mark.parametrize(
-    'action,max_queries,url_kwargs',
-    [
-        (action, case['max_queries'], case.get('url_kwargs'))
-        for action, case in max_query_map.items()
-    ],
-)
+@pytest.mark.parametrize('action,max_queries,url_kwargs', max_queries)
 def test_catalog_endpoints_query_counts(
     db, admin_client, django_assert_max_num_queries,
     action, max_queries, url_kwargs,
