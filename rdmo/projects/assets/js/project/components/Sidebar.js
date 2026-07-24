3edbc4e6376152c7018c7e7847d11df8e1d96d7f
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-07-24 13:22:04 +0200

    Make sure there is visibility in state

--- rdmo/projects/assets/js/project/components/Sidebar.js
+++ rdmo/projects/assets/js/project/components/Sidebar.js
@@ -12,7 +12,7 @@ const Sidebar = () => {
   const dispatch = useDispatch()
 
   const { area } = useSelector((state) => state.config)
-  const { project } = useSelector((state) => state.project)
+  const { project, visibility } = useSelector((state) => state.project)
 
   const catalog = project?.catalogs.find(catalog => catalog.id == project.project.catalog)
 
@@ -48,7 +48,7 @@ const Sidebar = () => {
           <h2 className="font-large mb-2">{project.project.title}</h2>
           <p className="font-normal text-muted m-0">{catalog.title}</p>
           {
-            project.project?.visibility ? (
+            visibility && project.project?.visibility ? (
               <p className="font-normal text-muted m-0 mt-2">{project.project.visibility}</p>) : null
           }
         </div>
