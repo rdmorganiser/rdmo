078011f45a0a5e578430b7e24c26169b7f8ecf68
David Wallace <david.wallace@tu-darmstadt.de>
2026-08-31 13:30:48 +0200

    Add tests for max queries and resolve count on projects endpoints
    Signed-off-by: David Wallace <david.wallace@tu-darmstadt.de>

--- rdmo/projects/tests/test_queries.py
+++ rdmo/projects/tests/test_queries.py
@@ -2,29 +2,50 @@ import pytest
 
 from django.urls import reverse
 
+from .test_viewset_project import urlnames
+
 max_queries = [
-    # method, urlname, max_queries, url_args
-    ('get', 'project_answers', 38, [1]),
-    ('get', 'project_answers_export', 31, [1, 'html']),
-    ('get', 'v1-projects:project-navigation', 43, [1]),
-    ('get', 'v1-projects:project-answers', 43, [1]),
-    ('post', 'v1-projects:project-progress', 44, [1]),
-    ('get', 'v1-projects:project-page-detail', 46, [1, 1]),
-    ('get', 'v1-projects:project-page-detail', 50, [1, 42]),
-    ('get', 'v1-projects:project-page-detail', 62, [1, 87]),
+    # action, max_queries, url_kwargs, url_params
+    ('project_answers', 38, {'pk': 1}, {}),
+    ('project_answers_export', 31, {'pk': 1, 'format': 'html'}, {}),
+    ('navigation', 43, {'pk': 1}, {}),
+    ('answers', 43, {'pk': 1}, {}),
+    ('page_detail', 46, {'parent_lookup_project': 1, 'pk': 1}, {}),
+    ('page_detail', 50, {'parent_lookup_project': 1, 'pk': 42}, {}),
+    ('page_detail', 62, {'parent_lookup_project': 1, 'pk': 87}, {}),
+    ('progress', 44, {'pk': 1}, {}),
 ]
 
 
 @pytest.mark.performance
-@pytest.mark.parametrize('method,urlname,max_queries,url_args', max_queries)
-def test_queries(db, client, django_assert_max_num_queries, method, urlname, max_queries, url_args):
+@pytest.mark.parametrize('action,max_queries,url_kwargs,url_params', max_queries)
+def test_queries(db, client, django_assert_max_num_queries, action, max_queries, url_kwargs, url_params):
     client.login(username='owner', password='owner')
-    url = reverse(urlname, args=url_args)
+    url = reverse(urlnames[action], kwargs=url_kwargs)
 
     with django_assert_max_num_queries(max_queries):
-        if method == 'get':
-            response = client.get(url)
-        elif method == 'post':
-            response = client.post(url)
+        if action == 'progress':
+            response = client.post(url, query_params=url_params)
+        else:
+            response = client.get(url, query_params=url_params)
 
     assert response.status_code == 200
+
+
+@pytest.mark.performance
+def test_resolve_queries(db, client, django_assert_max_num_queries):
+    client.login(username='owner', password='owner')
+    url = reverse(urlnames['resolve'], kwargs={'pk': 1})
+    params = {
+        'set_prefix': '',
+        'set_index': 0,
+        'element_type': 'conditions',
+        'element_id': 1,
+    }
+
+    with django_assert_max_num_queries(19):
+        response = client.post(url, [params, params, params], content_type='application/json')
+
+    assert response.status_code == 200
+    assert len(response.json()) == 3
+    assert response.json()[0] == response.json()[1] == response.json()[2]
