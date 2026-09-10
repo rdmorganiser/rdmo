70066b0162d6b26d401f86af3137977eec8ee8b0
Jochen Klar <mail@jochenklar.de>
2026-09-10 15:34:15 +0200

    Remove back_to_project_link

--- rdmo/core/templatetags/core_tags.py
+++ rdmo/core/templatetags/core_tags.py
@@ -3,8 +3,7 @@ from django.conf import settings
 from django.template import TemplateDoesNotExist
 from django.template.defaultfilters import stringfilter
 from django.template.loader import get_template, render_to_string
-from django.urls import resolve, reverse
-from django.urls.resolvers import Resolver404
+from django.urls import reverse
 from django.utils import translation
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
@@ -104,27 +103,6 @@ def bootstrap_delete_form(context, **kwargs):
         form_context['submit'] = kwargs['submit']
 
     return render_to_string('core/bootstrap_delete_form.html', form_context, request=context.request)
-
-
-@register.simple_tag(takes_context=True)
-def back_to_project_link(context):
-    try:
-        resolver_match = resolve(context.request.path_info)
-        if resolver_match.url_name != 'project':
-            project = context.get('project')
-            if project:
-                project_id = project.id
-            else:
-                project_id = resolver_match.kwargs.get('project_id')
-
-            if project_id is not None:
-                return render_to_string('core/back_to_project_link.html', {
-                    'project_id': project_id
-                }, request=context.request)
-    except Resolver404:
-        pass
-
-    return ''
 
 
 @register.filter(is_safe=True)
