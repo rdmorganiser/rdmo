22ba4e40192d9292cf4d3a96bcae4d13890a731c
Marco Klement <marco.klement@fau.de>
2026-09-10 08:12:36 +0000

    p: reparent on delete

--- rdmo/projects/models/project.py
+++ rdmo/projects/models/project.py
@@ -180,5 +180,5 @@ class Project(TreeModel, Model):
 @receiver(pre_delete, sender=Project)
 def reparent_children(sender, instance, **kwargs):
     for child in instance.get_children():
-        Project.objects.move(child, instance.parent, 'last-child')
+        child.parent = instance.parent
         child.save()
