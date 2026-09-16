8274f6a3fc32cb1c1b931146b5cd53526237c73b
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-09-16 14:56:38 +0200

    Use user pems to determine if sites and groups are to be fetched

--- rdmo/projects/assets/js/project/store/configureStore.js
+++ rdmo/projects/assets/js/project/store/configureStore.js
@@ -3,9 +3,9 @@ import thunk from 'redux-thunk'
 import { isNil } from 'lodash'
 
 import * as configActions from 'rdmo/core/assets/js/actions/configActions'
-// import * as groupsActions from 'rdmo/core/assets/js/actions/groupsActions'
+import * as groupsActions from 'rdmo/core/assets/js/actions/groupsActions'
 import * as settingsActions from 'rdmo/core/assets/js/actions/settingsActions'
-// import * as sitesActions from 'rdmo/core/assets/js/actions/sitesActions'
+import * as sitesActions from 'rdmo/core/assets/js/actions/sitesActions'
 import * as templateActions from 'rdmo/core/assets/js/actions/templateActions'
 import * as userActions from 'rdmo/core/assets/js/actions/userActions'
 import configReducer from 'rdmo/core/assets/js/reducers/configReducer'
@@ -86,16 +86,24 @@ export default function configureStore() {
       store.dispatch(projectActions.fetchProject()),
       store.dispatch(rolesActions.fetchRoles())
     ]).then(() => {
-      const permissions = store.getState().project.project.project.permissions
+      const state = store.getState()
+      const project = state.project.project.project
+      const currentUser = state.user.currentUser
+
+      const permissions = getEffectivePermissions(
+        project.permissions,
+        currentUser.permissions
+      )
+
       if (permissions.can_view_invite) {
         store.dispatch(projectActions.fetchProjectInvites(projectId))
       }
 
       if (permissions.can_view_visibility) {
-        // TODO: enable again and guard with the new user permissions
-        // store.dispatch(sitesActions.fetchSites())
-        // store.dispatch(groupsActions.fetchGroups())
-        const project = store.getState().project.project.project
+        if (currentUser.permissions.can_view_visibility) {
+          store.dispatch(sitesActions.fetchSites())
+          store.dispatch(groupsActions.fetchGroups())
+        }
         if (!isNil(project.visibility)) {
           store.dispatch(projectActions.fetchProjectVisibility(projectId))
         }
