9b320b43588e2b293ea44a46a18cb2d21b0aa01d
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2026-09-17 17:44:32 +0200

    Render Link conditionally

--- rdmo/projects/assets/js/project/components/Sidebar.js
+++ rdmo/projects/assets/js/project/components/Sidebar.js
@@ -35,11 +35,7 @@ const Sidebar = () => {
       items: [
         { area: 'information', name: gettext('Project information'), icon: 'bi-info-square' },
         { area: 'memberships', name: gettext('Membership'), icon: 'bi-people' },
-        ...(
-          Object.keys(providers).length > 0 ? (
-            [{ area: 'integrations', name: gettext('Integrations'), icon: 'bi-wrench' }]
-          ) : []
-        ),
+        { area: 'integrations', name: gettext('Integrations'), icon: 'bi-wrench' },
       ],
     },
   ]
@@ -69,18 +65,20 @@ const Sidebar = () => {
               <nav className="nav nav-pills nav-fill flex-column">
                 {
                   group.items.map((item, itemIndex) => (
-                    <Link
-                      key={itemIndex}
-                      href={buildPath({ area: item.area })}
-                      title={item.name}
-                      className={classNames('nav-link', { active: area === item.area })}
-                      onClick={() => dispatch(navigateDashboard({ area: item.area }))}
-                    >
-                      <div className="d-flex align-items-center gap-2">
-                        <i className={`bi ${item.icon}`}></i>
-                        <span className="d-none d-lg-inline">{item.name}</span>
-                      </div>
-                    </Link>
+                    (item.area !== 'integrations' || Object.keys(providers).length > 0) && (
+                      <Link
+                        key={itemIndex}
+                        href={buildPath({ area: item.area })}
+                        title={item.name}
+                        className={classNames('nav-link', { active: area === item.area })}
+                        onClick={() => dispatch(navigateDashboard({ area: item.area }))}
+                      >
+                        <div className="d-flex align-items-center gap-2">
+                          <i className={`bi ${item.icon}`}></i>
+                          <span className="d-none d-lg-inline">{item.name}</span>
+                        </div>
+                      </Link>
+                    )
                   ))
                 }
               </nav>
