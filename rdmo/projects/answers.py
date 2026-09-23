3fee6cb02c8053436fc5e51ecd1ac8a5058ff79f
Marco Klement <marco.klement@fau.de>
2026-09-23 11:40:54 +0000

    fix verbose AnswerTree for catalog and questionset (#1788)

--- rdmo/projects/answers.py
+++ rdmo/projects/answers.py
@@ -42,14 +42,19 @@ class AnswerTree:
                 'uri': element.uri
             })
 
-            if element_type in ['catalog', 'page', 'questionset']:
+            if element_type in ['catalog', 'questionset']:
                 element_node.update({
-                    'title': markdown2html(element.short_title) or markdown2html(element.title),
+                    'title': markdown2html(element.title),
                     'help': markdown2html(element.help)
                 })
             elif element_type == 'section':
                 element_node.update({
                     'title': markdown2html(element.short_title) or markdown2html(element.title)
+                })
+            elif element_type == 'page':
+                element_node.update({
+                    'title': markdown2html(element.short_title) or markdown2html(element.title),
+                    'help': markdown2html(element.help)
                 })
             elif element_type == 'question':
                 element_node.update({
